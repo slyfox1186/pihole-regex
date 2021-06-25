1cbe547455a3b393c0dc085fee9a49b9635d01d3
Jeff <37704019+slyfox1186@users.noreply.github.com>
2021-06-25 17:59:20 -0400

    Update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -36,7 +36,7 @@ def fetch_url(url):
     return response
 
 
-url_regexps_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.youtube.blocklist/main/blacklist/regex-filters.txt'
+url_regexps_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.youtube.blocklist/main/blacklist/youtube-regex-filters.txt'
 install_comment = 'github.com/slyfox1186'
 
 cmd_restart = ['pihole', 'restartdns', 'reload']
