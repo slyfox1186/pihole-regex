f4d790940eb9a52659f767e25d6b9278e4a39dd6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2021-08-15 21:35:08 -0400

    Update uninstall.py

--- whitelist/scripts/uninstall.py
+++ whitelist/scripts/uninstall.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Project homepage: https://github.com/slyfox1186/pihole.youtube.blocklist/
 # ================================================================================
 
 import os
