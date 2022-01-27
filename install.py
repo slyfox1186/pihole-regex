2d4e55208131d53588caaa76b340d7e829097a05
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-01-27 16:25:30 -0500

    Update install.py

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ def fetch_url(url):
     if not url:
         return
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
 
     print('[i] Fetching:', url)
 
