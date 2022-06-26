28a07e639d7bd6e2f091d7ccc5bb7fffa8d704c5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-26 04:48:42 -0400

    Update install-regex-blacklist.py

--- scripts/install-regex-blacklist.py
+++ scripts/install-regex-blacklist.py
@@ -40,7 +40,7 @@ def fetch_url(url):
 
 
 url_regexps_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/regex-blacklist.txt'
-install_comment = 'slyregex'
+install_comment = 'slyRegEx'
 
 cmd_restart = ['pihole', 'restartdns', 'reload']
 
