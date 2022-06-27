e7621acfb6da311b52a6105f9844be105e1b8892
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-27 18:58:10 -0400

    Update uninstall-regex-blacklist.py

--- scripts/uninstall-regex-blacklist.py
+++ scripts/uninstall-regex-blacklist.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
 import json
 import os
-import time
 import sqlite3
+import time
 import subprocess, platform
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
@@ -39,7 +38,7 @@ def fetch_url(url):
     return response
 
 
-url_RegExStrings_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/regex-blacklist.txt'
+url_regstrings_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/regex-blacklist.txt'
 install_comment = 'sly99'
 
 cmd_restart = ['pihole', 'restartdns', 'reload']
@@ -48,11 +47,11 @@ db_exists = False
 conn = None
 c = None
 
-RegExStrings_remote = set()
-RegExStrings_local = set()
-RegExStrings_slyfox1186_local = set()
-RegExStrings_legacy_slyfox1186 = set()
-RegExStrings_remove = set()
+regstrings_remote = set()
+regstrings_local = set()
+regstrings_slyfox1186_local = set()
+regstrings_legacy_slyfox1186 = set()
+regstrings_remove = set()
 
 # Start the docker directory override
 print('[i] Checking if Pi-hole is running inside a docker container.')
@@ -117,15 +116,15 @@ if os.path.isfile(path_pihole_db) and os.path.getsize(path_pihole_db) > 0:
 else:
     print('[i] Legacy regex.list detected.')
 
-# Fetch the remote RegExStrings
-str_RegExStrings_remote = fetch_url(url_RegExStrings_remote)
+# Fetch the remote regstrings
+str_regstrings_remote = fetch_url(url_regstrings_remote)
 
-# If RegExStrings were fetched, remove any comments and add to set
-if str_RegExStrings_remote:
-    RegExStrings_remote.update(x for x in map(str.strip, str_RegExStrings_remote.splitlines()) if x and x[:1] != '#')
-    print(f'[i] {len(RegExStrings_remote)} RegExStrings collected from {url_RegExStrings_remote}')
+# If regstrings were fetched, remove any comments and add to set
+if str_regstrings_remote:
+    regstrings_remote.update(x for x in map(str.strip, str_regstrings_remote.splitlines()) if x and x[:1] != '#')
+    print(f'[i] {len(regstrings_remote)} regstrings collected from {url_regstrings_remote}')
 else:
-    print('[i] No remote RegExStrings were found.')
+    print('[i] No remote regstrings were found.')
     exit(1)
 
 if db_exists:
@@ -141,13 +140,13 @@ if db_exists:
     # Create a cursor object
     c = conn.cursor()
 
-    # Identify and remove RegExStrings
-    print("[i] Removing slyfox1186's RegExStrings")
+    # Identify and remove regstrings
+    print("[i] Removing slyfox1186's regstrings")
 
     c.executemany('DELETE FROM domainlist '
                   'WHERE type = 3 '
                   'AND (domain in (?) OR comment = ?)',
-                  [(x, install_comment) for x in RegExStrings_remote])
+                  [(x, install_comment) for x in regstrings_remote])
 
     conn.commit()
 
@@ -158,9 +157,9 @@ if db_exists:
     print("[i] Pi-hole is running. Continue executing the script.\n")
     c.execute('Select domain FROM domainlist WHERE type = 3')
     final_results = c.fetchall()
-    RegExStrings_local.update(x[0] for x in final_results)
+    regstrings_local.update(x[0] for x in final_results)
 
-    print(*sorted(RegExStrings_local), sep='\n')
+    print(*sorted(regstrings_local), sep='\n')
 
     conn.close()
 
@@ -169,32 +168,32 @@ else:
     if os.path.isfile(path_legacy_regex) and os.path.getsize(path_legacy_regex) > 0:
         print('[i] Analyzing the current regex.list')
         with open(path_legacy_regex, 'r') as fRead:
-            RegExStrings_local.update(x for x in map(str.strip, fRead) if x and x[:1] != '#')
+            regstrings_local.update(x for x in map(str.strip, fRead) if x and x[:1] != '#')
 
     # If the local regexp set is not empty
-    if RegExStrings_local:
-        print(f'[i] {len(RegExStrings_local)} existing RegExStrings identified')
+    if regstrings_local:
+        print(f'[i] {len(regstrings_local)} existing regstrings identified')
         # If we have a record of a previous legacy install
         if os.path.isfile(path_legacy_slyfox1186_regex) and os.path.getsize(path_legacy_slyfox1186_regex) > 0:
             print('[i] An existing slyfox1186-regex installation was found.')
-            # Read the previously installed RegExStrings to a set
+            # Read the previously installed regstrings to a set
             with open(path_legacy_slyfox1186_regex, 'r') as fOpen:
-                RegExStrings_legacy_slyfox1186.update(x for x in map(str.strip, fOpen) if x and x[:1] != '#')
+                regstrings_legacy_slyfox1186.update(x for x in map(str.strip, fOpen) if x and x[:1] != '#')
 
-                if RegExStrings_legacy_slyfox1186:
-                    print(f'[i] The script is removing RegExStrings found in {path_legacy_slyfox1186_regex}')
-                    RegExStrings_local.difference_update(RegExStrings_legacy_slyfox1186)
+                if regstrings_legacy_slyfox1186:
+                    print(f'[i] The script is removing regstrings found in {path_legacy_slyfox1186_regex}')
+                    regstrings_local.difference_update(regstrings_legacy_slyfox1186)
 
             # Remove slyfox1186-regex.list as it will no longer be required
             os.remove(path_legacy_slyfox1186_regex)
         else:
-            print('[i] Removing the RegExStrings that have a match in the remote repository')
-            RegExStrings_local.difference_update(RegExStrings_remote)
+            print('[i] Removing the regstrings that have a match in the remote repository')
+            regstrings_local.difference_update(regstrings_remote)
 
     # Output to regex.list
-    print(f'[i] Outputting {len(RegExStrings_local)} RegExStrings to {path_legacy_regex}')
+    print(f'[i] Outputting {len(regstrings_local)} regstrings to {path_legacy_regex}')
     with open(path_legacy_regex, 'w') as fWrite:
-        for line in sorted(RegExStrings_local):
+        for line in sorted(regstrings_local):
             fWrite.write(f'{line}\n')
 
     print('[i] Pi-hole must restart... please wait for it to boot.')
