ba5bdfd3e5347e526214098fd9767414b5ffbf42
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 01:13:47 -0400

    Update install-exact-whitelist.py

--- scripts/install-exact-whitelist.py
+++ scripts/install-exact-whitelist.py
@@ -167,9 +167,8 @@ if db_exists:
         newWL = [None]
         newWhiteList = [None] * newWhiteListlen
         for newWhiteDomain in newWhiteTUP: # For each line found domains.sql
-            s = newWhiteDomain
-            print(re.sub('^\/.*$', '', s))
             nW[nwl] = newWhiteDomain # Add line to a controlled list
+            removeComments = nW[nwl].replace(re.sub('^\/.*$', '', nW[nwl]))
             removeBrace = nW[nwl].replace('(', '') # Remove (
             removeBraces10 = removeBrace.replace(')', '') # Remove )
             newWL = removeBraces10.split(', ') # Split at commas to create a list
