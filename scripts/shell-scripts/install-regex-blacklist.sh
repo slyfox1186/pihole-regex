9a32bac06865fdd7481467eb52aaeee10302186e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 02:20:39 -0400

    Update install-regex-blacklist.sh

--- scripts/shell-scripts/install-regex-blacklist.sh
+++ scripts/shell-scripts/install-regex-blacklist.sh
@@ -22,7 +22,7 @@ fi
 shopt -s nocasematch
 
 # Get the user input
-echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option:\\n\\n\\t[A]dd or [R]emove: "
+echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read myChoice
 if [ "$myChoice" == "A" ]; then
     echo -e "Adding the custom RegEx filters to Pi-hole.\\n"
