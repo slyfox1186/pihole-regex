b39b739941f19198b9f6ade8224236d3188a0944
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 01:50:34 -0400

    Update regex-blacklist.sh

--- scripts/shell-scripts/regex-blacklist.sh
+++ scripts/shell-scripts/regex-blacklist.sh
@@ -2,11 +2,12 @@
 
 clear
 
-# Get user input
-read -p "Make a choice: [I]nstall or [U]ninstall RegEx filters: " myChoice
-
 # Make user input case insensitive
 shopt -s nocasematch
+
+# Get user input
+echo "Choose an action for whitelist.txt: [I]nstall or [U]ninstall: "
+read myChoice
 if [ "$myChoice" == "I" ]; then
     echo -e "Adding RegEx to Pi-hole.\\n"
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-blacklist.py' | /usr/bin/python3
