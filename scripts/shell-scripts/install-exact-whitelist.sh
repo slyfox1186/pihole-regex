99ab6a699888c007c1cd6979747c9f350ce61798
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 04:45:22 -0400

    Update install-exact-whitelist.sh

--- scripts/shell-scripts/install-exact-whitelist.sh
+++ scripts/shell-scripts/install-exact-whitelist.sh
@@ -2,15 +2,12 @@
 
 clear
 
-# Change to user's home directory
-cd $HOME
-
-# Create $HOME/myScripts directory if not exist
+# Create $HOME/myScripts directory if it doesn't exist
 if [ ! -d $HOME/myScripts ]; then
     mkdir -pv $HOME/myScripts
 fi
 
-# Change to $HOME/myScripts directory
+# Change the working directory to $HOME/myScripts
 cd $HOME/myScripts
 
 # Delete any leftover files from previous runs.
@@ -21,15 +18,25 @@ fi
 # Make user input case insensitive
 shopt -s nocasematch
 
-# Get user input
-echo "Please input an option: [A]dd or [R]emove the whitelist.txt domains: "
-read myChoice
-if [[ $myChoice == "A" ]]; then
+# Get the user's input
+echo -e "SlyFox1186's shell script for adding or removing the exact whitelist repository domains for Pi-hole.\\n\\n"
+echo "Choose an option: [A]dd or [R]emove the repository whitelist.txt: "
+read a
+if [[ $a == "A" ]]; then
     clear
+    echo -e "Adding exact whitelist domains to Pi-hole.\\n"
+    sleep 3
     git clone 'https://github.com/slyfox1186/pihole.regex.git'
     python3 'pihole.regex/scripts/install-exact-whitelist.py'
-else
+    echo -e "\\nScript complete!\\nReload your Pi-hole's web interface to see the changes."
+elif [[ $a == "R" ]]; then
     clear
+    echo -e "Removing the exact whitelist domains from Pi-hole.\\n"
+    sleep 3
     git clone 'https://github.com/slyfox1186/pihole.regex.git'
     python3 'pihole.regex/scripts/uninstall-exact-whitelist.py'
+    echo -e "\\nScript complete!\\nReload your Pi-hole's web interface to see the changes."
+else
+    echo -e "Syntax failure!\\nYou must input either the letter 'A' or 'R'\\nTry running the commands again."
+    exit 1
 fi
