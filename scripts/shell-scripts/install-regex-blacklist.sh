31c504fb2b2a6efaf93ead16b865006e0688b2a8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 02:26:17 -0400

    Update install-regex-blacklist.sh

--- scripts/shell-scripts/install-regex-blacklist.sh
+++ scripts/shell-scripts/install-regex-blacklist.sh
@@ -20,20 +20,20 @@ fi
 
 # Make user input case insensitive
 shopt -s nocasematch
-
 # Get the user input
 echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
-read myChoice
-if [ "$myChoice" == "A" ]; then
-    echo -e "Adding the custom RegEx filters to Pi-hole.\\n"
+read a
+if [[ $a == "A" ]]; then
+    clear
+    echo -e "Adding custom RegEx filters to Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-blacklist.py' | sudo /usr/bin/python3
-elif [ "$myChoice" == "R" ]; then
-    echo -e "Removing the custom RegEx filters from Pi-hole.\\n"
+elif [[ $a == "R" ]]; then
+    clear
+    echo -e "Removing custom RegEx filters from Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-blacklist.py' | sudo /usr/bin/python3
 else
     echo -e "Syntax failure!\\nYou must input either the letter 'A' or 'R'\\nTry running the commands again."
-    read -p "Press Enter to exit"
     exit 1
 fi
