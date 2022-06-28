45a46cadbc98f4bddb7ea4f5979f7f2fadf88c2e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-06-28 04:46:00 -0400

    Update install-regex-blacklist.sh

--- scripts/shell-scripts/install-regex-blacklist.sh
+++ scripts/shell-scripts/install-regex-blacklist.sh
@@ -25,12 +25,12 @@ if [[ $a == "A" ]]; then
     clear
     echo -e "Adding custom RegEx filters to Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-blacklist.py' | sudo /usr/bin/python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-blacklist.py' | sudo python3
 elif [[ $a == "R" ]]; then
     clear
     echo -e "Removing custom RegEx filters from Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-blacklist.py' | sudo /usr/bin/python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-blacklist.py' | sudo python3
 else
     echo -e "Syntax failure!\\nYou must input either the letter 'A' or 'R'\\nTry running the commands again."
     exit 1
