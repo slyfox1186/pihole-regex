c1e5b63e3b0638331a39586fad8186801f78dded
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 05:38:32 -0400

    Update regex-whitelist.sh

--- scripts/shell-scripts/regex-whitelist.sh
+++ scripts/shell-scripts/regex-whitelist.sh
@@ -17,13 +17,12 @@ shopt -s nocasematch
 # Get the user's input
 echo -e "Install script for SlyFox1186's RegEx whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
+clear
 if [[ $a == "A" ]]; then
-    clear
     echo -e "Adding custom RegEx whitelist filters to Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-whitelist.py' | sudo python3
 else
-    clear
     echo -e "Removing custom RegEx whitelist filters from Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-whitelist.py' | sudo python3
