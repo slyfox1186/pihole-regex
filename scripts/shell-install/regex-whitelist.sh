dbe8724ec7ae66948f6c06ce535ef730368de35d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 10:57:44 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -21,11 +21,11 @@ clear
 if [[ $a == "A" ]]; then
     echo -e "Adding custom RegEx whitelist filters to Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-whitelist.py' | sudo python3
 else
     echo -e "Removing custom RegEx whitelist filters from Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py' | sudo python3
 fi
 
 # CHANGE WORKING DIRECTORY TO THE USER'S "$HOME"
