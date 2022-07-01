380c51fe03ccd7721f134da003f87780975f0b97
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 11:23:43 -0400

    Update exact-whitelist.py

--- scripts/python-uninstall/exact-whitelist.py
+++ scripts/python-uninstall/exact-whitelist.py
@@ -68,9 +68,8 @@ if args.dir:
 else:
     pihole_location = r'/etc/pihole'
 
-
-whitelist_remote_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/exact-whitelist.txt'
-remote_sql_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/exact-whitelist.sql'
+whitelist_remote_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/whitelist/exact-whitelist.txt'
+remote_sql_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/whitelist/exact-whitelist.sql'
 gravity_whitelist_location = os.path.join(pihole_location, 'whitelist.txt')
 gravity_db_location = os.path.join(pihole_location, 'gravity.db')
 slyfox1186_whitelist_location = os.path.join(
