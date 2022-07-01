5b8cfe21696d04b5e885bf81bca71fa372bec67c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 02:36:27 -0400

    Update regex-whitelist.sh

--- scripts/shell-scripts/regex-whitelist.sh
+++ scripts/shell-scripts/regex-whitelist.sh
@@ -13,11 +13,9 @@ if [[ $a == "A" ]]; then
     echo -e "Adding custom RegEx whitelist filters to Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-regex-whitelist.py' | sudo python3
-elif [[ $a == "R" ]]; then
+else
     clear
     echo -e "Removing custom RegEx whitelist filters from Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-regex-whitelist.py' | sudo python3
-else
-    echo -e "Syntax failure!\\nYou must input either the letter 'A' or 'R'\\nTry running the commands again."
 fi
