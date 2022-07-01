7069e9a00f118eb88019fdf2f9f08b7961284a8f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 12:06:03 -0400

    Update exact-whitelist.sql

--- domains/whitelist/exact-whitelist.sql
+++ domains/whitelist/exact-whitelist.sql
@@ -1,3 +1,4 @@
+(0, 'm3u8.gammacdn.com', 1, 'SlyEWL - AdultTime - Required to STREAN VIDEOS')
 (0, 'seedbox-react-infra.s3.amazonaws.com', 1, 'SlyEWL - AdultTime - Required to display SIMILAR ACTORS')
 (0, '0-edge-chat.facebook.com', 1, 'SlyEWL - Facebook')
 (0, '0.client-channel.google.com', 1, 'SlyEWL - Google')
