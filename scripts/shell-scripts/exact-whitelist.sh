6ca094e7f7a00bbaed3650a15fba439874a1bfc8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 05:36:21 -0400

    Update exact-whitelist.sh

--- scripts/shell-scripts/exact-whitelist.sh
+++ scripts/shell-scripts/exact-whitelist.sh
@@ -8,7 +8,7 @@ if [ ! -d "$HOME/myScripts" ]; then
     clear
 fi
 
-# Change working directory to "$HOME/myScripts"
+# Change working directory to "myScripts"
 pushd "$HOME/myScripts"
 
 # Make user input case insensitive
@@ -17,22 +17,21 @@ shopt -s nocasematch
 # Get the user's input
 echo -e "Install script for SlyFox1186's Exact whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
+clear
 if [[ $a == "A" ]]; then
-    clear
     echo -e "Adding custom Exact whitelist filters to Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/install-exact-whitelist.py' | sudo python3
 else
-    clear
     echo -e "Removing custom Exact whitelist filters from Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/uninstall-exact-whitelist.py' | sudo python3
 fi
 
-# CHANGE WORKING DIRECTORY TO THE USER'S "$HOME"
+# Change working directory to the user's "$HOME"
 pushd "$HOME"
 
-# DELETE THE TEMP DIRECTORY "$HOME/myScripts"
+# Delete the temp directory "$HOME/myScripts"
 if [ -d "$HOME/myScripts" ]; then
     rm -R "$HOME/myScripts"
 fi
