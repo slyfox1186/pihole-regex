3772b400430995cf39306bafbd7fa9af1eca8c07
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-07-01 11:04:00 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create tmp directory if not exist.
+# Create "$HOME/tmp" directory if not exist.
 if [ ! -d "$HOME/tmp" ]; then
     mkdir -p "$HOME/tmp"
 fi
@@ -10,21 +10,20 @@ pushd "$HOME/tmp"
 
 # Make user input case insensitive
 shopt -s nocasematch
+clear
 
 # Get the user's input
-echo -e "Install script for SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
 clear
 if [[ $a == "A" ]]; then
-    echo -e "Adding custom RegEx filters to Pi-hole.\\n"
+    echo -e "Adding custom RegEx blacklist filters to Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-elif [[ $a == "R" ]]; then
-    echo -e "Removing custom RegEx filters from Pi-hole.\\n"
+else
+    echo -e "Removing custom RegEx blacklist filters from Pi-hole.\\n"
     sleep 3
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-else
-    echo -e "Syntax failure!\\nYou must input either the letter 'A' or 'R'\\nTry running the commands again."
 fi
 
 echo -e "\\nScript completed!\\n"
