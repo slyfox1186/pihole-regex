1b22fa0db8a8756aeff716b1cea0c0720f73435a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-03 15:53:51 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -7,9 +7,10 @@ fi
 
 # Change working directory to "$HOME/tmp"
 pushd "$HOME/tmp"
+clear
 
 # Make user input case insensitive
-shopt -s nocasematch; clear
+shopt -s nocasematch
 
 # Get the user's input
 echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
