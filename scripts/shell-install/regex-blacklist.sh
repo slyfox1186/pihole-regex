265d1dd1beb37c1b2f38a0f9f07832839979d4ec
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-03 16:05:53 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -12,16 +12,18 @@ pushd "$HOME/tmp"
 shopt -s nocasematch
 
 # Get the user's input
-echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "SlyFox1186's RegEx Blacklist Filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
 clear
 if [[ $a == "A" ]]; then
     echo -e "Adding custom RegEx blacklist filters to Pi-hole.\\n"
     sleep 3
+    clear
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
 else
     echo -e "Removing custom RegEx blacklist filters from Pi-hole.\\n"
     sleep 3
+    clear
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
 fi
 
