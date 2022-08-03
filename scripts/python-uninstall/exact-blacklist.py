0cf364ed7547c55a9a9ec6d38a8b313ce13f4d40
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-03 12:11:35 -0400

    Update exact-blacklist.py

--- scripts/python-uninstall/exact-blacklist.py
+++ scripts/python-uninstall/exact-blacklist.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 
-import json
 import os
+import argparse
 import sqlite3
-import subprocess
+import subprocess, platform
 import time
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 
-def fetch_url(url):
+def fetch_blacklist_url(url):
 
     if not url:
         return
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
 
-    print('[i] Fetching:', url)
-
     try:
         response = urlopen(Request(url, headers=headers))
     except HTTPError as e:
-        print('[E] HTTP Error:', e.code, 'whilst fetching', url)
-        return
+        print('[X] HTTP Error:', e.code, 'whilst fetching', url)
+        print('\n')
+        print('\n')
+        exit(1)
     except URLError as e:
-        print('[E] URL Error:', e.reason, 'whilst fetching', url)
-        return
+        print('[X] URL Error:', e.reason, 'whilst fetching', url)
+        print('\n')
+        print('\n')
+        exit(1)
 
     # Read and decode
     response = response.read().decode('UTF-8').replace('\r\n', '\n')
@@ -32,175 +34,192 @@ def fetch_url(url):
     # If there is data
     if response:
         # Strip leading and trailing whitespace
-        response = '\n'.join(x for x in map(str.strip, response.splitlines()))
+        response = '\n'.join(x.strip() for x in response.splitlines())
 
     # Return the hosts
     return response
 
 
-url_exactps_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/blacklist/exact-blacklist.txt'
-install_comment = 'SlyEBL'
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
 
-cmd_restart = ['pihole', 'restartdns', 'reload']
+
+def restart_pihole(docker):
+    if docker is True:
+        subprocess.call("docker exec -it pihole pihole restartdns reload",
+                        shell=True, stdout=subprocess.DEVNULL)
+    else:
+        subprocess.call(['pihole', 'restartdns', 'reload'],
+                        stdout=subprocess.DEVNULL)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dir", type=dir_path,
+                    help="Optional: Pi-hole /etc directory")
+parser.add_argument(
+    "-D", "--docker",  action='store_true', help="Optional: Set if you're using Pi-hole in a Docker environment")
+args = parser.parse_args()
+
+if args.dir:
+    pihole_location = args.dir
+else:
+    pihole_location = r'/etc/pihole'
+
+blacklist_remote_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/blacklist/exact-blacklist.txt'
+remote_sql_url = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/blacklist/exact-blacklist.sql'
+gravity_blacklist_location = os.path.join(pihole_location, 'blacklist.txt')
+gravity_db_location = os.path.join(pihole_location, 'gravity.db')
+slyfox1186_blacklist_location = os.path.join(
+    pihole_location, 'slyfox1186-blacklist.txt')
 
 db_exists = False
-conn = None
-c = None
+sqliteConnection = None
+cursor = None
 
-exactps_remote = set()
-exactps_local = set()
-exactps_slyfox1186_local = set()
-exactps_legacy_slyfox1186 = set()
-exactps_remove = set()
+blacklist_remote = set()
+blacklist_local = set()
+blacklist_slyfox1186_local = set()
+blacklist_old_slyfox1186 = set()
 
-# Start the docker directory override
-print('[i] Checking for "pihole" docker container')
+os.system('clear')
+print('\n')
+print('''
+If you are using Pi-hole 5.0 or later, this script will only remove domains that were added by itself.
+Any other domains added by the user will remain uneffected.
+''')
 
-# Initialise the docker variables
-docker_id = None
-docker_mnt = None
-docker_mnt_src = None
-
-# Check to see whether the default "pihole" docker container is active
-try:
-    docker_id = subprocess.run(['docker', 'ps', '--filter', 'name=pihole', '-q'],
-                               stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
-# Exception for if docker is not installed
-except FileNotFoundError:
-    pass
-
-# If a pihole docker container was found, locate the first mount
-if docker_id:
-    docker_mnt = subprocess.run(['docker', 'inspect', '--format', '{{ (json .Mounts) }}', docker_id],
-                                stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
-    # Convert output to JSON and iterate through each dict
-    for json_dict in json.loads(docker_mnt):
-        # If this mount's destination is /etc/pihole
-        if json_dict['Destination'] == r'/etc/pihole':
-            # Use the source path as our target
-            docker_mnt_src = json_dict['Source']
-            break
-
-    # If we successfully found the mount
-    if docker_mnt_src:
-        print('[i] Running in docker installation mode.')
-        # Prepend restart commands
-        cmd_restart[0:0] = ['docker', 'exec', '-i', 'pihole']
+# Check for pihole path exsists
+if os.path.exists(pihole_location):
+    print("[i] Pi-hole's path exists.")
 else:
-    print('[i] Running in physical installation mode.')
-
-# Set paths
-path_pihole = docker_mnt_src if docker_mnt_src else r'/etc/pihole'
-path_legacy_exact = os.path.join(path_pihole, 'exact.list')
-path_legacy_slyfox1186_exact = os.path.join(path_pihole, 'slyfox1186-exact.list')
-path_pihole_db = os.path.join(path_pihole, 'gravity.db')
-
-# Check that pi-hole path exists
-if os.path.exists(path_pihole):
-    print('[i] Pi-hole path exists.')
-else:
-    print(f'[e] {path_pihole} was not found')
+    print("[X] {} was not found".format(pihole_location))
+    print('\n')
+    print('\n')
     exit(1)
 
 # Check for write access to /etc/pihole
-if os.access(path_pihole, os.X_OK | os.W_OK):
-    print(f'[i] Write access to {path_pihole} verified')
+if os.access(pihole_location, os.X_OK | os.W_OK):
+    print("[i] Write access to {} verified." .format(pihole_location))
+    blacklist_str = fetch_blacklist_url(blacklist_remote_url)
+    remote_blacklist_lines = blacklist_str.count('\n')
+    remote_blacklist_lines += 1
 else:
-    print(f'[e] Write access is not available for {path_pihole}. Please run as root or other privileged user')
+    print("[X] Write access is not available for {}. Please run the script as a privileged user." .format(pihole_location))
+    print('\n')
+    print('\n')
     exit(1)
 
 # Determine whether we are using DB or not
-if os.path.isfile(path_pihole_db) and os.path.getsize(path_pihole_db) > 0:
+if os.path.isfile(gravity_db_location) and os.path.getsize(gravity_db_location) > 0:
     db_exists = True
-    print('[i] DB detected')
-else:
-    print('[i] Legacy exact.list detected')
+    print('[i] Pi-hole Gravity database found.')
 
-# Fetch the remote exactps
-str_exactps_remote = fetch_url(url_exactps_remote)
+    remote_sql_str = fetch_blacklist_url(remote_sql_url)
+    remote_sql_lines = remote_sql_str.count('\n')
+    remote_sql_lines += 1
 
-# If exactps were fetched, remove any comments and add to set
-if str_exactps_remote:
-    exactps_remote.update(x for x in map(str.strip, str_exactps_remote.splitlines()) if x and x[:1] != '#')
-    print(f'[i] {len(exactps_remote)} exactps collected from {url_exactps_remote}')
+    if len(remote_sql_str) > 0:
+        print("[i] {} domains were discovered." .format(remote_blacklist_lines))
+    else:
+        print('[X] No remote SQL queries were found.')
+        print('\n')
+        print('\n')
+        exit(1)
 else:
-    print('[i] No remote exactps were found.')
+    print('[i] Legacy Pi-hole detected (Version older than 5.0).')
+
+if blacklist_str:
+    blacklist_remote.update(x for x in map(
+        str.strip, blacklist_str.splitlines()) if x and x[:1] != '#')
+else:
+    print('[X] No remote domains found.')
+    print('\n')
+    print('\n')
     exit(1)
 
 if db_exists:
     # Create a DB connection
-    print(f'[i] Connecting to {path_pihole_db}')
+    print("[i] Connecting to Gravity's database.")
 
     try:
-        conn = sqlite3.connect(path_pihole_db)
-    except sqlite3.Error as e:
-        print(e)
+        sqliteConnection = sqlite3.connect(gravity_db_location)
+        cursor = sqliteConnection.cursor()
+        print("[i] Successfully connected to Gravity's database.")
+        total_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 1 AND comment LIKE '%SlyEBL%' ")
+
+        totalDomains = len(total_domains.fetchall())
+        print("[i] There are a total of {} domains in your blacklist which were added by this script." .format(totalDomains))
+        print('[i] Removing domains in the Gravity database.')
+        cursor.execute (" DELETE FROM domainlist WHERE type = 1 AND comment LIKE '%SlyEBL%' ")
+
+        sqliteConnection.commit()
+
+        # We only removed domains we added so use total_domains
+        print("[i] {} domains were removed." .format(totalDomains))
+        remaining_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 1 OR type = 3 ")
+        print("[i] There are a total of {} domains remaining in your blacklist." .format(len(remaining_domains.fetchall())))
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("[X] Failed to remove domains from Gravity's database.", error)
+        print('\n')
+        print('\n')
         exit(1)
 
-    # Create a cursor object
-    c = conn.cursor()
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
 
-    # Identifying slyfox1186 exactps
-    print("[i] Removing slyfox1186's exactps")
-    c.executemany('DELETE FROM domainlist '
-                  'WHERE type = 1 '
-                  'AND (domain in (?) OR comment = ?)',
-                  [(x, install_comment) for x in exactps_remote])
+            print('[i] Connection to Gravity is closed.')
+            time.sleep(2)
 
-    conn.commit()
-
-    print('[i] Restarting Pi-hole')
-    subprocess.run(cmd_restart, stdout=subprocess.DEVNULL)
-
-    # Prepare final result
-    print('[i] Done - Please see your installed exactps below\n')
-
-    c.execute('Select domain FROM domainlist WHERE type = 1')
-    final_results = c.fetchall()
-    exactps_local.update(x[0] for x in final_results)
-
-    print(*sorted(exactps_local), sep='\n')
-
-    conn.close()
+            print('[i] Pi-hole is rebooting... it could take a few seconds.')
+            restart_pihole(args.docker)
+            print('\n')
+            print('The exact blacklist filters have been added successfully! Script complete!')
+            print('\n')
+            print('Star me on GitHub: https://github.com/slyfox1186/pihole.regex')
+            print('\n')
 
 else:
-    # If exact.list exists and is not empty
-    # Read it and add to a set
-    if os.path.isfile(path_legacy_exact) and os.path.getsize(path_legacy_exact) > 0:
-        print('[i] Collecting existing entries from exact.list')
-        with open(path_legacy_exact, 'r') as fRead:
-            exactps_local.update(x for x in map(str.strip, fRead) if x and x[:1] != '#')
+    if os.path.isfile(gravity_blacklist_location) and os.path.getsize(gravity_blacklist_location) > 0:
+        print('[i] Collecting existing entries from blacklist.txt')
+        with open(gravity_blacklist_location, 'r') as fRead:
+            blacklist_local.update(x for x in map(
+                str.strip, fRead) if x and x[:1] != '#')
 
-    # If the local exactp set is not empty
-    if exactps_local:
-        print(f'[i] {len(exactps_local)} existing exactps identified')
-        # If we have a record of the previous legacy install
-        if os.path.isfile(path_legacy_slyfox1186_exact) and os.path.getsize(path_legacy_slyfox1186_exact) > 0:
-            print('[i] Existing slyfox1186-exact install identified')
-            with open(path_legacy_slyfox1186_exact, 'r') as fOpen:
-                exactps_legacy_slyfox1186.update(x for x in map(str.strip, fOpen) if x and x[:1] != '#')
+    if blacklist_local:
+        print("[i] {} existing blacklisted domains identified" .format(
+            len(blacklist_local)))
 
-                if exactps_legacy_slyfox1186:
-                    print(f'[i] Removing exactps found in {path_legacy_slyfox1186_exact}')
-                    exactps_local.difference_update(exactps_legacy_slyfox1186)
+        if os.path.isfile(slyfox1186_blacklist_location) and os.path.getsize(slyfox1186_blacklist_location) > 0:
+            print('[i] Existing slyfox1186-blacklist installation located.')
+            with open(slyfox1186_blacklist_location, 'r') as fOpen:
+                blacklist_old_slyfox1186.update(x for x in map(
+                    str.strip, fOpen) if x and x[:1] != '#')
 
-            # Remove slyfox1186-exact.list as it will no longer be required
-            os.remove(path_legacy_slyfox1186_exact)
+                if blacklist_old_slyfox1186:
+                    blacklist_local.difference_update(blacklist_old_slyfox1186)
+
+            os.remove(slyfox1186_blacklist_location)
+
         else:
-            print('[i] Removing exactps that match the remote repo')
-            exactps_local.difference_update(exactps_remote)
+            print('[i] Removing all domains that match the remote repo.')
+            blacklist_local.difference_update(blacklist_remote)
 
-    # Output to exact.list
-    print(f'[i] Outputting {len(exactps_local)} exactps to {path_legacy_exact}')
-    with open(path_legacy_exact, 'w') as fWrite:
-        for line in sorted(exactps_local):
-            fWrite.write(f'{line}\n')
+    print("[i] Adding exsisting {} domains to {}" .format(
+        len(blacklist_local), gravity_blacklist_location))
+    with open(gravity_blacklist_location, 'w') as fWrite:
+        for line in sorted(blacklist_local):
+            fWrite.write("{}\n".format(line))
 
-    print('[i] Restarting Pi-hole')
-    subprocess.run(cmd_restart, stdout=subprocess.DEVNULL)
-
-    # Prepare final result
-    print('[i] Done - Please see your installed exactps below\n')
-    with open(path_legacy_exact, 'r') as fOpen:
-        for line in fOpen:
-            print(line, end='')
+    print('[i] Pi-hole must restart... please wait for it to boot.')
+    restart_pihole(args.docker)
+    print('The exact blacklist filters have been added successfully! Script complete!')
+    print('\n')
+    print('Star me on GitHub: https://github.com/slyfox1186/pihole.regex.')
+    print('\n')
