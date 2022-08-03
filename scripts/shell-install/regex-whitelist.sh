4f0f3b83e12f4faf180d943e6c9e002c3594237c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-03 15:21:50 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -25,4 +25,4 @@ else
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py' | sudo python3
 fi
 
-echo -e "\\nScript complete!"
+echo -e "Script complete!\\n"
