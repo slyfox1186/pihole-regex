0830036ea1b57825e0ed9f714a99c5d8100d66e4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-11 08:23:04 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create variable
+URL=https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts
+
 # Create "$HOME/tmp" directory if not exist.
 if [ ! -d "$HOME/tmp" ]; then
     mkdir -p "$HOME/tmp"
@@ -9,18 +12,18 @@ fi
 cd "$HOME/tmp"
 
 # Make user input case insensitive
-shopt -s nocasematch
+shopt -s nocasematch; clear
 
 # Get the user's input
-echo -e "SlyFox1186's RegEx Blacklist Filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
 clear
 if [[ $a == "A" ]]; then
     echo -e "Adding custom RegEx blacklist filters to Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py | sudo python3
+    /usr/bin/curl -sSl "$URL/python-install/regex-blacklist.py" | sudo python3
 else
     echo -e "Removing custom RegEx blacklist filters from Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py | sudo python3
+    /usr/bin/curl -sSl "$URL/python-uninstall/regex-blacklist.py" | sudo python3
 fi
