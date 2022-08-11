98e0ba58b745d18d220558dbe8e46244b107b008
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-11 08:25:07 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create variable
+URL=https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts
+
 # Create "$HOME/tmp" directory if not exist.
 if [ ! -d "$HOME/tmp" ]; then
     mkdir -p "$HOME/tmp"
@@ -12,15 +15,15 @@ cd "$HOME/tmp"
 shopt -s nocasematch; clear
 
 # Get the user's input
-echo -e "SlyFox1186's Exact whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "SlyFox1186's exact whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
 clear
 if [[ $a == "A" ]]; then
-    echo -e "Adding custom Exact whitelist filters to Pi-hole.\\n"
+    echo -e "Adding exact whitelist filters to Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl "$URL/python-install/exact-whitelist.py" | sudo python3
 else
-    echo -e "Removing custom Exact whitelist filters from Pi-hole.\\n"
+    echo -e "Removing exact whitelist filters from Pi-hole.\\n"
     sleep 3
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl "$URL/python-uninstall/exact-whitelist.py" | sudo python3
 fi
