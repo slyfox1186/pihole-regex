844438ab3bf984d5c5ea5ca1e32aa14bf678ff77
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 18:11:13 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -2,26 +2,37 @@
 
 clear
 
-# Change working directory to "$HOME/pihole.regex"
-cd "$HOME/pihole.regex"
+FILE_DIR="$HOME/pihole.RegEx"
 
-# Make user input case insensitive
-shopt -s nocasematch
+# Verify: run.sh exists / was downloaded
+if [ ! -f $FILE_DIR/run.sh ]; then
+  if [ ! -f run.sh ]; then
+    echo -e "\\nFile missing: run.sh\\nIt might not have been downloaded so check."
+    read -p 'Press [Enter] to exit.'
+    exit 1
+  else
+    mv 'run.sh' "$FILE_DIR/run.sh"
+    clear
+  fi
+fi
+
+# Change working directory to "$FILE_DIR"
+cd "$FILE_DIR"
 
 # Get the user's input
-echo -e "\\n[RegEx Blacklist] filters: [A]dd [R]emove [E]xit"
+echo -e "\\nRegEx Blacklist Filters: [A]dd [R]emove [S]kip"
 read a
 clear
 if [[ $a == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-    echo '[i] The regex blacklist filters have been added.'
+    echo -e "[i] The RegEx Blacklist Filters were added to pi-hole.\\n"
 elif [[ $a == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-    echo '[i] The regex blacklist filters were removed.'
-elif [[ $a == "E" ]]; then
-    exit 0
+    echo -e "[i] The RegEx Blacklist Filters were removed from pi-hole.\\n"
+elif [[ $a == "S" ]]; then
+    clear
+    echo -e "Skipping ahead!\\n"
 fi
 
-echo -e "\\n"
 read -p '[i] Press enter to continue.'
 clear
