2982e213ab19aa6215b0ffd1a1755f5e43a2cc35
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 16:15:40 -0400

    Update replaced.sql

--- domains/whitelist/replaced.sql
+++ domains/whitelist/replaced.sql
@@ -7,6 +7,7 @@
 (0, '4-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, '5-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, '6-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, 'edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, 'device.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
 (0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
 (0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
