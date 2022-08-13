c45d29fb196163798f532fef8040d41040397c8d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 02:04:12 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -2,9 +2,6 @@
 
 clear
 
-# Create variable
-URL=https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts
-
 # Create "$HOME/pihole.regex" directory if not exist.
 if [ ! -d "$HOME/pihole.regex" ]; then
     mkdir -p "$HOME/pihole.regex"
@@ -21,10 +18,10 @@ echo -e "Exact whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "$URL/python-install/exact-whitelist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py" | sudo python3
     echo 'Script complete: The exact whitelist filters have been added.'
 else
-    /usr/bin/curl -sSl "$URL/python-uninstall/exact-whitelist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py" | sudo python3
     echo 'Script complete: The exact whitelist filters have been removed.'
 fi
 
