fa53066a537ee265ad0f1bf3a2e17f3f48ace043
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 02:03:20 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -2,9 +2,6 @@
 
 clear
 
-# Create variable
-URL=https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts
-
 # Create "$HOME/pihole.regex" directory if not exist.
 if [ ! -d "$HOME/pihole.regex" ]; then
     mkdir -p "$HOME/pihole.regex"
@@ -21,12 +18,13 @@ echo -e "Exact blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "$URL/python-install/exact-blacklist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-blacklist.py" | sudo python3
     echo 'Script complete: The exact blacklist filters have been added.'
 else
-    /usr/bin/curl -sSl "$URL/python-uninstall/exact-blacklist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-blacklist.py" | sudo python3
     echo 'Script complete: The exact blacklist filters have been removed.'
 fi
 
 echo -e "\\n\\n"
 read -p 'Press Enter to continue'
+clear
