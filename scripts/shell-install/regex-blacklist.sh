a3a737b5f55b2bd0d677c2019044eb4e2db45315
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 18:30:56 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -2,22 +2,19 @@
 
 clear
 
-# Change working directory to "$FILE_DIR"
-cd "$FILE_DIR"
-
-# Get the user's input
-echo -e "\\nRegEx Blacklist Filters: [A]dd [R]emove [S]kip"
+# Get user's input
+echo -e "RegEx Blacklist Filters: [A]dd [R]emove [S]kip"
 read a
 clear
 if [[ $a == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-    echo -e "[i] The RegEx Blacklist Filters were added to pi-hole.\\n"
+    echo -e "[i] The RegEx Blacklist Filters were added to pi-hole.\\n\\n"
 elif [[ $a == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-    echo -e "[i] The RegEx Blacklist Filters were removed from pi-hole.\\n"
+    echo -e "[i] The RegEx Blacklist Filters were removed from pi-hole.\\n\\n"
 elif [[ $a == "S" ]]; then
     clear
-    echo -e "Skipping ahead!\\n"
+    echo -e "Skipping ahead!\\n\\n"
 fi
 
 read -p '[i] Press enter to continue.'
