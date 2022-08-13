55c7d203ecfa6a683f86326483d7d4db7fed8c31
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 16:28:41 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -11,20 +11,22 @@ fi
 cd "$HOME/pihole.regex"
 
 # Make user input case insensitive
-shopt -s nocasematch; clear
+shopt -s nocasematch
 
 # Get the user's input
-echo -e "RegEx whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "\\n[RegEx Blacklist] filters: [A]dd [R]emove [E]xit"
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-whitelist.py" | sudo python3
-    echo 'Script complete: The RegEx whitelist filters have been added.'
-else
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py" | sudo python3
-    echo 'Script complete: The RegEx whitelist filters have been removed.'
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
+    echo '[i] The regex blacklist filters have been added.'
+elif [[ $a == "R" ]]; then
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
+    echo '[i] The regex blacklist filters were removed.'
+elif [[ $a == "E" ]]; then
+    exit 1
 fi
 
-echo -e "\\n\\n"
-read -p 'Press Enter to continue'
+echo -e "\\n"
+read -p '[i] Press enter to continue.'
 clear
