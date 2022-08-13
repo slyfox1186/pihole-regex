28225bc919bc10c8c26a36bbc910810e7d666eb6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 17:27:56 -0400

    Update run.sh

--- scripts/shell-install/run.sh
+++ scripts/shell-install/run.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
+sleep 1
 clear
 
-# delete the useless index.html file that is downloaded
+FILE_DIR="$HOME/pihole.regex"
+
+# Delete the useless index.html file that is downloaded
 if [ -f index.html ]; then
     rm index.html
     clear
 fi
 
-# source each script one at a time
+# Create $FILE_DIR directory if not exist
+if [ ! -d $FILE_DIR ]; then
+    mkdir -p "$FILE_DIR"
+fi
+
+# Move scripts to to $FILE_DIR
+if [ -f exact-blacklist.sh ]; then
+    mv 'exact-blacklist.sh' "$FILE_DIR/exact-blacklist.sh"
+    clear
+fi
+
+if [ -f exact-whitelist.sh ]; then
+    mv 'exact-whitelist.sh' "$FILE_DIR/exact-whitelist.sh"
+    clear
+fi
+
+if [ -f regex-blacklist.sh ]; then
+    mv 'regex-blacklist.sh' "$FILE_DIR/regex-blacklist.sh"
+    clear
+fi
+
+if [ -f regex-whitelist.sh ]; then
+    mv 'regex-whitelist.sh' "$FILE_DIR/regex-whitelist.sh"
+    clear
+fi
+
+if [ -f $FILE_DIR/exact-blacklist.sh ] 
+
+# Change working directory to "$HOME/pihole.regex"
+cd "$HOME/pihole.regex"
+
+# Make user input case insensitive
+shopt -s nocasematch
+
+# Source each script one at a time
 . exact-blacklist.sh && \
 . exact-whitelist.sh && \
 . regex-blacklist.sh && \
