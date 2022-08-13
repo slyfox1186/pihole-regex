ce223bb35f335dd233e64e229b687eb3f67fef0c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 15:20:02 -0400

    Create exact-replaced-by-regex.sql

---
+++ domains/whitelist/exact-replaced-by-regex.sql
@@ -0,0 +1,5 @@
+/ This is a backup of all exact white and blacklists that I have
+/ created regex entries for so they don't get thrown away.
+
+(0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
+(0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
