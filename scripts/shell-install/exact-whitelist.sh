4663d29102418bacd9be5b7a001ab0240a494122
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 18:25:27 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -2,31 +2,20 @@
 
 clear
 
-# Create "$HOME/pihole.regex" directory if not exist.
-if [ ! -d "$HOME/pihole.regex" ]; then
-    mkdir -p "$HOME/pihole.regex"
-fi
-
-# Change working directory to "$HOME/pihole.regex"
-cd "$HOME/pihole.regex"
-
-# Make user input case insensitive
-shopt -s nocasematch
-
 # Get the user's input
-echo -e "\\nExact Whitelist Filters: [A]dd [R]emove [E]xit"
+echo -e "Exact Whitelist Filters: [A]dd [R]emove [S]kip"
 read a
 clear
 if [[ $a == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
-    echo '[i] The Exact Whitelist Filters have been added.'
+    echo -e "[i] The Exact Whitelist Filters have been added.\\n\\n"
 elif [[ $a == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
-    echo '[i] The Exact Whitelist Filters have been removed.'
-elif [[ $a == "E" ]]; then
-    exit 0
+    echo -e "[i] The Exact Whitelist Filters have been removed.\\n\\n"
+elif [[ $a == "S" ]]; then
+    clear
+    echo -e "Skipping ahead!\\n\\n"
 fi
 
-echo -e "\\n"
 read -p '[i] Press enter to continue.'
 clear
