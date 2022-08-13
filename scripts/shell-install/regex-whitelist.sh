ac1dd3e040816c9b69d6d27368bfb8e24de660c7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 02:02:39 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -2,9 +2,6 @@
 
 clear
 
-# Create variable
-URL=https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts
-
 # Create "$HOME/pihole.regex" directory if not exist.
 if [ ! -d "$HOME/pihole.regex" ]; then
     mkdir -p "$HOME/pihole.regex"
@@ -21,10 +18,10 @@ echo -e "RegEx whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "$URL/python-install/regex-whitelist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-whitelist.py" | sudo python3
     echo 'Script complete: The RegEx whitelist filters have been added.'
 else
-    /usr/bin/curl -sSl "$URL/python-uninstall/regex-whitelist.py" | sudo python3
+    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py" | sudo python3
     echo 'Script complete: The RegEx whitelist filters have been removed.'
 fi
 
