46b525d54060f5e797508e8e4c2418a52ce14137
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 15:02:59 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -15,15 +15,15 @@ shopt -s nocasematch
 
 # Get the user's input
 # echo -e "Please choose an option: [A]dd or [R]emove [exact whitelist] filters\\n"
-echo -e "[i] [exact whitelist]\\n\\nChoose an option: [A]dd or [R]emove or [E]xit"
+echo -e "[i] exact whitelist\\n\\nChoose an option: [A]dd or [R]emove or [E]xit"
 read a
 clear
 if [[ $a == "A" ]]; then
   /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py" | sudo python3
-  echo '[i] The exact [whitelist filters] have been added.'
+  echo '[i] The exact whitelist filters have been added.'
 else if [[ $a == "R" ]]; then
   /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py" | sudo python3
-  echo '[i] The [exact whitelist] filters have been removed.'
+  echo '[i] The exact whitelist filters have been removed.'
 else if [[ $a == "E" ]]; then
   read -p "exit"
   exit 1
