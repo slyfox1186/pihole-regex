95e87812ea1e504eafc03930df357a51e694dcd9
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 01:31:48 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -15,7 +15,7 @@ cd "$HOME/pihole.regex"
 shopt -s nocasematch; clear
 
 # Get the user's input
-echo -e "SlyFox1186's RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+echo -e "RegEx blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
 read a
 clear
 if [[ $a == "A" ]]; then
