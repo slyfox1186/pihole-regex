143d5e898896a597fe1912b93445d5a7a07a87b0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 16:28:49 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -18,10 +18,10 @@ echo -e "\\n[RegEx Blacklist] filters: [A]dd [R]emove [E]xit"
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
     echo '[i] The regex blacklist filters have been added.'
 elif [[ $a == "R" ]]; then
-    /usr/bin/curl -sSl https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
     echo '[i] The regex blacklist filters were removed.'
 elif [[ $a == "E" ]]; then
     exit 1
