9cc308a29ebac5eb7cd4af8faabd063d6cffa8a4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 15:01:04 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -14,18 +14,21 @@ cd "$HOME/pihole.regex"
 shopt -s nocasematch
 
 # Get the user's input
-# echo -e "Exact whitelist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
-echo -e "[exact whitelist]\\n\\nPlease choose an option: [A]dd or [R]emove"
+# echo -e "Please choose an option: [A]dd or [R]emove [exact whitelist] filters\\n"
+echo -e "[i] [exact whitelist]\\n\\nChoose an option: [A]dd or [R]emove or [E]xit"
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py" | sudo python3
-    echo 'Script complete: The exact whitelist filters have been added.'
-else
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py" | sudo python3
-    echo 'Script complete: The exact whitelist filters have been removed.'
+  /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py" | sudo python3
+  echo '[i] The exact [whitelist filters] have been added.'
+else if [[ $a == "R" ]]; then
+  /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py" | sudo python3
+  echo '[i] The [exact whitelist] filters have been removed.'
+else if [[ $a == "E" ]]; then
+  read -p "exit"
+  exit 1
 fi
 
 echo -e "\\n\\n"
-read -p 'Press Enter to continue'
+read -p 'Press Enter to continue.'
 clear
