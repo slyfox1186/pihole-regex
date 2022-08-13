e4cbdbda90bc5d2e371e2b093960be50c67a2318
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 16:05:53 -0400

    Update and rename domains/whitelist/replaced.sql to domains/replaced.sql

--- domains/replaced.sql
+++ domains/replaced.sql
@@ -1,6 +1,12 @@
-/ This is a backup of all exact white and blacklists that I have
-/ created regex entries for so they don't get thrown away.
+// This is a backup of all exact white and black lists that I have replaced with regex filters.
 
+(0, '0-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '1-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '2-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '3-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '4-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '5-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+(0, '6-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, 'device.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
 (0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
 (0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
