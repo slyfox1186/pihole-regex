1a9e89a8ca9163fb110f9c27a9d91ee8ec0991dd
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 14:57:51 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -14,17 +14,21 @@ cd "$HOME/pihole.regex"
 shopt -s nocasematch
 
 # Get the user's input
-echo -e "Exact blacklist filters\\n\\nPlease choose an option: [A]dd or [R]emove: "
+# echo -e "Please choose an option: [A]dd or [R]emove [exact blacklist] filters\\n"
+echo -e "[i] [exact blacklist]\\n\\nChoose an option: [A]dd or [R]emove or [E]xit"
 read a
 clear
 if [[ $a == "A" ]]; then
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-blacklist.py" | sudo python3
-    echo 'Script complete: The exact blacklist filters have been added.'
-else
-    /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-blacklist.py" | sudo python3
-    echo 'Script complete: The exact blacklist filters have been removed.'
+  /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-blacklist.py" | sudo python3
+  echo '[i] The exact [blacklist filters] have been added.'
+else if [[ $a == "R" ]]; then
+  /usr/bin/curl -sSl "https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-blacklist.py" | sudo python3
+  echo '[i] The [exact blacklist] filters have been removed.'
+else if [[ $a == "E" ]]; then
+  read -p "exit"
+  exit 1
 fi
 
 echo -e "\\n\\n"
-read -p 'Press Enter to continue'
+read -p 'Press Enter to continue.'
 clear
