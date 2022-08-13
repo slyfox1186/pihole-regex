521f3863ce15da9a02bb42ca1364de2eba9bd39b
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 18:31:10 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -2,24 +2,19 @@
 
 clear
 
-FILE_DIR="$HOME/pihole.RegEx"
-
-# Change working directory to "$FILE_DIR"
-cd "$FILE_DIR"
-
-# Get the user's input
-echo -e "\\nRegEx Whitelist Filters: [A]dd [R]emove [S]kip"
+# Get user's input
+echo -e "RegEx Whitelist Filters: [A]dd [R]emove [S]kip"
 read a
 clear
 if [[ $a == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-whitelist.py' | sudo python3
-    echo -e "[i] The RegEx Whitelist Filters were added to pi-hole.\\n"
+    echo -e "[i] The RegEx Whitelist Filters were added to pi-hole.\\n\\n"
 elif [[ $a == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py' | sudo python3
-    echo -e "[i] The RegEx Whitelist Filters were removed from pi-hole.\\n"
+    echo -e "[i] The RegEx Whitelist Filters were removed from pi-hole.\\n\\n"
 elif [[ $a == "S" ]]; then
     clear
-    echo -e "Skipping ahead!\\n"
+    echo -e "Skipping ahead!\\n\\n"
 fi
 
 read -p '[i] Press enter to continue.'
