261591f192e0f462c092b9e0b29f199fa7bb926e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 16:55:29 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+clear
+
+# delete the useless index.html file that is downloaded
+if [ -f index.html ]; then
+    rm index.html
+    clear
+fi
+
+# source each script one at a time
 . exact-blacklist.sh && \
 . exact-whitelist.sh && \
 . regex-blacklist.sh && \
