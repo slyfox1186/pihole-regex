7ba17d4e7256ba293815993a0e4a55ed7c72bcc8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 15:34:28 -0400

    Update replaced.sql

--- domains/whitelist/replaced.sql
+++ domains/whitelist/replaced.sql
@@ -1,5 +1,8 @@
 / This is a backup of all exact white and blacklists that I have
 / created regex entries for so they don't get thrown away.
 
+(0, 'device.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
 (0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
 (0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
+(0, 'title.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
+(0, 'xsts.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
