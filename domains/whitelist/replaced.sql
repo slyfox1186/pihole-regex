2133db7cabe0391223ac906a8d1c58cf90cdc501
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 22:12:31 -0400

    Update replaced.sql

--- domains/whitelist/replaced.sql
+++ domains/whitelist/replaced.sql
@@ -13,3 +13,6 @@
 (0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
 (0, 'title.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
 (0, 'xsts.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
+
+// RegEx Strings replaced
+^kosmos(|-assets)-prod\.react\.gammacdn\.com$
