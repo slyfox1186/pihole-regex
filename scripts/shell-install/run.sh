0cfa8cd68a37b473663c02f6e72379d7d8c23779
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-14 00:44:50 -0400

    Update run.sh

--- scripts/shell-install/run.sh
+++ scripts/shell-install/run.sh
@@ -14,19 +14,19 @@ if [ -f index.html ]; then
 fi
 
 # Create: $FILE_DIR directory if not exist
-if [ -d $FILE_DIR ]; then
-    rm -R "$FILE_DIR"
+if [ -d pihole.regex ]; then
+    rm -R 'pihole.regex'
 else
-    mkdir -p "$FILE_DIR"
+    mkdir -p 'pihole.regex'
 fi
 
 # Verify: If files exist move them to $HOME/pihole.regex
-SHELL_FILES="exact-blacklist exact-whitelist regex-blacklist regex-whitelist"
+SHELL_FILES='exact-blacklist exact-whitelist regex-blacklist regex-whitelist'
 FILES=($SHELL_FILES.sh run.sh)
 
 for i in ${FILES[@]}; do
   if [ -f $i ]; then
-      mv -f "$i" "$FILE_DIR/$i"
+      mv -f "$i" "$FILE_DIR/$i" &>/dev/null
   fi
 done
 
