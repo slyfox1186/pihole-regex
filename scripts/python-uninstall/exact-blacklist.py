bd0ef34809f495a2ad55f3195e6d670298eee6c5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-13 20:04:10 -0400

    Update exact-blacklist.py

--- scripts/python-uninstall/exact-blacklist.py
+++ scripts/python-uninstall/exact-blacklist.py
@@ -171,7 +171,7 @@ if db_exists:
         if (sqliteConnection):
             sqliteConnection.close()
 
-            print("[i] The connection to the Gravity database has closed.")
+            print("[i] The connection to Gravity's Database has closed.")
             time.sleep(2)
             print('\n')
             print('[i] Please wait for the Pi-hole server to restart.')
@@ -216,10 +216,15 @@ else:
         for line in sorted(blacklist_local):
             fWrite.write("{}\n".format(line))
 
+    print("[i] The connection to Gravity's Database has closed.")
+    time.sleep(2)
+    print('\n')
     print('[i] Please wait for the Pi-hole server to restart.')
     restart_pihole(args.docker)
-    print('[i] The Exact Blacklist Domains have been successfully removed from Gravity!')
+    print('\n')
+    print('[i] The exact blacklist domains have been successfully removed from Gravity!')
     print('\n')
     print('Please make sure to star this repository to show support... it helps keep me motivated!')
     print('https://github.com/slyfox1186/pihole.regex')
+    print('\n')
     print('\n')
