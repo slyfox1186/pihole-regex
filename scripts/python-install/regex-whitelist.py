93d824c6542665f1de3c4cedea24de127d12c74e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 13:08:48 -0400

    Update regex-whitelist.py

--- scripts/python-install/regex-whitelist.py
+++ scripts/python-install/regex-whitelist.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import json
 import os
 import argparse
 import sqlite3
@@ -40,7 +39,8 @@ def fetch_whitelist_url(url):
     # Return the hosts
     return response
 
-url_regex_strings_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/whitelist/regex-whitelist.txt'
+
+url_regexps_remote = 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/domains/whitelist/regex-whitelist.txt'
 install_comment = 'SlyRWL'
 
 cmd_restart = ['pihole', 'restartdns', 'reload']
@@ -49,11 +49,11 @@ db_exists = False
 conn = None
 c = None
 
-regex_strings_remote = set()
-regex_strings_local = set()
-regex_strings_slyfox1186_local = set()
-regex_strings_legacy_slyfox1186 = set()
-regex_strings_remove = set()
+regexps_remote = set()
+regexps_local = set()
+regexps_slyfox1186_local = set()
+regexps_legacy_slyfox1186 = set()
+regexps_remove = set()
 
 # Start the docker directory override
 print('[i] Checking for "pihole" docker container')
@@ -66,15 +66,15 @@ docker_mnt_src = None
 # Check to see whether the default "pihole" docker container is active
 try:
     docker_id = subprocess.run(['docker', 'ps', '--filter', 'name=pihole', '-q'],
-    stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
+                               stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
 # Exception for if docker is not installed
 except FileNotFoundError:
     pass
 
 # If a pihole docker container was found, locate the first mount
 if docker_id:
-    docker_mnt = subprocess.run(["docker", "inspect", "--format", "{{ (json .Mounts) }}", docker_id],
-    stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
+    docker_mnt = subprocess.run(['docker', 'inspect', '--format', '{{ (json .Mounts) }}', docker_id],
+                                stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
     # Convert output to JSON and iterate through each dict
     for json_dict in json.loads(docker_mnt):
         # If this mount's destination is /etc/pihole
@@ -85,11 +85,11 @@ if docker_id:
 
     # If we successfully found the mount
     if docker_mnt_src:
-        print('[i] Running in docker installation mode.')
+        print('[i] Running in docker installation mode')
         # Prepend restart commands
         cmd_restart[0:0] = ['docker', 'exec', '-i', 'pihole']
 else:
-    print('[i] Running in physical installation mode.')
+    print('[i] Running in physical installation mode ')
 
 # Set paths
 path_pihole = docker_mnt_src if docker_mnt_src else r'/etc/pihole'
@@ -99,39 +99,39 @@ path_pihole_db = os.path.join(path_pihole, 'gravity.db')
 
 # Check that pi-hole path exists
 if os.path.exists(path_pihole):
-    print('[i] Pi-hole path exists...')
+    print('[i] Pi-hole path exists')
 else:
-    print(f'[e] {path_pihole} was not found.')
+    print(f'[e] {path_pihole} was not found')
     exit(1)
 
 # Check for write access to /etc/pihole
 if os.access(path_pihole, os.X_OK | os.W_OK):
-    print(f'[i] Write access to {path_pihole} verified.')
+    print(f'[i] Write access to {path_pihole} verified')
 else:
-    print(f'[e] Write access is not available for {path_pihole}. Please run as root or other privileged user.')
+    print(f'[e] Write access is not available for {path_pihole}. Please run as root or other privileged user')
     exit(1)
 
-# Determine whether we are using database or not
+# Determine whether we are using DB or not
 if os.path.isfile(path_pihole_db) and os.path.getsize(path_pihole_db) > 0:
     db_exists = True
-    print("[i] Gravity's database was detected.")
+    print('[i] DB detected')
 else:
-    print('[i] Legacy regex.list detected.')
+    print('[i] Legacy regex.list detected')
 
-# Fetch the remote regex strings
-str_regex_strings_remote = fetch_whitelist_url(url_regex_strings_remote)
+# Fetch the remote regexps
+str_regexps_remote = fetch_whitelist_url(url_regexps_remote)
 
-# If regex strings were fetched, remove any comments and add to set
-if str_regex_strings_remote:
-    regex_strings_remote.update(x for x in map(str.strip, str_regex_strings_remote.splitlines()) if x and x[:1] != '#')
-    print(f'[i] {len(regex_strings_remote)} regex strings collected from {url_regex_strings_remote}.')
+# If regexps were fetched, remove any comments and add to set
+if str_regexps_remote:
+    regexps_remote.update(x for x in map(str.strip, str_regexps_remote.splitlines()) if x and x[:1] != '#')
+    print(f'[i] {len(regexps_remote)} regexps collected from {url_regexps_remote}')
 else:
-    print('[i] No remote regex strings were found.')
+    print('[i] No remote regexps were found.')
     exit(1)
 
 if db_exists:
-    # Create a database connection
-    print(f'[i] Connecting to {path_pihole_db}.')
+    # Create a DB connection
+    print(f'[i] Connecting to {path_pihole_db}')
 
     try:
         conn = sqlite3.connect(path_pihole_db)
@@ -142,94 +142,94 @@ if db_exists:
     # Create a cursor object
     c = conn.cursor()
 
-    # Update remote regex strings
-    print('[i] Updating regex strings in the database.')
+    # Add / update remote regexps
+    print('[i] Adding / updating regexps in the DB')
 
     c.executemany('INSERT OR IGNORE INTO domainlist (type, domain, enabled, comment) '
                   'VALUES (2, ?, 1, ?)',
-                  [(x, install_comment) for x in sorted(regex_strings_remote)])
+                  [(x, install_comment) for x in sorted(regexps_remote)])
     c.executemany('UPDATE domainlist '
                   'SET comment = ? WHERE domain in (?) AND comment != ?',
-                  [(install_comment, x, install_comment) for x in sorted(regex_strings_remote)])
+                  [(install_comment, x, install_comment) for x in sorted(regexps_remote)])
 
     conn.commit()
 
-    # Fetch all current slyfox1186 regex strings in the local db
+    # Fetch all current slyfox1186 regexps in the local db
     c.execute('SELECT domain FROM domainlist WHERE type = 2 AND comment = ?', (install_comment,))
-    regex_strings_slyfox1186_local_results = c.fetchall()
-    regex_strings_slyfox1186_local.update([x[0] for x in regex_strings_slyfox1186_local_results])
+    regexps_slyfox1186_local_results = c.fetchall()
+    regexps_slyfox1186_local.update([x[0] for x in regexps_slyfox1186_local_results])
 
     # Remove any local entries that do not exist in the remote list
     # (will only work for previous installs where we've set the comment field)
-    print('[i] Identifying obsolete regex strings.')
-    regex_strings_remove = regex_strings_slyfox1186_local.difference(regex_strings_remote)
+    print('[i] Identifying obsolete regexps')
+    regexps_remove = regexps_slyfox1186_local.difference(regexps_remote)
 
-    if regex_strings_remove:
-        print('[i] Removing obsolete regex strings.')
-        c.executemany('DELETE FROM domainlist WHERE type = 2 AND domain in (?)', [(x,) for x in regex_strings_remove])
+    if regexps_remove:
+        print('[i] Removing obsolete regexps')
+        c.executemany('DELETE FROM domainlist WHERE type = 2 AND domain in (?)', [(x,) for x in regexps_remove])
         conn.commit()
 
     # Delete slyfox1186-regex.list as if we've migrated to the db, it's no longer needed
     if os.path.exists(path_legacy_slyfox1186_regex):
         os.remove(path_legacy_slyfox1186_regex)
 
-    print('[i] Please wait for the Pi-hole server to restart.')
+    print('[i] Restarting Pi-hole')
     subprocess.run(cmd_restart, stdout=subprocess.DEVNULL)
-    print('[i] The RegEx Whitelist filters were added to Gravity.\n')
-    print('Please make sure to star this repository to show support... it helps keep me motivated!')
-    print('https://github.com/slyfox1186/pihole.regex\n')
+
+    # Prepare final result
+    print('[i] Done - Please see your installed regexps below\n')
 
     c.execute('Select domain FROM domainlist WHERE type = 2')
     final_results = c.fetchall()
-    regex_strings_local.update(x[0] for x in final_results)
+    regexps_local.update(x[0] for x in final_results)
 
-    print(*sorted(regex_strings_local), sep='\n')
+    print(*sorted(regexps_local), sep='\n')
 
     conn.close()
 
 else:
-    # If regex.list exists and is not empty read it and add to the set
+    # If regex.list exists and is not empty
+    # Read it and add to a set
     if os.path.isfile(path_legacy_regex) and os.path.getsize(path_legacy_regex) > 0:
-        print('[i] Collecting existing entries from regex.list.')
+        print('[i] Collecting existing entries from regex.list')
         with open(path_legacy_regex, 'r') as fRead:
-            regex_strings_local.update(x for x in map(str.strip, fRead) if x and x[:1] != '#')
+            regexps_local.update(x for x in map(str.strip, fRead) if x and x[:1] != '#')
 
-    # If the local regex strings set is not empty
-    if regex_strings_local:
-        print(f'[i] {len(regex_strings_local)} existing regex strings identified.')
+    # If the local regexp set is not empty
+    if regexps_local:
+        print(f'[i] {len(regexps_local)} existing regexps identified')
         # If we have a record of a previous legacy install
         if os.path.isfile(path_legacy_slyfox1186_regex) and os.path.getsize(path_legacy_slyfox1186_regex) > 0:
-            print('[i] Existing slyfox1186-regex install identified.')
-            # Read the previously installed regex strings to a set
+            print('[i] Existing slyfox1186-regex install identified')
+            # Read the previously installed regexps to a set
             with open(path_legacy_slyfox1186_regex, 'r') as fOpen:
-                regex_strings_legacy_slyfox1186.update(x for x in map(str.strip, fOpen) if x and x[:1] != '#')
+                regexps_legacy_slyfox1186.update(x for x in map(str.strip, fOpen) if x and x[:1] != '#')
 
-                if regex_strings_legacy_slyfox1186:
-                    print('[i] Removing previously installed regex strings.')
-                    regex_strings_local.difference_update(regex_strings_legacy_slyfox1186)
+                if regexps_legacy_slyfox1186:
+                    print('[i] Removing previously installed regexps')
+                    regexps_local.difference_update(regexps_legacy_slyfox1186)
 
-    # Add remote regex strings to local regex strings
-    print(f'[i] Syncing with {url_regex_strings_remote}.')
-    regex_strings_local.update(regex_strings_remote)
+    # Add remote regexps to local regexps
+    print(f'[i] Syncing with {url_regexps_remote}')
+    regexps_local.update(regexps_remote)
 
     # Output to regex.list
-    print(f'[i] Outputting {len(regex_strings_local)} regex strings to {path_legacy_regex}.')
+    print(f'[i] Outputting {len(regexps_local)} regexps to {path_legacy_regex}')
     with open(path_legacy_regex, 'w') as fWrite:
-        for line in sorted(regex_strings_local):
+        for line in sorted(regexps_local):
             fWrite.write(f'{line}\n')
 
-    # Output slyfox1186 remote regex strings to slyfox1186-regex.list for future Uninstall
+    # Output slyfox1186 remote regexps to slyfox1186-regex.list
+    # for future install / uninstall
     with open(path_legacy_slyfox1186_regex, 'w') as fWrite:
-        for line in sorted(regex_strings_remote):
+        for line in sorted(regexps_remote):
             fWrite.write(f'{line}\n')
 
-    print('[i] Please wait for the Pi-hole server to restart.')
-    restart_pihole(args.docker)
-    print('[i] The Pi-hole server is running.\n')
-    print('[i] The Exact Blacklist filters were added to Gravity!\n')
-    print('Please make sure to star this repository to show support... it helps keep me motivated!')
-    print('https://github.com/slyfox1186/pihole.regex\n')
-    
+    print('[i] Restarting Pi-hole')
+    subprocess.run(cmd_restart, stdout=subprocess.DEVNULL)
+
+    # Prepare final result
+    print('[i] Done - Please see your installed regexps below\n')
     with open(path_legacy_regex, 'r') as fOpen:
         for line in fOpen:
             print(line, end='')
