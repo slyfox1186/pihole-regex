2afdeec246a5695bd08c71f4d2b63c6155efef67
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 06:01:44 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -2,18 +2,18 @@
 
 # Get user's input
 clear
-echo -e "RegEx Whitelist Filters: [A]dd [R]emove [S]kip"
-read a
+echo -e "[i] RegEx Whitelist Filters: [A]dd [R]emove [S]kip"
+read answer
 clear
-if [[ $a == "A" ]]; then
+if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-whitelist.py' | sudo python3
     echo -e "[i] The RegEx Whitelist Filters were added to pi-hole.\\n\\n"
-elif [[ $a == "R" ]]; then
+elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-whitelist.py' | sudo python3
     echo -e "[i] The RegEx Whitelist Filters were removed from pi-hole.\\n\\n"
-elif [[ $a == "S" ]]; then
+elif [[ $answer == "S" ]]; then
     clear
-    echo -e "Skipping ahead!\\n"
+    echo -e "[i] Skipping ahead!\\n"
 fi
 
 sleep 2
