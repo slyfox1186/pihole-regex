e6b06076da62cf3e93a25cfd08272436afa5d771
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 08:43:52 -0400

    Update exact-blacklist.py

--- scripts/python-install/exact-blacklist.py
+++ scripts/python-install/exact-blacklist.py
@@ -22,11 +22,9 @@ def fetch_blacklist_url(url):
     except HTTPError as e:
         print('[X] HTTP Error:', e.code, 'whilst fetching', url)
         print('\n')
-        print('\n')
         exit(1)
     except URLError as e:
         print('[X] URL Error:', e.reason, 'whilst fetching', url)
-        print('\n')
         print('\n')
         exit(1)
 
