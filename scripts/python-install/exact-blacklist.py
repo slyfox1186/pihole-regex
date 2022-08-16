17e0c835e824bbc2db5262a2c40f5fff852cf0e3
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 12:47:45 -0400

    Update exact-blacklist.py

--- scripts/python-install/exact-blacklist.py
+++ scripts/python-install/exact-blacklist.py
@@ -170,7 +170,7 @@ if db_exists:
             time.sleep(2)
             print('[i] Please wait for the Pi-hole server to restart...\n')
             restart_pihole(args.docker)
-            print('[i] The Exact Blacklist Filters have been successfully added to Gravity!\n')
+            print('[i] The Exact Blacklist filters have been successfully added to Gravity!\n')
             print('Please make sure to star this repository to show support... it helps keep me motivated!')
             print('https://github.com/slyfox1186/pihole.regex\n')
 
@@ -206,8 +206,10 @@ else:
         for line in sorted(whitelist_local):
             fWrite.write("{}\n".format(line))
 
-    print('[i] Please wait for the Pi-hole server to restart.')
+    print("[i] The connection to the Gravity database has closed.\n")
+    time.sleep(2)
+    print('[i] Please wait for the Pi-hole server to restart...\n')
     restart_pihole(args.docker)
-    print('[i] The Exact Blacklist Filters have been successfully added to Gravity!\n')
+    print('[i] The Exact Blacklist filters have been successfully added to Gravity!\n')
     print('Please make sure to star this repository to show support... it helps keep me motivated!')
     print('https://github.com/slyfox1186/pihole.regex\n')
