5daf8a93241729cbe89b8239cc29e0c8ca33c7b5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 06:03:24 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -3,15 +3,15 @@
 # Get user's input
 clear
 echo -e "[i] Exact Blacklist Filters: [A]dd [R]emove [S]kip"
-read a
+read answer
 clear
-if [[ $a == "A" ]]; then
+if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-blacklist.py' | sudo python3
     echo -e "[i] The Exact Blacklist Filters have been added.\\n\\n"
-elif [[ $a == "R" ]]; then
+elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-blacklist.py' | sudo python3
     echo -e "[i] The Exact Blacklist Filters have been removed.\\n\\n"
-elif [[ $a == "S" ]]; then
+elif [[ $answer == "S" ]]; then
     clear
     echo -e "[i] Skipping ahead!\\n"
 fi
