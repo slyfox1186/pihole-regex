82434225e117fb83f18bbeb7528a088b143446c6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 06:11:06 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -7,13 +7,13 @@ read answer
 clear
 if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-blacklist.py' | sudo python3
-    echo -e "[i] The Exact Blacklist Filters have been added.\\n\\n"
+    echo -e "[i] The Exact Blacklist Filters have been added.\\n"
 elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-blacklist.py' | sudo python3
-    echo -e "[i] The Exact Blacklist Filters have been removed.\\n\\n"
+    echo -e "[i] The Exact Blacklist Filters have been removed.\\n"
 elif [[ $answer == "S" ]]; then
     clear
     echo -e "[i] Skipping ahead!\\n"
 fi
 
-sleep 2
+read -p 'Press Enter to continue...'
