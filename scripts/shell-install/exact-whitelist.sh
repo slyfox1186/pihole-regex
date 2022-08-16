c689d58b237aee7af8814059fe87570d94f9be6c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 06:18:14 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -2,18 +2,18 @@
 
 # Get user's input
 clear
-echo -e "[i] Exact Whitelist Filters: [A]dd [R]emove [S]kip"
+echo -e "[i] Exact Whitelist: [A]dd [R]emove [S]kip"
 read answer
 clear
 if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
-    echo -e "[i] The Exact Whitelist Filters have been added.\\n\\n"
+    echo -e "[i] The [Exact Whitelist] filters were added to Pi-hole\\n"
 elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
-    echo -e "[i] The Exact Whitelist Filters have been removed.\\n\\n"
+    echo -e "[i] The [Exact Whitelist] filters were removed from Pi-hole\\n"
 elif [[ $answer == "S" ]]; then
     clear
     echo -e "[i] Skipping ahead!\\n"
 fi
 
-sleep 2
+read -p 'Press Enter to continue...'
