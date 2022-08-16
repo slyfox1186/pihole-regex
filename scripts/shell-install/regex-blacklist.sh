6b8f03a89bea9352e6fe721ff2ac0f7b75808b34
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 06:19:32 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -2,18 +2,18 @@
 
 # Get user's input
 clear
-echo -e "[i] RegEx Blacklist Filters: [A]dd [R]emove [S]kip"
+echo -e "[i] RegEx Blacklist: [A]dd [R]emove [S]kip"
 read answer
 clear
 if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-    echo -e "[i] The RegEx Blacklist Filters were added to pi-hole.\\n"
+    echo -e "[i] The [RegEx Blacklist] filters were added to pi-hole\\n"
 elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-    echo -e "[i] The RegEx Blacklist Filters were removed from pi-hole.\\n"
+    echo -e "[i] The [RegEx Blacklist] filters were removed from Pi-hole\\n"
 elif [[ $answer == "S" ]]; then
     clear
-    echo -e "[i] Skipping ahead!\\n"
+    echo -e "[i] Finishing up!\\n"
 fi
 
-read -p 'Press Enter to continue...'
+read -t 3 -p "Press Enter to exit..."
