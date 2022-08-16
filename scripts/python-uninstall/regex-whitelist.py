e6a6bff22a575b503af540810ecf793f5209a983
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 11:50:18 -0400

    Update regex-whitelist.py

--- scripts/python-uninstall/regex-whitelist.py
+++ scripts/python-uninstall/regex-whitelist.py
@@ -14,7 +14,7 @@ def fetch_url(url):
     if not url:
         return
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'}
 
     print('[i] Fetching:', url)
 
