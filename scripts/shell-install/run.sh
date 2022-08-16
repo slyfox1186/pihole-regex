9bf9acaafa20e5d683b7b4fe85279e60768d47bb
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 08:33:59 -0400

    Update run.sh

--- scripts/shell-install/run.sh
+++ scripts/shell-install/run.sh
@@ -20,21 +20,19 @@ else
     mkdir -p "$FILE_DIR"
 fi
 
-# Verify: If files exist move them to /root/pihole.regex
+# If files exist move them to /root/pihole.regex
 SHELL_FILES='exact-blacklist.sh exact-whitelist.sh regex-blacklist.sh regex-whitelist.sh'
-FILES=($SHELL_FILES run.sh)
-
+FILES=( $SHELL_FILES run.sh )
+# move all the scripts in the array FILES to /root/pihole.regex
 for i in ${FILES[@]}; do
-  if [ -f /root/$i ]; then
-      mv -f "/root/$i" "$FILE_DIR/$i"
+  if [ -f $i ]; then
+      mv -f "$i" "$FILE_DIR/$i"
+      clear
   fi
 done
 
-sleep 1
-clear
-
-SUB_FILES=($SHELL_FILES)
-
+# execute all scripts in array
+SUB_FILES=( $SHELL_FILES )
 for i in ${SUB_FILES[@]}; do
     . "$FILE_DIR/$i"
 done
