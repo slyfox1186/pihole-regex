0309270559948e77b7b6aee5a2753d4d1fd77526
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-16 12:38:18 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -7,10 +7,10 @@ read answer
 clear
 if [[ $answer == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-    echo -e "\\n[i] The RegEx Blacklist filters were added to Pi-hole\\n"
+    echo -e "[i] The RegEx Blacklist filters were added to Pi-hole\\n"
 elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-    echo -e "\\n[i] The RegEx Blacklist filters were removed from Pi-hole\\n"
+    echo -e "[i] The RegEx Blacklist filters were removed from Pi-hole\\n"
 elif [[ $answer == "S" ]]; then
     clear
     echo -e "[i] Skipping ahead!\\n"
