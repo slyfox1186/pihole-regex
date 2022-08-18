96d3b87ebb40ae9be346b71454457580314782e1
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-18 06:17:22 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -6,9 +6,9 @@ echo '[i] Exact Whitelist: [A]dd [R]emove [S]kip'
 read answer
 clear
 if [[ $answer == "A" ]]; then
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
 elif [[ $answer == "R" ]]; then
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole.regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
+    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
 elif [[ $answer == "S" ]]; then
     clear
     echo -e "[i] Skipping ahead!\\n"
