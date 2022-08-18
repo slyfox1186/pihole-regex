1d3b045e683dc184bfd33fa99cb95cd8c87f8093
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-18 06:52:21 -0400

    Update exact-whitelist.py

--- scripts/python-install/exact-whitelist.py
+++ scripts/python-install/exact-whitelist.py
@@ -170,9 +170,9 @@ if db_exists:
             newWL = removeBraces10.split(', ') # Split at commas to create a list
             newWhiteList[nwl] = newWL[1].replace('\'', '') # Remove ' from domain and add to list
             # Uncomment to see list of sql varables being imported
-            print(nW[nwl])
+            # print(nW[nwl])
             # Uncomment to see list of domains being imported
-            print(newWhiteList[nwl])
+            # print(newWhiteList[nwl])
             nwl += 1 # count + 1
         # Check database for user added exact whitelisted domains
         print('[i] Checking Gravity for domains added by a user that are also in the script.')
