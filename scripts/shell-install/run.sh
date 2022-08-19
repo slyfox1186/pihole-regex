13a57bd2a2f3eecd0cc88eb361c66e383cb20639
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 08:38:22 -0400

    Update run.sh

--- scripts/shell-install/run.sh
+++ scripts/shell-install/run.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
-# Make user input case insensitive
-clear; shopt -s nocasematch
+# make user input case insensitive
+shopt -s nocasematch
 
-# Delete: The useless index.html file
-if [ -f index.html ]; then
-    rm index.html
-fi
+# delete the useless index.html file that is downloaded
+if [ -f index.html ]; then rm index.html; fi
 
-# Create: pihole-regex directory if not exist
-if [ -d pihole-regex ]; then
-    rm -R pihole-regex
-fi
+# Delete & create pihole-regex directory if not exist
+if [ -d pihole-regex ]; then rm -R pihole-regex; fi
 
-# Make pihole-regex directory
+# make pihole-regex directory
 mkdir -p pihole-regex
 
-# If files exist move them to the pihole-regex directory
 SHELL_FILES='exact-blacklist.sh exact-whitelist.sh regex-blacklist.sh regex-whitelist.sh'
 FILES=( $SHELL_FILES run.sh )
-# move all the scripts in the array FILES to the pihole-regex directory
+# if the shell scripts exist, move them to the
+# pihole-regex dir
 for i in ${FILES[@]}; do
     if [ -f $i ]; then
         mv -f $i pihole-regex/$i
-        clear
     fi
     if [ -f pihole-regex/run.sh ]; then
         rm pihole-regex/run.sh
-        clear
     fi
 done
 
-# change working directory to pihole-regex
-cd pihole-regex
-
-# execute all scripts
+# execute all scripts in pihole-regex
 SUB_FILES=( $SHELL_FILES )
 for i in ${SUB_FILES[@]}; do
-    . $i
+    . pihole-regex/$i
 done
-
-# return user to starting directory
-cd ..
