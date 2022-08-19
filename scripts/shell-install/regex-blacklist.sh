d7767da2465a3491f90837bc27c17b6cd8a44c3b
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 08:19:04 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -11,7 +11,7 @@ elif [[ $i == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
 elif [[ $i == "S" ]]; then
     clear
-    echo '[i] Skipping ahead!'
+    echo '[i] Loading: RegEx Whitelist'
     sleep 1
     . regex-whitelist.sh
 fi
