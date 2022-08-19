c2401b667cdeaa0f64ecb0748d2fc5800f833564
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 11:59:46 -0400

    Update exact-whitelist.py

--- scripts/python-uninstall/exact-whitelist.py
+++ scripts/python-uninstall/exact-whitelist.py
@@ -142,12 +142,12 @@ if db_exists:
         sqliteConnection = sqlite3.connect(gravity_db_location)
         cursor = sqliteConnection.cursor()
         print("[i] Successfully connected to Gravity's database!")
-        total_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 0 AND comment LIKE '%SlyEBL - github.com/slyfox1186/pihole-regex%' ")
+        total_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 0 AND comment LIKE '%SlyEWL - github.com/slyfox1186/pihole-regex%' ")
 
         totalDomains = len(total_domains.fetchall())
         print("[i] There are a total of {} domains in your whitelist which were added by this script." .format(totalDomains))
         print('[i] Removing domains in the Gravity database...')
-        cursor.execute (" DELETE FROM domainlist WHERE type = 0 AND comment LIKE '%SlyEBL - github.com/slyfox1186/pihole-regex%' ")
+        cursor.execute (" DELETE FROM domainlist WHERE type = 0 AND comment LIKE '%SlyEWL - github.com/slyfox1186/pihole-regex%' ")
 
         sqliteConnection.commit()
 
