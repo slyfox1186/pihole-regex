8880a530db1fb9e14fc8d025155405fb9f027da3
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 14:24:05 -0400

    Update replaced.sql

--- domains/whitelist/replaced.sql
+++ domains/whitelist/replaced.sql
@@ -8,11 +8,15 @@
 (0, '5-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, '6-edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
 (0, 'edge-chat.facebook.com', 1, 'Facebook - SlyEWL')
+
 (0, 'device.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
-(0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
-(0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
 (0, 'title.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
 (0, 'xsts.auth.xboxlive.com', 1, 'Microsoft Xbox - Used for Xbox updates, game downloads, acheivements - SlyEWL')
+
+(0, 'kosmos-assets-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
+(0, 'kosmos-prod.react.gammacdn.com', 1, 'AdultTime - Required for react buttons to work - SlyEWL')
+(0, 'static01-cms-fame.gammacdn.com', 1, 'AdultTime - A required domain of the site to access image, videos, etc - SlyEWL')
+(0, 'static03-cms-fame.gammacdn.com', 1, 'AdultTime - A required domain of the site to access image, videos, etc - SlyEWL')
 
 // RegEx Strings replaced
 ^kosmos(|-assets)-prod\.react\.gammacdn\.com$
