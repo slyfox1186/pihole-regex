d8998d587fe32ac3fcc6ff448dbe5f6810993a92
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 09:41:01 -0400

    Update regex-blacklist.py

--- scripts/python-uninstall/regex-blacklist.py
+++ scripts/python-uninstall/regex-blacklist.py
@@ -55,6 +55,7 @@ regexps_slyfox1186_local = set()
 regexps_legacy_slyfox1186 = set()
 regexps_remove = set()
 
+# Inform user of script's intent
 os.system('clear')
 print('If you are using Pi-hole v5.0 or later, this script will only remove domains that were added by itself.')
 print('Any other domains added personally by the user will remain uneffected.')
