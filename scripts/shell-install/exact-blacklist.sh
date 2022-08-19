4fdf71737a171011ebd5462d1b8fb055d7608869
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 08:12:10 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -11,9 +11,9 @@ elif [[ $answer == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | sudo python3
 elif [[ $answer == "S" ]]; then
     clear
-    echo -e "[i] Skipping ahead!\\n"
+    echo '[i] Skipping ahead!'
     sleep 1
     . exact-whitelist.sh
 fi
 
-read -t 30 -p 'Press Enter to continue...'
+read -t 30 -p 'Press [Enter] to continue...'
