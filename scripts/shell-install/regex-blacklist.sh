078e119a83e26e5a949c8db79262e50042a1c4b4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-19 08:11:42 -0400

    Update regex-blacklist.sh

--- scripts/shell-install/regex-blacklist.sh
+++ scripts/shell-install/regex-blacklist.sh
@@ -3,18 +3,17 @@
 # Get user's input
 clear
 echo 'RegEx Blacklist: [A]dd [R]emove [S]kip'
-read answer
+read i
 clear
-if [[ $answer == "A" ]]; then
+if [[ $i == "A" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/regex-blacklist.py' | sudo python3
-elif [[ $answer == "R" ]]; then
+elif [[ $i == "R" ]]; then
     /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/regex-blacklist.py' | sudo python3
-elif [[ $answer == "S" ]]; then
+elif [[ $i == "S" ]]; then
     clear
-    echo -e "[i] Skipping ahead!\\n"
-    sleep 2
+    echo '[i] Skipping ahead!'
+    sleep 1
     . regex-whitelist.sh
-    exit 0
 fi
 
 read -t 30 -p 'Press Enter to continue...'
