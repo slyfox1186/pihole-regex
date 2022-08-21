e72d8e2622c5247faf4e7c3361d1b51d58705cb5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-21 02:24:06 -0400

    Update my-regex-blacklist.sh

--- scripts/shell-install/my-regex-blacklist.sh
+++ scripts/shell-install/my-regex-blacklist.sh
@@ -9,10 +9,10 @@ echo 'My RegEx Blacklist: [A]dd [R]emove [S]kip'
 read i
 clear
 if [[ $i == "A" ]]; then
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/my-regex-blacklist.py' | sudo python3
+    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/my-regex-blacklist.py' | sudo python3
 else
     [[ $i == "R" ]]; then
-    /usr/bin/curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/my-regex-blacklist.py' | sudo python3
+    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/my-regex-blacklist.py' | sudo python3
 fi
 
 echo -e "\\n"
