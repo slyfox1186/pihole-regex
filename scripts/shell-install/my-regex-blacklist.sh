db4598ac710d1f87186672ccfeee80c4a9ab00ea
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-21 02:39:51 -0400

    Update my-regex-blacklist.sh

--- scripts/shell-install/my-regex-blacklist.sh
+++ scripts/shell-install/my-regex-blacklist.sh
@@ -5,14 +5,17 @@ shopt -s nocasematch
 
 # Get user's input
 clear
-echo 'My RegEx Blacklist: [A]dd [R]emove [S]kip'
+echo 'My RegEx Blacklist: [A]dd [R]emove'
 read i
 clear
 if [[ $i == "A" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/my-regex-blacklist.py' | sudo python3
-else
-    [[ $i == "R" ]]; then
+elif [[ $i == "R" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/my-regex-blacklist.py' | sudo python3
+elif [[ ! $i == "A" ]] || [[ ! $i == "R" ]]; then
+    echo -e "You didn't input A or R...\\nPlease try again.\\n"
+    read -t 3 -p 'Press Enter to continue.'
+    bash "$0"
 fi
 
 echo -e "\\n"
