005fef31bf14850de7cb03b5730b2346e24aaeb3
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-26 21:56:33 -0400

    Update exact-whitelist.sh

--- scripts/shell-install/exact-whitelist.sh
+++ scripts/shell-install/exact-whitelist.sh
@@ -9,9 +9,12 @@ echo 'Exact Whitelist: [A]dd [R]emove [S]kip'
 read i
 clear
 if [[ $i == "A" ]]; then
-    curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
+    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-whitelist.py' | sudo python3
 elif [[ $i == "R" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-whitelist.py' | sudo python3
+elif [[ $i == "S" ]]; then
+    bash regex-blacklist.sh
+    exit
 fi
 
 echo
