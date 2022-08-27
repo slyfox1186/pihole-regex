bc735b07f7dde9d509156881dcffa4c0aaf484ba
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-26 21:56:26 -0400

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -21,5 +21,4 @@ echo
 echo 'Make sure to star this repository and show your support!'
 echo 'Github Profile: https://github.com/slyfox1186/pihole-regex'
 echo
-echo
 reap -p "Press Enter to restart Pi-hole's FTLDNS"
