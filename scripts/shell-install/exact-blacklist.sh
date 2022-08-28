451121e44e8c866400e97ba458be0996aa555300
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-28 13:44:05 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -8,12 +8,12 @@ clear
 echo 'Exact Blacklist: [A]dd [R]emove [S]kip'
 read i
 clear
-if [[ $i == "A" ]]; then
+if [[ "$i" == "A" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-blacklist.py' | sudo python3
-elif [[ $i == "R" ]]; then
+elif [[ "$i" == "R" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | sudo python3
-elif [[ $i == "S" ]]; then
-    bash exact-whitelist.sh
+elif [[ "$i" == "S" ]]; then
+    bash pihole-regex/exact-whitelist.sh
     exit
 fi
 
