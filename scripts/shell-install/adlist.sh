b6a762d5025f2e2ff6e8fac270265be73e441aa8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-10 05:45:08 -0400

    Update adlist.sh

--- scripts/shell-install/adlist.sh
+++ scripts/shell-install/adlist.sh
@@ -8,7 +8,7 @@
 shopt -s nocasematch
 
 # Change this to the full path of gravity's database if the one below is wrong
-GRAVITY='/etc/pihole/gravity.db'
+DB_FILE='/etc/pihole/gravity.db'
 
 # Display the script's purpose in terminal
 clear
@@ -24,7 +24,7 @@ if [[ "$a" == "1" ]]; then
     echo 'Continuing'
     clear
 elif [[ "$a" == "2" ]]; then
-    sqlite3 "$GRAVITY" "DELETE FROM adlist"
+    sqlite3 "$DB_FILE" "DELETE FROM adlist"
     clear
     echo -e "All domains have been removed from Pi-hole's adlists\\n"
     exit
@@ -39,48 +39,45 @@ else
     exit 1
 fi
 
+URL1='https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/adlist/adlists.txt'
+COMMENT1='SlyFox1186 - Firebog + Other'
+URL2='https://v.firebog.net/hosts/lists.php?type=tick'
+COMMENT2='Firebog - Ticked'
+URL3='https://v.firebog.net/hosts/lists.php?type=nocross'
+COMMENT3='Firebog - Non-crossed'
+URL4='https://v.firebog.net/hosts/lists.php?type=all'
+COMMENT4='Firebog - All'
+# SET OUTPUT FILE LOCATION
+AD_TMP='/tmp/adlist.txt'
+
 # Prompt the user with Adlist option 2
 echo -e "Choose which adlist to import into Pi-hole\\n"
-echo '[1] Firebog: Ticked'
-echo '[2] Firebog: Ticked + Non-Crossed'
-echo '[3] Firebog: All'
-echo '[4] SlyFox1186'
+echo "[1] SlyFox1186 (Recommended. Sly's personal lists with \"Firebog's - Ticked + Non-Crossed\")"
+echo '[2] Firebog: Ticked (Recommended for builds with little oversight.)'
+echo '[3] Firebog: Ticked + Non-Crossed (Similar to the "Ticked List" but may need more attention.)'
+echo '[4] Firebog: All (Not recommended. False positives abound!)'
 read b
 clear
 if [[ "$b" == "1" ]]; then
-    URL1='https://v.firebog.net/hosts/lists.php?type=tick'
-    COMMENT1='Firebog - Ticked'
-    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qNO - "$URL1" |
-    sed '/^#/ d' | sed '/^$/ d' > '/tmp/adlist.txt'
-    cat '/tmp/adlist.txt' |
-    xargs -n1 -I {} sqlite3 "$GRAVITY" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT1')"
+    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qO - "$URL1" |
+    sed '/^#/ d' | sed '/^$/ d' > "$AD_TMP"
+    cat "$AD_TMP" |
+    xargs -n1 -I {} sqlite3 "$DB_FILE" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT1')"
 elif [[ "$b" == "2" ]]; then
-    URL1='https://v.firebog.net/hosts/lists.php?type=tick'
-    COMMENT1='Firebog - Ticked'
-    URL2='https://v.firebog.net/hosts/lists.php?type=nocross'
-    COMMENT2='Firebog - Non-crossed'
-    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qNO - "$URL1" |
-    sed '/^#/ d' | sed '/^$/ d' > '/tmp/adlist.txt'
-    cat '/tmp/adlist.txt' |
-    xargs -n1 -I {} sqlite3 "$GRAVITY" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT1')"
-    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qNO - "$URL2" |
-    sed '/^#/ d' | sed '/^$/ d' > '/tmp/adlist.txt'
-    cat '/tmp/adlist.txt' |
-    xargs -n1 -I {} sqlite3 "$GRAVITY" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT2')"
+    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qO - "$URL2" |
+    sed '/^#/ d' | sed '/^$/ d' > "$AD_TMP"
+    cat "$AD_TMP" |
+    xargs -n1 -I {} sqlite3 "$DB_FILE" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT2')"
 elif [[ "$b" == "3" ]]; then
-    URL3='https://v.firebog.net/hosts/lists.php?type=all'
-    COMMENT3='Firebog - All'
-    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qNO - "$URL3" |
-    sed '/^#/ d' | sed '/^$/ d' > '/tmp/adlist.txt'
-    cat '/tmp/adlist.txt' |
-    xargs -n1 -I {} sqlite3 "$GRAVITY" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT3')"
+    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qO - "$URL3" |
+    sed '/^#/ d' | sed '/^$/ d' > "$AD_TMP"
+    cat "$AD_TMP" |
+    xargs -n1 -I {} sqlite3 "$DB_FILE" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT3')"
 elif [[ "$b" == "4" ]]; then
-    URL4='https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/adlist/adlists.txt'
-    COMMENT4='SlyFox1186 - Firebog + Other'
-    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qNO - "$URL4" |
-    sed '/^#/ d' | sed '/^$/ d' > '/tmp/adlist.txt'
-    cat '/tmp/adlist.txt' |
-    xargs -n1 -I {} sqlite3 "$GRAVITY" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT4')"
+    wget --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0' -qO - "$URL4" |
+    sed '/^#/ d' | sed '/^$/ d' > "$AD_TMP"
+    cat "$AD_TMP" |
+    xargs -n1 -I {} sqlite3 "$DB_FILE" "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}','$COMMENT4')"
 else
     echo -e "Warning: Bad user input...\\n"
     read -p 'Press Enter to start over.'
@@ -102,3 +99,7 @@ if [[ "$c" == "1" ]]; then
 else
     exit
 fi
+
+if [ if "$AD_TMP" ]; then rm "$AD_TMP"; fi
+
+UNSET AD_TMP COMMENT1 COMMENT2 COMMENT3 COMMENT4 DB_FILE URL1 URL2 URL3 URL4
