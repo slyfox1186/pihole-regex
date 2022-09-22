8e353df1edbb9c85058d07edf4ee18d7ed5bb607
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-09-22 01:51:49 -0400

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -12,7 +12,7 @@ if [[ "$iChoice" == "A" ]]; then
 elif [[ "$iChoice" == "R" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | sudo python3
 elif [[ "$iChoice" == "S" ]]; then
-    bash pihole-regex/exact-whitelist.sh
+    source 'pihole-regex/exact-whitelist.sh'
     exit
 fi
 
