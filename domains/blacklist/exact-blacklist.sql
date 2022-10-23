9ee131c71d499244e416b35a4681fd76f59a13d8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-10-23 02:00:31 -0400

    Update exact-blacklist.sql

--- domains/blacklist/exact-blacklist.sql
+++ domains/blacklist/exact-blacklist.sql
@@ -58,7 +58,6 @@
 (1, 'cdn.smartclip.net', 1, 'Other - SlyEBL')
 (1, 'cdns-content.dzcdn.net', 1, 'Other - SlyEBL')
 (1, 'cert-test.sandbox.google.com', 1, 'Other - SlyEBL')
-(1, 'client-rapi.recombee.us', 1, 'AdultTime - SlyEBL')
 (1, 'cloud.arqiva.tv', 1, 'Samsung - SlyEBL')
 (1, 'collect-eu-fy2014.myhomescreen.tv', 1, 'Panasonic - SlyEBL')
 (1, 'collect-eu-fy2015.myhomescreen.tv', 1, 'Panasonic - SlyEBL')
