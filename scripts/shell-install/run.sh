1e9eb99951e1699fdbf58618ca7ff3d96d4c0e10
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-12-02 16:17:02 -0500

    Update run.sh

--- scripts/shell-install/run.sh
+++ scripts/shell-install/run.sh
@@ -3,29 +3,25 @@
 clear
 
 # Delete any useless files that get downloaded.
-if [ -f 'index.html' ]; then rm 'index.html'; fi
-if [ -f 'urls.txt' ]; then rm  'urls.txt'; fi
+if [ -f index.html ] || [ -f urls.txt ]; then rm index.html urls.txt; fi
 
 # Delete the pihole-regex folder if it already exists.
-if [ -d 'pihole-regex' ]; then rm -R 'pihole-regex'; fi
+if [ -d pihole-regex ]; then rm -R pihole-regex; fi
 
 # Create the pihole-regex folder to store the downloaded files in.
-mkdir -p 'pihole-regex'
+mkdir -p pihole-regex
 
 # define variables
 FILES='exact-blacklist.sh exact-whitelist.sh regex-blacklist.sh regex-whitelist.sh'
-ADD_FILE=( ${FILES} run.sh )
-
+ADD_FILE=( $FILES run.sh )
 # If the shell scripts exist, move them to the pihole-regex dir
-for i in "${ADD_FILE[@]}"
+for i in ${ADD_FILE[@]}
 do
-    if [ -f "${i}" ]; then
-        mv -f "${i}" 'pihole-regex'
-    fi
+    if [ -f "$i" ]; then mv -f $i pihole-regex; fi
 done
 
 # execute all scripts in pihole-regex
-for i in "${FILES[@]}"
+for i in ${FILES[@]}
 do
-    source pihole-regex/${i}
+    source pihole-regex/$i
 done
