7ca2b31abced2ee2e52736ecfe6c0bba6ef25f65
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-12-02 17:10:47 -0500

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -5,29 +5,26 @@ shopt -s nocasematch
 
 # Get user's input
 clear
-read -p 'RegEx Whitelist >> [A]dd [R]emove [S]kip: ' iChoice
+read -pr 'RegEx Whitelist >> [A]dd [R]emove [S]kip: ' iChoice
 clear
 if [[ "$iChoice" == "A" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/regex-whitelist.py' | sudo python3
 elif [[ "$iChoice" == "R" ]]; then
     curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/regex-whitelist.py' | sudo python3
 elif [[ "$iChoice" == "S" ]]; then
-    echo 'Next'
+    clear
 fi
 
 # remove all remaining files that were downloaded by wget
 if [ -d pihole-regex ]; then rm -R pihole-regex; fi
 
 echo -e "\\n"
-read -p 'Press Enter to Continue'
+read -pr 'Press enter to continue.'
 clear
-echo "Restart Pihole's DNS?"
-echo
-echo '[Y]es'
-echo '[N]o'
-read uChoice
+echo -e "Restart Pihole's DNS?\\n"
+read -pr '[Y]es or [N]o' uChoice
 clear
 if [[ "$uChoice" == "Y" ]]; then sudo pihole restartdns; fi
 
 echo -e "\\nMake sure to star this repository and show your support!"
-echo -e "Github Profile: https://github.com/slyfox1186/pihole-regex\\n"
+echo -e "GitHub Repo:  https://github.com/slyfox1186/pihole-regex\\n"
