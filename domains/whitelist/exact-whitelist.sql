3992749bbe8aa18f96111988f2609b3590c88da4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-02-02 18:41:30 -0500

    Update exact-whitelist.sql

--- domains/whitelist/exact-whitelist.sql
+++ domains/whitelist/exact-whitelist.sql
@@ -1,4 +1,4 @@
-((0, 'gateway.cname.ustclug.org', 1, 'Crucial CDN server - SlyEWL')
+(0, 'gateway.cname.ustclug.org', 1, 'Crucial CDN server - SlyEWL')
 (0, '79423.analytics.edgekey.net', 1, 'REHL online learning, training embedded videos to work properly - SlyEWL')
 (0, 'android.clients.google.com', 1, 'Google Play - SlyEWL')
 (0, 'api.flightproxy.teams.microsoft.com', 1, 'Microsoft - SlyEWL')
