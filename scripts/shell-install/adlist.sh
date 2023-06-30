f253b6dc86bed2501abffc68b665f33c96c75afa
Jeff <jhollis.ga@gmail.com>
2023-06-30 17:38:44 -0500

    Update adlist.sh

--- scripts/shell-install/adlist.sh
+++ scripts/shell-install/adlist.sh
@@ -193,7 +193,7 @@ case "$choice2" in
 esac
 
 # REMOVE TEMPORARY FILES CREATED BY THE SCRIPT
-sudo rm "$fb_adlist" "$sly_adlist"
+sudo rm "$fb_adlist" "$sly_adlist" "$0"
 
 # PROMPT THE USER TO UPDATE GRAVITY'S DATABASE
 gravity_fn
