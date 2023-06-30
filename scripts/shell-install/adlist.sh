23b7d2f3d4f1803b84e939fdecfe3429574c707f
Jeff <jhollis.ga@gmail.com>
2023-06-30 14:31:51 -0500

    Update adlist.sh

--- scripts/shell-install/adlist.sh
+++ scripts/shell-install/adlist.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2162
 
-clear
-
 ################################################################################################################################
 ##
 ## Purpose: Add or remove Adlists from Pi-hole's database.
@@ -13,21 +11,23 @@ clear
 ##
 ################################################################################################################################
 
-if ! which 'sqlite3' &>/dev/null; then
-    echo 'sqlite3 must be installed to run this script.'
-    echo
-    exit 1
-fi
+clear
 
-# Delete any extra files that were downloaded alongside the other scripts that have no uses.
-if [ -f 'index.html' ] || [ -f 'urls.txt' ]; then
-    rm 'index.html' 'urls.txt'
-fi
-
-# Make user input case insensitive
+# MAKE USER INPUT CASE INSENSITIVE
 shopt -s nocasematch
 
-# Set a failure function
+# INSTALL SQLITE3 IF MISSING
+if ! which sqlite3 &>/dev/null; then
+    sudo apt -y install sqlite3
+    clear
+fi
+
+# DELETE ANY USELESS FILES THAT WERE DOWNLOADED ALONGSIDE THE OTHER SCRIPTS
+if [ -f index.html ] || [ -f urls.txt ]; then
+    sudo rm index.html urls.txt 2>/dev/null
+fi
+
+# FAILURE FUNCTION
 fail_fn()
 {
     printf "\n%s\n\n%s\n%s\n\n" \
@@ -37,8 +37,8 @@ fail_fn()
     exit 1
 }
 
-# Set the exit script function
-fn_done()
+# EXIT FUNCTION
+exit_fn()
 {
     printf "\n%s\n\n%s\n%s\n\n" \
         '[i] The script has completed.' \
@@ -47,97 +47,113 @@ fn_done()
     exit 0
 }
 
-# Set the function to restart pihole's dns
-fn_dns()
+# SET THE FUNCTION TO RESTART PIHOLE'S DNS
+dns_fn()
 {
-    local dns_choice
+    local choice
+
     printf "\n%s\n\n%s\n%s\n\n" \
         'Would you like to restart Pi-hole'\''s DNS resolver? (Recommended)' \
         '[1] Yes' \
         '[2] No'
-    read -p 'Enter a number: ' dns_choice
+    read -p 'Enter a number: ' choice
+
     clear
-    if [ "$dns_choice" -eq '1' ]; then
-        sudo pihole restartdns
-    fi
+
+    case "$choice" in
+        1)      sudo pihole restartdns;;
+        2)      echo;;
+        *)
+                unset choice
+                clear
+                gravity_fn
+                ;;
+    esac
 }
 
 # Set the function to prompt the user to update Gravity's database
-fn_gravity()
+gravity_fn()
 {
-    local gravity_choice
+    local choice
+
     printf "\n%s\n\n%s\n%s\n\n" \
-        'Would you like to update Gravity'\''s database? (Recommended to enable any changes made)' \
+        'Would you like to update Gravity'\''s database? (Recommended)' \
         '[1] Yes' \
         '[2] No'
-    read -p 'Your choices are (1 or 2): ' gravity_choice
+    read -p 'Your choices are (1 or 2): ' choice
+
     clear
-    if [ "$gravity_choice" -eq '1' ]; then
-        sudo pihole -g
-    fi
+
+    case "$choice" in
+        1)      sudo pihole -g;;
+        2)      echo;;
+        *)
+                unset choice
+                clear
+                gravity_fn
+                ;;
+    esac
 }
 
-# Set adlist url variable
+# SET ADLIST URL VARIABLE
 ad_url='https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/adlist/adlists.txt'
-# Set the comments
+
+# SET THE COMMENTS
 c1='SlyADL - SlyFox1186 + Firebog'\''s safe list'
 c2='SlyADL - Firebog - Ticked'
 c3='SlyADL - Firebog - Non-Crossed'
 c4='SlyADL - Firebog - All'
-user_agent="--user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'"
-list='/tmp/adlist.txt'
-url_base='https://v.firebog.net/hosts/lists.php?type'
-gravity="$(sudo find /etc -type f -name gravity.db)"
 
-# If necessary change the value of the GRAVITY variable to the full path of the 'gravity.db' file
-if [ -x "$gravity" ]; then
-    fail_fn 'Unable to find the full path of the "gravity.db" file in the /etc folder.'
+# SET ADDITIONAL VARS
+user_agent='--user-agent='\''Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'\'''
+list=/tmp/adlist.txt
+url_base='https://v.firebog.net/hosts/lists.php?type'
+gravity="$(sudo find /etc/pihole -type f -name gravity.db)"
+
+# IF NECESSARY CHANGE THE VALUE OF THE GRAVITY VARIABLE TO THE FULL PATH OF THE 'GRAVITY.DB' FILE
+if [ -z "$gravity" ]; then
+    fail_fn "Unable to find the database file: $gravity"
 fi
 
-# Prompt the user with choice 1
+# PROMPT THE USER TO MODIFY THE PIHOLE DATABASE
 printf "%s\n\n%s\n\n%s\n%s\n%s\n\n" \
     'Modify the Pi-hole Adlist Group' \
     'Enter one of the selections.' \
     '[1] Add domains' \
     '[2] Remove all domains (This should only delete lists added by this script)' \
     '[3] Exit'
-read -p 'Your choices are (1 to 3): ' choice_1
 
-case "$choice_1" in
-    1)
-        clear
-        ;;
+read -p 'Your choices are (1 to 3): ' choice1
+clear
+
+case "$choice1" in
+    1)          clear;;
     2)
-        if sudo sqlite3 "$gravity" "DELETE FROM adlist WHERE comment LIKE '%SlyADL%'"; then
-            clear
-            echo 'Success: All adlists have been removed from Pi-hole.'
-            fn_gravity
-            fn_dns
-            fn_done
-        else
-            fail_fn 'Unable to delete the adlists from Gravity'\''s database.'
-        fi
-        ;;
-    3)
-        fn_done
-        ;;
-    *)
-        clear
-        fail_fn 'Bad user input.'
-        ;;
+                if sudo sqlite3 "$gravity" "DELETE FROM adlist WHERE comment LIKE '%SlyADL%'"; then
+                    clear
+                    echo 'Success: All adlists have been removed from Pi-hole.'
+                    gravity_fn
+                    dns_fn
+                    exit_fn
+                else
+                    fail_fn 'Unable to delete the adlists from Gravity'\''s database.'
+                fi
+                ;;
+    3)          exit_fn;;
+    *)          fail_fn 'Bad user input.';;
 esac
 
-# Prompt the user with choice 2
+# PROMPT THE USER TO SELECT THE ADLIST
 printf "%s\n\n%s\n%s\n%s\n%s\n\n" \
     'Choose from the adlists below to insert their contents into the Gravity database.' \
     '[1] SlyFox1186: [Personal Adlist] - Self made with lists from the good work of others. (Includes: Firebog Ticked).' \
     '[2] Firebog:    [Ticked] - Perfect for system admins with little time available to fix database issues.' \
     '[3] Firebog:    [Non-Crossed] - These domains are generally less safe than the "Tick" list and most likely have an increased risk of false positives.' \
     '[4] Firebog:    [All] - False positives are very likely and will required much more effort than the average system admin would wish to spend fixing a database.'
-read -p 'Your choices are (1 to 4): ' choice_2
+read -p 'Your choices are (1 to 4): ' choice2
 clear
 
-case "$choice_2" in
+case "$choice2" in
     1)
         wget "$user_agent" -qO - "$ad_url" |
         sudo sed '/^#/ d' | sudo sed '/^$/ d' 2>/dev/null > "$list"
@@ -163,12 +179,14 @@ case "$choice_2" in
         "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c4\")"
         ;;
     *)
-        fail_fn 'Bad user input.'
+        fail_fn 'Bad user input.';;
 esac
 
-# Prompt the user to update Gravity's database
-fn_gravity
-# Prompt the user to restart Pi-hole's DNS
-fn_dns
-# Show exit message
-fn_done
+# PROMPT THE USER TO UPDATE GRAVITY'S DATABASE
+gravity_fn
+
+# PROMPT THE USER TO RESTART PI-HOLE'S DNS
+dns_fn
+
+# SHOW EXIT MESSAGE
+exit_fn
