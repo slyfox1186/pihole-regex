761f5318c8aef315e3da7c36026fff255c1751e6
Jeff <jhollis.ga@gmail.com>
2023-06-30 18:24:41 -0500

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# make user input case insensitive
-shopt -s nocasematch
+clear
 
-# Get user's input
+# GET THE USER'S INPUT
+read -p 'Exact Blacklist >> [A]dd [R]emove [S]kip: ' choice
 clear
-read -p 'Exact Blacklist >> [A]dd [R]emove [S]kip: ' iChoice
-clear
-if [[ "${iChoice}" == "A" ]]; then
-    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-blacklist.py' | python3
-elif [[ "${iChoice}" == "R" ]]; then
-    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | python3
-elif [[ "${iChoice}" == "S" ]]; then
-    clear
-    return
-else
-    echo 'Input error: Please try again.'
-    echo
-    read -p 'Press enter to start over.'
-    unset iChoice
-    bash 'pihole-regex/exact-blacklist.sh'
-fi
+
+case "$choice" in
+    ([Aa])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-blacklist.py' | python3;;
+
+    ([Rr])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | python3;;
+    ([Ss])      return 0;;
+    *)
+                printf "%s\n\n" 'Bad user input. Restarting script...'
+                unset choice
+                sleep 2
+                clear
+                sudo bash "$0"
+                ;;
+esac
+
+unset choice
 
 echo
 read -p 'Press Enter to continue: '
