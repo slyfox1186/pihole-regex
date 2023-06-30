b39e7f8d15d7729a0ba94387414bf0abef88a406
Jeff <jhollis.ga@gmail.com>
2023-06-30 18:43:50 -0500

    Update regex-whitelist.sh

--- scripts/shell-install/regex-whitelist.sh
+++ scripts/shell-install/regex-whitelist.sh
@@ -1,43 +1,62 @@
 #!/bin/bash
 
-# make user input case insensitive
-shopt -s nocasematch
+clear
 
-fn_exit_white_regex()
+exit_fn()
 {
     clear
-    echo 'Restart Pihole'\''s DNS?'
-    echo
-    read -p '[Y]es or [N]o: ' uChoice
+    printf "%s\n\n%s\n%s\n\n" \
+        'Do you want to restart pihole'\''s DNS?' \
+        '[1] Yes' \
+        '[2] No'
+    read -p 'Your choices are (1 or 2): ' rchoice
     clear
-    if [[ "${uChoice}" == "Y" ]]; then pihole restartdns; fi
-    # remove all files that were downloaded
-    if [ -d 'pihole-regex' ]; then rm -R 'pihole-regex'; fi
-    echo
-    echo 'Make sure to star this repository to show your support!'
-    echo 'GitHub: https://github.com/slyfox1186/pihole-regex'
-    echo
-    exit
+
+    case "$rchoice" in
+        1)
+                sudo rm -fr 'pihole-regex'
+                sudo pihole restartdns
+                ;;
+        2)      sudo rm -fr 'pihole-regex';;
+        '')
+                sudo rm -fr 'pihole-regex'
+                sudo pihole restartdns
+                ;;
+        *)      
+                printf "%s\n\n" 'Bad user input. Restarting script...'
+                unset rchoice
+                sleep 2
+                clear
+                exit_fn
+                ;;
+    esac
+
+    printf "%s\n\n%s\n\n" \
+        'Make sure to star this repository to show your support!' \
+        'GitHub: https://github.com/slyfox1186/pihole-regex'
+    exit 0
 }
 
-# Get user's input
+# GET THE USER'S INPUT
+read -p 'RegEx Whitelist >> [A]dd [R]emove [S]kip: ' choice
 clear
-read -p 'RegEx Whitelist >> [A]dd [R]emove [S]kip: ' iChoice
-clear
-if [[ "${iChoice}" == "A" ]]; then
-    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/regex-whitelist.py' | python3
-elif [[ "${iChoice}" == "R" ]]; then
-    curl -sSl 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/regex-whitelist.py' | python3
-elif [[ "${iChoice}" == "S" ]]; then
-    fn_exit_white_regex
-else
-    echo 'Input error: Please try again.'
-    echo
-    read -p 'Press enter to start over.'
-    unset iChoice
-    bash 'pihole-regex/regex-whitelist.sh'
-fi
+
+case "$choice" in
+    ([Aa])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/regex-whitelist.py' | python3;;
+
+    ([Rr])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/regex-whitelist.py' | python3;;
+    ([Ss])      exit_fn;;
+    *)
+                printf "%s\n\n" 'Bad user input. Restarting script...'
+                unset choice
+                sleep 2
+                clear
+                sudo bash 'pihole-regex/regex-whitelist.sh'
+                ;;
+esac
+
+unset choice
 
 echo
 read -p 'Press Enter to continue: '
-fn_exit_white_regex
+exit_fn
