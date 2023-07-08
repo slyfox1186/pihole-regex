f3a6baab37e112ebe9e735014873db93d525a65e
Jeff <jhollis.ga@gmail.com>
2023-07-07 19:59:05 -0500

    Update adlist.sh

--- scripts/shell-install/adlist.sh
+++ scripts/shell-install/adlist.sh
@@ -15,9 +15,6 @@ clear
 
 repo=https://github.com/slyfox1186/pihole-regex
 
-# MAKE USER INPUT CASE INSENSITIVE
-shopt -s nocasematch
-
 # INSTALL SQLITE3 IF MISSING
 if ! which sqlite3 &>/dev/null; then
     sudo apt -y install sqlite3
@@ -52,25 +49,22 @@ fail_fn()
 # SET THE FUNCTION TO RESTART PIHOLE'S DNS
 dns_fn()
 {
+    unset choice
     local choice
+    clear
 
     printf "\n%s\n\n%s\n%s\n\n" \
-        'Would you like to restart Pi-hole'\''s DNS resolver? (Recommended)' \
+        "Would you like to restart Pi-hole's DNS resolver? (Recommended)" \
         '[1] Yes' \
         '[2] No'
     read -p 'Enter a number: ' choice
-
     clear
 
     case "$choice" in
         1)      sudo pihole restartdns;;
         2)      echo;;
-        '')     sudo pihole restartdns;;
-        *)
-                unset choice
-                clear
-                dns_fn
-                ;;
+       '')      sudo pihole restartdns;;
+        *)      dns_fn;;
     esac
 }
 
@@ -78,9 +72,10 @@ dns_fn()
 gravity_fn()
 {
     local choice
+    clear
 
-    printf "\n%s\n\n%s\n%s\n\n" \
-        'Would you like to update Gravity'\''s database? (Recommended)' \
+    printf "%s\n\n%s\n%s\n\n" \
+        "Would you like to update Gravity's database? (Recommended)" \
         '[1] Yes' \
         '[2] No'
     read -p 'Your choices are (1 or 2): ' choice
@@ -93,17 +88,16 @@ gravity_fn()
         '')     sudo pihole -g;;
         *)
                 unset choice
-                clear
                 gravity_fn
                 ;;
     esac
 }
 
 # SET ADLIST URL VARIABLES
-slyfox_url='https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/adlist/adlists.txt'
+slyfox_url=https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/adlist/adlists.txt
 
 # SET THE COMMENTS
-c1='SlyADL - SlyFox1186 + Firebog'\''s Ticked'
+c1="SlyADL - SlyFox1186 + Firebog's Ticked"
 c2='SlyADL - Firebog - Ticked'
 c3='SlyADL - Firebog - Non-Crossed'
 c4='SlyADL - Firebog - All'
@@ -114,7 +108,7 @@ user_agent='--user-agent='\''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebK
 sly_adlist=/tmp/sly_adlist.txt
 fb_adlist=/tmp/firebog_adlist.txt
 fb_url_base='https://v.firebog.net/hosts/lists.php?type'
-gravity="$(sudo find /etc/pihole -type f -name gravity.db)"
+gravity="$(sudo find /etc -type f -name gravity.db)"
 
 # IF NECESSARY CHANGE THE VALUE OF THE GRAVITY VARIABLE TO THE FULL PATH OF THE 'GRAVITY.DB' FILE
 if [ -z "$gravity" ]; then
@@ -138,6 +132,8 @@ case "$choice1" in
                 if sudo sqlite3 "$gravity" "DELETE FROM adlist WHERE comment LIKE '%SlyADL%'"; then
                     clear
                     echo 'Success: All adlists have been removed from Pi-hole.'
+                    sleep 3
+                    clear
                     gravity_fn
                     dns_fn
                     exit_fn
@@ -152,7 +148,7 @@ esac
 # PROMPT THE USER TO SELECT THE ADLIST
 printf "%s\n\n%s\n%s\n%s\n%s\n\n" \
     'Choose from the adlists below to insert their contents into the Gravity database.' \
-    '[1] SlyFox1186: [Personal Adlist] - Self-made with lists from the good work of others. (Includes: Firebog Ticked).' \
+    '[1] SlyFox1186: [Personal Adlist] - Self-made with lists that include the good work from others. (Includes: Firebog Ticked, BlockList Project).' \
     '[2] Firebog:    [Ticked] - Perfect for system admins with little time available to fix database issues.' \
     '[3] Firebog:    [Non-Crossed] - These domains are generally less safe than the "Tick" list and most likely have an increased risk of false positives.' \
     '[4] Firebog:    [All] - False positives are very likely and will require much more effort than the average system admin would wish to spend fixing a database.'
@@ -162,46 +158,42 @@ clear
 case "$choice2" in
     1)
         wget "$user_agent" -qO "$sly_adlist" "$slyfox_url"
-        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"=tick
-        sudo tee < "$fb_adlist" -a "$sly_adlist"
-        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' 2>/dev/null "$sly_adlist"
-        sudo sort -o "$sly_adlist" "$sly_adlist"
-        sudo cat < "$sly_adlist" | sudo xargs -n1 -I{} sudo sqlite3 "$gravity" 2>/dev/null \
-            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c1\")"
+        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"'=tick'
+        sudo tee < "$fb_adlist" -a "$sly_adlist" 2>/dev/null
+        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' "$sly_adlist" 2>/dev/null
+        sudo sort -o "$sly_adlist" "$sly_adlist" 2>/dev/null
+        sudo cat "$sly_adlist" | sudo xargs -I{} sudo sqlite3 "$gravity" \
+            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c1\")" 2>/dev/null
         ;;
     2)
-        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"=tick |
-        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' 2>/dev/null > "$fb_adlist"
-        sudo sort -o "$fb_adlist" "$fb_adlist"
-        sudo cat < "$fb_adlist" | sudo xargs -n1 -I{} sudo sqlite3 "$gravity" 2>/dev/null \
-            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c2\")"
+        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"'=tick' |
+        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' > "$fb_adlist" 2>/dev/null
+        sudo sort -o "$fb_adlist" "$fb_adlist" 2>/dev/null
+        sudo cat "$fb_adlist" | sudo xargs -I{} sudo sqlite3 "$gravity" \
+            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c2\")" 2>/dev/null
         ;;
     3)
-        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"=nocross |
-        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' 2>/dev/null > "$fb_adlist"
-        sudo sort -o "$fb_adlist" "$fb_adlist"
-        sudo cat < "$fb_adlist" | sudo xargs -n1 -I{} sudo sqlite3 "$gravity" 2>/dev/null \
-            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c3\")"
+        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"'=nocross' |
+        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' > "$fb_adlist" 2>/dev/null
+        sudo sort -o "$fb_adlist" "$fb_adlist" 2>/dev/null
+        sudo cat "$fb_adlist" | sudo xargs -I{} sudo sqlite3 "$gravity" \
+            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c3\")" 2>/dev/null
         ;;
     4)
-        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"=all |
-        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' 2>/dev/null > "$fb_adlist"
-        sudo sort -o "$fb_adlist" "$fb_adlist"
-        sudo cat < "$fb_adlist" | sudo xargs -n1 -I{} sudo sqlite3 "$gravity" 2>/dev/null \
-            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c4\")"
+        wget "$user_agent" -qO "$fb_adlist" "$fb_url_base"'=all' |
+        sudo sed -i -e '/^#/ d' -i -e '/^$/ d' -i -e '/^$/d' > "$fb_adlist" 2>/dev/null
+        sudo sort -o "$fb_adlist" "$fb_adlist" 2>/dev/null
+        sudo cat "$fb_adlist" | sudo xargs -I{} sudo sqlite3 "$gravity" \
+            "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('{}',\"$c4\")" 2>/dev/null
         ;;
-    *)
-        fail_fn 'Bad user input.';;
+    *)  fail_fn 'Bad user input.';;
 esac
 
 # REMOVE TEMPORARY FILES CREATED BY THE SCRIPT
-sudo rm "$fb_adlist" "$sly_adlist" 'adlist.sh'
-
+sudo rm "$fb_adlist" "$sly_adlist" adlist.sh
 # PROMPT THE USER TO UPDATE GRAVITY'S DATABASE
 gravity_fn
-
 # PROMPT THE USER TO RESTART PI-HOLE'S DNS
 dns_fn
-
 # SHOW EXIT MESSAGE
 exit_fn
