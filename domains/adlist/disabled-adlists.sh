4c8799bae04c8c1b71ac2f258270e4bb83617da7
Jeff <jhollis.ga@gmail.com>
2023-07-11 23:00:47 -0500

    Create disabled-adlists.sh

---
+++ domains/adlist/disabled-adlists.sh
@@ -0,0 +1,39 @@
+######################################################
+#
+# These Adlists have been disabled due to their low
+# hit counts registered by the pihole_adlist_tool
+#
+######################################################
+
+#
+# Firebog "Tick" list
+#
+
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
+https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt
+https://urlhaus.abuse.ch/downloads/hostfile/
+https://v.firebog.net/hosts/Admiral.txt
+https://v.firebog.net/hosts/RPiList-Phishing.txt
+
+#
+# Blocklist Project Disabled Adlists due to low hit count measured by the pihole_adlist_tool
+#
+
+https://blocklistproject.github.io/Lists/phishing.txt
+https://blocklistproject.github.io/Lists/ransomware.txt
+https://blocklistproject.github.io/Lists/redirect.txt
+https://blocklistproject.github.io/Lists/scam.txt
+
+#
+# Perflyst - github.com/Perflyst/PiHoleBlocklist
+#
+
+https://perflyst.github.io/PiHoleBlocklist/android-tracking.txt
+https://perflyst.github.io/PiHoleBlocklist/SessionReplay.txt
+https://perflyst.github.io/PiHoleBlocklist/SmartTV.txt
+
