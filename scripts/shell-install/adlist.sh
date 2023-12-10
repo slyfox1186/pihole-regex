8aa249ca66328819d1f629a1193f124d0e70ab79
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-12-10 01:06:55 -0500

    Update adlist.sh

--- scripts/shell-install/adlist.sh
+++ scripts/shell-install/adlist.sh
@@ -32,7 +32,7 @@ c3='SlyADL - Firebog - Non-Crossed Lists'
 c4='SlyADL - Firebog - All Lists'
 
 # SET CURL COMMAND VARIABLE
-user_agent='--user-agent='\''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'\'''
+user_agent='--user-agent='\''Mozilla/5.0 (X11; Linux x86_64) Applpc_typeeWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36''\'''
 
 # STORE TEMPORARY FILES AND DIRECTORIES IN VARIABLES
 tmp_dir="$(mktemp -d)"
