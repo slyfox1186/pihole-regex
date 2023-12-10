8bbff94a0071346df6a508dec7d69b99506e71a9
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-12-10 16:02:52 -0500

    Update exact-blacklist.sh

--- scripts/shell-install/exact-blacklist.sh
+++ scripts/shell-install/exact-blacklist.sh
@@ -1,26 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-clear
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
-# GET THE USER'S INPUT
-read -p 'Exact Blacklist >> [A]dd [R]emove [S]kip: ' choice
-clear
+user_prompt_fn()
+{
+    local choice
+    clear
 
-case "$choice" in
-    ([Aa])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-blacklist.py' | python3;;
-
-    ([Rr])      curl -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | python3;;
-    ([Ss])      return 0;;
-    *)
-                printf "%s\n\n" 'Bad user input. Restarting script...'
-                unset choice
-                sleep 2
-                clear
-                sudo bash "$0"
-                ;;
-esac
-
-unset choice
+    read -p 'Exact Blacklist >> [A]dd [R]emove [S]kip: ' choice
+    clear
+    
+    case "$choice" in
+        ([Aa])      curl -A "${user_agent}" -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-install/exact-blacklist.py' | python3;;
+    
+        ([Rr])      curl -A "${user_agent}" -sSL 'https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/scripts/python-uninstall/exact-blacklist.py' | python3;;
+        ([Ss])      return 0;;
+        *)
+                    unset choice
+                    clear
+                    sudo bash "${0}"
+                    ;;
+    esac
+}
+user_prompt_fn
 
 echo
 read -p 'Press Enter to continue: '
