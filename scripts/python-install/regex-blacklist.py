6f7c13c33b17258c34455a2a4e1e24f20c922c9d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-01 23:50:21 -0800

    Update regex-blacklist.py

--- scripts/python-install/regex-blacklist.py
+++ scripts/python-install/regex-blacklist.py
@@ -11,7 +11,7 @@ def fetch_blacklist_url(url):
     if not url:
         return None
 
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"}
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
