4351491cbd7098c45ab0055d566a46596c747adc
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-02 00:01:51 -0800

    Update regex-whitelist.py

--- scripts/python-install/regex-whitelist.py
+++ scripts/python-install/regex-whitelist.py
@@ -74,7 +74,7 @@ def main():
     args = parse_arguments()
     db_path = os.path.join(args.dir, "gravity.db")
 
-    whitelist_url = "https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/whitelist/regex-whitelist.txt"
+    whitelist_url = "https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/regex-whitelist.txt"
     domains = fetch_whitelist_url(whitelist_url)
     if domains:
         update_database(db_path, domains)
