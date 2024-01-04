314da6d993b805ea516a4c5af4d9a3b9b0c02683
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-04 01:27:51 -0800

    Create regex-whitelist.sql

---
+++ domains/regex-whitelist.sql
@@ -0,0 +1,67 @@
+(\.|^)minecraft\.net$ -- Mincraft Video Game
+^(benchmarks|commons|empoweringtrust|support\.benchmarks|www)(\.ul\.com(|\.cdn\.cloudflare\.net))$ -- 3d Mark PC Benchmark tests
+(\.|^)(dns|)algolia(|net)\.(com|io|net)$ -- AdultTime Necessary for the site to deliver non-ad content
+^([a-z0-9]+\.|)(appsync|execute)-api\.us-east-1\.amazonaws\.com$ -- AdultTime Necessary for the site to deliver non-ad content
+^images[0-9]+-fame\.gammacdn\.com$ -- AdultTime Necessary for the site to deliver non-ad content
+(\.|^)amd\.com$ -- AMD Microchip Maker
+^cdn\.(bootcss|cloudflare|datatables|discordapp|embed|embedly|materialdesignicons|mathjax|plyr|samsungcloudsolution|signal|staticfile)\.(com|io|ly|net|org)$ -- Content Delivery Networks AKA CDN
+^connectivitycheck\.(android|gstatic|ubuntu)\.com$ -- Content Delivery Networks AKA CDN
+^cdn\.boot(|cdn|css)\.(com|net)\.maoyundns\.com$ -- Content Delivery Networks AKA CDN
+^code\.(bildstatic|createjs|jquery)\.(com|de)Content Delivery Networks AKA CDN
+^cds\.[a-z0-9]+\.hwcdn\.net$ -- Content Delivery Networks AKA CDN
+(\.|^)apple-(cloudkit|livephotoskit)\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^(appattest|appleid|apps|audiocontentdownload|bpapi|business|captive|cdn-apple|certs|configuration|crl|cssubmissions|deviceenrollment|diagassets|fba|gc|gdmf|gg.*|gs(|[ap1]*?)|humb|identity|idmsa|i(|g|profiles|su|tunes|work)|mdmenrollment|mesu|oscdn|oscp|osrecovery|ppq|push|radarsubmissions|school|serverstatus|skl|suconfig|sw(|cdn|dist|download|post|scan)|sylvan|sym[b-d]+?|tbsc|valid|www|axm-app|deviceservices-external|gnf-mdn|gnf-mr|lcdn-(locator|registration)|sq-device|time(|-ios|-macos)|xp(|-cdn))\.apple\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^(devimages|xp-cdn|playgrounds(|-assets))-cdn\.apple\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^(download.developer|ns.itunes|static.ips|vpp.itunes)\.apple\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^(ocsp[0-9]*?\.(apple|apzones|digicert|entrust|verisign)\.(com|net))$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^axm-adm-|(adm|mdm|enroll|scep)\.apple\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^gsp-ssl\.ls(\.|-)apple\.com(|\.akadns\.net)$ -- Apple - Main domains for a good chunk of legitimate iOS services
+^cdn[0-3]*?\.optimizely\.com$ -- A progressive delivery and experimentation software for other companies
+(\.|^)mee6\.xyz$ -- Chrome Browser Discord App MEE6 Support Website
+^ns[1-2]?\.dropbox\.com$ -- DropBox
+^(account|accts|catalog|entitlement|eulatracking|lightswitch|metric|priceengine)-public-service-prod[0-9]*?\.(ak|ol)\.epicgames\.com$ -- Epic Games - Essential domains for authentication during user login and other services such as purchasing/unlocking games to your user library
+(\.|^)(accountkit|messenger)\.com$ -- Facebook - Essential CDN servers
+(\.|^)(connect\.|)facebook\.(com|net)$ -- Facebook - Essential CDN servers
+^scontent-(atl3|lhr3|mia3|ort2)-1\.xx\.fbcdn\.net$ -- Facebook - Essential CDN servers
+^[0-6]+?-edge-chat\.facebook\.com$ -- Facebook - Essential CDN servers
+^(github(|\.githubassets)\.(com|io))$ -- GitHub
+^beacons[0-9]*\.gvt[0-9]+\.com$ --
+^(calendar|developer(|s)|dns|docs|home|inbox|clients[1-6]*|mail|meet|pay(|ments)|photos|sites|time|wallet|workspace|www)\.google\.com$ -- Google - Allow known essential widget service
+(\.|^)lencr\.org$ -- Lets Encrypt - One of the main SSL certificate encryption websites
+(www\.|^)((go\.|)microsoft|windows)\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+(\.|^)1drv\.ms$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+(\.|^)windowsupdate\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^(attestation|cert\.mgt|def-vef|(device(|\.auth))|eds|help\.ui|licensing|notify|title(|\.(auth|mgt))|user|xflight|www|xboxexperiencesprod\.experimentation|xkms|xsts\.auth)\.xboxlive\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^(csp|c\.s|devicelistenerprod)(-|\.)microsoft\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^(edge|fs|mobile\.pipe\.aria|msedge\.api.\cdp|officecdnmac|officeclient|support|teams|vlscppe|whiteboard|www)\.microsoft\.com$ --
+^(settings|v10\.vortex)-win\.data\.microsoft\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^europe.(cp\.wd|smartscreen(-prod|))\.microsoft\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^microsoft(|online)\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^outlook\.(live|office(|365))\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^v[0-2]+?\.events\.data\.microsoft\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+^accounts\..*\.(com|net|org|uk|br|ly|gov)$ -- Required to log into general website user account pages
+^m\.(hotmail|reddit|reuters|twitch|weeklyad.target|yahoo|youtube)\.(com|tv)$ -- Mobile and Tablets
+(\.|^)(myqnapcloud|qnap)\.com$ -- QNAP NAS - Required for the myqnapclub feature to work in its entirety
+(\.|^)robvanderwoude\.com$ -- Rob VanderWoude Learn how to code multiple languages
+(\.|^)([iv]?\.|)redd\.it$ -- Reddit
+(\.|^)reddit\.com$ -- Reddit
+[a-z]\.thumbs\.redditmedia\.com$ -- Reddit
+^events-test(|-v2)\.redditmedia\.com$ -- Reddit
+^s{1..5}\.symcb\.com$ -- Skype
+(\.|^)samsungcloudcdn\.com$ -- Smart TV
+(\.|^)samsungotn\.net$ -- Smart TV
+^aic\.(homeprv|service)\.lgtvcommon\.com$ -- Smart TV
+^hbbtv\.(prosieben|redbutton|zdf|ard)\.de$ -- Smart TV
+^otnprd[0-9]+?\.samsungcloudsolution\.net$ -- Smart TV
+^(apresolve|audio2|embed|dealer|global-dealer-ssl|gucl-6.ap|login5|market|mobile-ap|open|play|spclient\.wg|spclient-wg|weblb-wg.dual-gslb)\.spotify\.com$ -- Spotify
+^crl[3-4]\.digicert\.com$ -- SSL Certificate
+^(blog|business|(www\.|)careers|email|(dev(|\.careers)|channels|contests|insights|meta|talent))\.stackoverflow\.com$ -- Stackoverflow Developer\Coding help site
+^(affiliate|app|id(|\.|-(canary|cdn|dev)))\.twitch\.tv$ -- Twitch.tv
+^video-edge-[0-9]+?\.[a-z]+?[0-9]+?\.abs\.hls\.ttvnw\.net$ -- Twitch.tv
+^whatsapp-cdn-shv-[0-9]{2}-[a-z]{3}[0-9]\.fbcdn\.net$ -- WhatsApp
+^((www|(w[0-9]\.)?web|media((-[a-z]{3}|\.[a-z]{4})[0-9]{1,2}-[0-9](\.|-)(cdn|fna))?)\.)?whatsapp\.(com|net)$ -- WhatsApp
+(\.|^)twimg\.com$ -- Twitter
+(\.|^)unraid\.net$ -- Unraid - Required for the operating system to update
+^(attestation|cert\.mgt|def-vef|eds|help\.ui|licensing|notify|title\.mgt|www|xboxexperiencesprod\.experimentation|xflight|xkms)\.xboxlive\.com$ -- Xbox Live (Microsoft)
+(www\.|^)yahoo\.com$ -- Yahoo
+^(oauthaccountmanager|www|youtubei)\.googleapis\.com$ -- YouTube
