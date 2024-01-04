2c047d22290867ad6c07d85ae27897de13284bb6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-04 01:38:49 -0800

    Create regex-blacklist.sql

---
+++ domains/regex-blacklist.sql
@@ -0,0 +1,99 @@
+^[a-z]+ad\.[a-z]+\.com\.edgesuite\.net$ -- Take out all ad servers in one go for master domain com.edgesuite.net
+(\.|^)antivirushub\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
+(\.|^)shadowadcity\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
+(\.|^)trafyield\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
+^(ad(|s))\.(amzn|amazonaws|amazontrust|apzones|digicert|marketwatch|mzstatic|sym[b-d]+?|wp|yimg|youtube|ytimg)\.(com|to)$ -- Amazon
+(\.|^)appsync-api\.us-[a-z]+-[0-9]?\.amazonaws\.com$ -- AdultTime
+(\.|^)gammaentertainment\.com$ -- AdultTime
+(\.|^)getbeamer\.com$ -- AdultTime
+(\.|^)lambda-url\.us-east-1\.on\.aws$ -- AdultTime
+(\.|^)usage\.trackjs\.com$ -- AdultTime
+(\.|^)[a-z0-9]+\.execute-api\.us-[a-z]+-[0-9]?\.amazonaws\.com$ -- AdultTime
+(\.|^)deimos3.apple\.com$ -- Apple
+^.*iad.*\.amazon\.com$ -- Amazon
+(\.|^)xyz$ -- Bittorrent
+^(ads|captive\.adservice|adwords(|\.l))\.google\.com$ -- Google
+^hbbtv\.(3sat|kabeleins|qvc|rtl2|sat1|sixx|superrtl)\.de$ -- HBBTV
+(\.|^)filesupdating\.com$ -- iTop VPN
+(\.|^)ibs\.lgappstv\.com$ -- LG Smart TVs
+(\.|^)lgsmartad\.com$ -- LG Smart TVs
+(\.|^)rdx2\.lgtvsdp\.com$ -- LG Smart TVs
+(\.|^)smartshare\.lgtvsdp\.com$ -- LG Smart TVs
+(\.|^)ok\.ru$ -- OK.ru
+^(api|auth|msg|unified).*\.hismarttv\.com$ -- Other Smart TVs
+^(collect|usage)-(eu|us)-fy[0-9]+\.myhomescreen\.(tv|tvcollect)$ -- Other Smart TVs
+(\.|^)ad-score\.com$ -- Overwolf Desktop App (PC Games)
+(\.|^)fake\.video\.url\.webm$ -- Overwolf Desktop App (PC Games)
+^(api-software|d-20p1an5qn|marketing-materials|marketing|origin-marketing|origin-www|ixq|iot|partner)\.qnap\.com$ -- QNAP Computers
+^(advertising|www)\.reddithelp\.com$ -- Reddit.com
+^(ssl|static|www)\.redditgifts\.com$ -- Reddit.com
+((abtauthprd|acr0|amauthprd|cdn|configprd|fkp|gamespromotion|musicid|notice|noticecdn|noticefile|oempprd|prderrordumphsm|prov|sas|vdterms)\.|^)samsungcloudsolution\.(com|net)$ -- Samsung Smart TV
+(\.|^)giraffic\.com$ -- Samsung Smart TV
+(\.|^)internetat\.tv$ -- Samsung Smart TV
+^log-[0-3]+\.samsungacr\.com$ -- Samsung Smart TV
+(\.|^)pavv\.co\.kr$ -- Samsung Smart TV
+(\.|^)samsungelectronics\.com$ -- Samsung Smart TV
+(\.|^)samsungqbe\.com$ -- Samsung Smart TV
+(\.|^)samsungrm\.net$ -- Samsung Smart TV
+^ext[0-9]?-iad[0-9]?\.steamserver\.net$ -- Steam Desktop App (Gaming)
+(\.|^)ahoravideo.*$ -- Suspected Virus Harboring websites
+(\.|^)bideo.*$ -- Suspected Virus Harboring websites
+(\.|^)fairu.*$ -- Suspected Virus Harboring websites
+(\.|^)privatproxy.*$ -- Suspected Virus Harboring websites
+(\.|^)wmail.*$ -- Suspected Virus Harboring websites
+(\.|^)(roblox|(rbx)(cdn|trk))\.(com|plus)$ -- Roblox
+(\.|^)impala-media-production\.s3\.amazonaws\.com$ -- Snapchat
+(\.|^)sc-cdn\.net$ -- Snapchat
+(\.|^)snap(-dev|ads|chat|kit)\.(c|com|net)$ -- Snapchat
+^[a-z0-9]+\.ssm[1-2]?\.internet\.sony\.tv$ -- Sony Smart TV
+(\.|^)log[0-9]*\.spotify\.com$ -- Spotify
+(\.|^)pixel(|-static)\.spotify\.com$ -- Spotify
+(\.|^)[go]*tinder[sparks]*\.com$ -- Tinder app
+^video-edge-(046b4c|078ff6|82d760|82dc48|830164|833564|918014|918388|9183d4|9183f0)\.atl01\.abs\.hls\.ttvnw\.net$ -- Twitch.tv Video ads
+(\.|^)tvinteractive\.tv$ -- Vizio Smart TV
+(\.|^)((www|(w[0-9]\.)?web|media((-[a-z]{3}|\.[a-z]{4})[0-9]{1,2}-[0-9](\.|-)(cdn|fna))?)\.)?whatsapp\.(com|net)$ -- WhatsApp
+(\.|^)whatsapp-cdn-shv-[0-9]{2}-[a-z]{3}[0-9]\.fbcdn\.net$ -- WhatsApp
+^a-content-[a-z0-9]+-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^adserver-.*\.amazonaws\.com$ -- Yahoo
+[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^[a-z]{2}-[a-z]{2}-[a-z]{2}\.ybp\.gysm\.yahoodns\.net$ -- Yahoo
+^content(|-geo)\.uplynk\.com$ -- Yahoo
+^cs[0-9]+\.wpc\.thetacdn\.net$ -- Yahoo
+^dms-[a-z0-9]+report\.wc\.yahoodns\.net$ -- Yahoo
+^dns-[a-z0-9]+\.sombrero\.yahoo\.net$ -- Yahoo
+^ds-beap[0-9]\.[a-z]{2}\.[a-z0-9]+\.yahoodns\.net$ -- Yahoo
+^ds-global[0-9]\.l[0-9].search\.[a-z0-9]+\.yahoo\.com$ -- Yahoo
+^nado-ecs-lb-[a-z]+-[a-z]+-[0-9]-[0-9]+\.[a-z]+-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^prod-elb-ats-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^ssp-ats-prod-[a-z\-]+-[0-9]\.one-mobile-prod\.aws\.oath\.cloud$ -- Yahoo
+^uplynk-beacon-newvpc-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^v-[a-z0-9]+\.wc\.yahoodns\.net$ -- Yahoo
+(\.|^)youtube-nocookie\.com$ -- YouTube
+^(.+[_.-])?adse?rv(er?|ice)?s?[0-9]*[_.-] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^(.+[_.-])?telemetry[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^ad([sxv]?[0-9]*|system)[_.-]([^.[:space:]]+\.){1,}|[_.-]ad([sxv]?[0-9]*|system)[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^adim(age|g)s?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^adtrack(er|ing)?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^advert(s|is(ing|ements?))?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^aff(iliat(es?|ion))?[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^analytics?[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^banners?[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^beacons?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^count(ers?)?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^mads\.] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^pixels?[-.]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+^stat(s|istics)?[0-9]*[_.-]] -- mmotti RegEx - github.com/mmotti/pihole-regex/blob/master/regex.list
+(^|\.)ibs\.lgappstv\.com$ -- LG TV
+(^|\.)lgsmartad\.com$ -- LG TV
+(^|\.)lgtvcommon\.com$ -- LG TV
+(^|\.)rdx2\.lgtvsdp\.com$ -- LG TV
+(^|\.)smartshare\.lgtvsdp\.com$ -- LG TV
+^[a-rt-z][a-z]{10,14}$ -- LG TV
+(^|\.)giraffic\.com$ -- Samsung TV
+(^|\.)internetat\.tv$ -- Samsung TV
+(^|\.)pavv\.co\.kr$ -- Samsung TV
+(^|\.)samsungcloudsolution\.net$ -- Samsung TV
+(^|\.)samsungelectronics\.com$ -- Samsung TV
+(^|\.)samsungrm\.net$ -- Samsung TV
+(\.|^)tvinteractive\.tv$ -- Vizio TV
+^api\..*\.hismarttv\.com$ -- Other TV
