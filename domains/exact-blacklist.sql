cabdbab22ccb7eeeb2da550e924ec3ec41038ae4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-04 01:18:41 -0800

    Create exact-blacklist.sql

---
+++ domains/exact-blacklist.sql
@@ -0,0 +1,97 @@
+a1241.dsct.akamai.net -- Akamai CDN Ad Server
+a21ed24aedde648804e7-228765c84088fef4ff5e70f2710398e9.r17.cf1.rackcdn.com -- Twitch Video Ads
+adserver-use1-ext-prod-a-563525597.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
+aic-gfts.lge.com -- LG Smart TV
+aic-ngfts.lge.com -- LG Smart TV
+aic-op-lss.lgthinq.com -- LG Smart TV
+aic.api.lgtviot.com -- LG Smart TV
+aic.homeprv.lgtvcommon.com -- LG Smart TV
+aic.lgtviot.com -- LG Smart TV
+aic.nudge.lgtvcommon.com -- LG Smart TV
+aic.rdl.lgtvcommon.com -- LG Smart TV
+aic.recommend.lgtvcommon.com -- LG Smart TV
+aic.sports.lgtviot.com -- LG Smart TV
+aic.wiseconfig.lgtvcommon.com -- LG Smart TV
+api.itopvpn.com -- iTop VPN Telemetry
+api.nfl.com -- NFL - American Football Telemetry
+api.topaz-labs.net -- Topaz Labs Telemetry
+api.us-east-1.aiv-delivery.net -- LG Smart TV
+applytics.nordvpn.com -- NordVPN Telemetry
+bench.utorrent.com -- uTorrent Desktop App Ads- SlyEBL
+canvas.tubitv.com -- LG Smart TV
+cc-api-data.adobe.io -- Adobe Systems Telemetry
+ccleaner.tools.avcdn.net -- CCleaner Telemetry Desktop App Spyware
+cdn.cookielaw.org -- 3D Mark Software tracking cookies
+cdn.inspectlet.com -- IObit Desktop Popup Advertisements
+cdn77.utomik.com -- LG Smart TV
+cf-trickplay.aux.pv-cdn.net -- LG Smart TV
+client-api.nordvpn.com -- NordVPN Telemetry
+cloudfront.xp-assets.aiv-cdn.net -- LG Smart TV
+codes.iobit.com -- IObit Desktop Pop up Ads
+common.lgthinq.com -- LG Smart TV
+cs833182181.wpc.etacdn.net -- IObit Desktop Pop-up Ads
+d186rixkn5ruba.cloudfront.net -- AdultTime Ads
+d1ut6fykkt3imt.cloudfront.net -- AdultTime Ads
+d20qwf0wrdtevy.cloudfront.net -- Cloudfront CDN Ad Server
+d2u28gf9o4d1cv.cloudfront.net -- Cloudfront CDN Ad Server
+d306ecvlh9tfv.cloudfront.net -- AdultTime Ads
+d3aqoihi2n8ty8.cloudfront.net -- AdultTime Ads
+d3cqix4sol87xw.cloudfront.net -- AdultTime Ads
+d3m1uefep57n8q.cloudfront.net -- Twitch Video Ads
+developers.google.com -- LG Smart TV
+discovery.meethue.com -- LG Smart TV
+dns-fxp05etdx.sombrero.yahoo.net -- Yahoo Video Ads
+doh.dns.apple.com -- Apple DoH Blocks Pi-Hole Queries Entirely
+doug1izaerwt3.cloudfront.net -- IObit Desktop Pop-up Ads
+dxtkri2c61io4.cloudfront.net -- Twitch Video Ads
+estore.iobit -- IObit Ads
+giveaway.iobit -- IObit Ads
+global.poe.live-video.net -- Twitch Video Ads
+gsn-rinkeby.redditspace.com -- Reddit Suspected Ads
+i.ibb.co -- LG Smart TV
+images.pluto.tv -- LG Smart TV
+images.redbox.com -- LG Smart TV
+img.nvidiagrid.net -- LG Smart TV
+ipv6.unagi-na.amazon.com -- LG Smart TV
+jc9wr0p5rbtco3ibchj4vnjz7z1lr1.ext-twitch.tv -- Twitch Video Ads
+kinesis.us-east-1.amazonaws.com -- AdultTime Ads
+lgtvonline.lge.com -- LG Smart TV
+lss.lgthinq.com -- LG Smart TV
+lynx-prod-beacon-alb-498367235.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
+manifest.googlevideo.com -- Possibly the number one domain that servers YouTube Ads. We must block it.
+mediaservices.cdn-apple.com -- LG Smart TV
+mindfieldonline.com -- LG Smart TV
+munchkin.marketo.net -- 3D Mark Software Tracking Cookies
+ncp-gw-sports.media.yahoo.com -- Yahoo - Sports Video Ads
+nevoai-iothub-54-prod.azure-devices.net -- LG Smart TV
+ngfts.lge.com -- LG Smart TV
+ocsp.sca1b.amazontrust.com -- amazon.com Ads
+prod-elb-ats-620772989.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
+prod.ups-ats.us-east-1.aolp-ds-prd.aws.oath.cloud -- Amazon CDN Ad Server
+purchase-iobit-com.us-east-1.elasticbeanstalk.com -- IObit Desktop Pop-up Ads
+purchase.iobit.com -- IObit Desktop Pop up Ads
+qt2-ngfts.lge.com -- LG Smart TV
+redirector.googlevideo.com -- Google Ads
+s3-iad-2.cf.dash.row.aiv-cdn.net -- LG Smart TV
+service.idsync.analytics.yahoo.com -- LG Smart TV
+snap.licdn.com -- 3D Mark Software tracking cookies
+snu.lge.com -- LG Smart TV
+stats.iobit.com -- IObit Desktop Pop up Ads
+stats.itopvpn.com -- iTop VPN Telemetry
+store.iobit.com -- IObit Desktop Pop up Ads
+survey.iobit -- IObit Ads
+threeplr-avuypkjypveaj-0.api.amazonvideo.com -- LG Smart TV
+unagi-na.amazon.com -- LG Smart TV
+update.filesupdating.com -- iTop VPN Telemetry
+us.emp.lgsmartplatform.com -- LG Smart TV
+us.ibs.lgappstv.com -- LG Smart TV
+us.lgeapi.com -- LG Smart TV
+us.rdx2.lgtvsdp.com -- LG Smart TV
+usage.truenas.com -- TrueNAS Software Telemetry
+video-edge-55b156.pdx01.abs.hls.ttvnw.net -- Twitch Video Ads
+video-edge-6dca22.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
+video-edge-833564.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
+video-edge-8491b8.pdx01.abs.hls.ttvnw.net -- Twitch Video Ads
+video-weaver.atl01.hls.ttvnw.net -- Twitch Video Ads
+video-weaver.jfk04.hls.ttvnw.net -- Twitch Video Ads
+www.ueiwsp.com -- LG Smart TV
