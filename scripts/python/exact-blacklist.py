3d1c86d93abc76174604abc1830f62237640d7c1
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-04 02:47:29 -0800

    Update exact-blacklist.py

--- scripts/python/exact-blacklist.py
+++ scripts/python/exact-blacklist.py
@@ -82,7 +82,7 @@ def main():
             print("\nNo domains needing removing from the exact blacklist.\n")
 
         if not added and not removed:
-            print("\nNo changes were made to the exact blacklist.")
+            print("\nNo changes were made to the exact blacklist.\n")
 
         if check_for_updates() and user_confirm("\nPi-hole update available. Do you want to update? (yes/no): "):
             subprocess.run(['pihole', '-up'], check=True)
