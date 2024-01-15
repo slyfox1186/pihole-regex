793c0f2e72cd87017daefe20fb3aa93e9bb5c4a6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-15 15:13:41 -0800

    Update exact-blacklist-test.py

--- domains/exact-blacklist-test.py
+++ domains/exact-blacklist-test.py
@@ -1,213 +1,238 @@
 # Akamai CDN Ad Server:
-- a1241.dsct.akamai.net
+a1241.dsct.akamai.net
+
 # Twitch Video Ads:
-- a21ed24aedde648804e7-228765c84088fef4ff5e70f2710398e9.r17.cf1.rackcdn.com
-- d3m1uefep57n8q.cloudfront.net
-- dxtkri2c61io4.cloudfront.net
+a21ed24aedde648804e7-228765c84088fef4ff5e70f2710398e9.r17.cf1.rackcdn.com
+d3m1uefep57n8q.cloudfront.net
+dxtkri2c61io4.cloudfront.net
+
 # Samsung Smart TV:
-- abtauthprd.samsungcloudsolution.com
-- abtauthprd.samsungcloudsolution.net
-- acr0.samsungcloudsolution.com
-- acr0.samsungcloudsolution.net
-- amauthprd.samsungcloudsolution.com
-- amauthprd.samsungcloudsolution.net
-- fkp.samsungcloudsolution.com
-- fkp.samsungcloudsolution.net
-- gamespromotion.samsungcloudsolution.com
-- gamespromotion.samsungcloudsolution.net
-- musicid.samsungcloudsolution.com
-- musicid.samsungcloudsolution.net
-- notice.samsungcloudsolution.com
-- notice.samsungcloudsolution.net
-- noticecdn.samsungcloudsolution.com
-- noticecdn.samsungcloudsolution.net
-- noticefile.samsungcloudsolution.com
-- noticefile.samsungcloudsolution.net
-- oempprd.samsungcloudsolution.com
-- oempprd.samsungcloudsolution.net
-- prderrordumphsm.samsungcloudsolution.com
-- prderrordumphsm.samsungcloudsolution.net
-- prov.samsungcloudsolution.com
-- prov.samsungcloudsolution.net
-- samsungcloudsolution.com
-- samsungcloudsolution.net
-- sas.samsungcloudsolution.com
-- sas.samsungcloudsolution.net
-- vdterms.samsungcloudsolution.com
-- vdterms.samsungcloudsolution.net
+abtauthprd.samsungcloudsolution.com
+abtauthprd.samsungcloudsolution.net
+acr0.samsungcloudsolution.com
+acr0.samsungcloudsolution.net
+amauthprd.samsungcloudsolution.com
+amauthprd.samsungcloudsolution.net
+fkp.samsungcloudsolution.com
+fkp.samsungcloudsolution.net
+gamespromotion.samsungcloudsolution.com
+gamespromotion.samsungcloudsolution.net
+musicid.samsungcloudsolution.com
+musicid.samsungcloudsolution.net
+notice.samsungcloudsolution.com
+notice.samsungcloudsolution.net
+noticecdn.samsungcloudsolution.com
+noticecdn.samsungcloudsolution.net
+noticefile.samsungcloudsolution.com
+noticefile.samsungcloudsolution.net
+oempprd.samsungcloudsolution.com
+oempprd.samsungcloudsolution.net
+prderrordumphsm.samsungcloudsolution.com
+prderrordumphsm.samsungcloudsolution.net
+prov.samsungcloudsolution.com
+prov.samsungcloudsolution.net
+samsungcloudsolution.com
+samsungcloudsolution.net
+sas.samsungcloudsolution.com
+sas.samsungcloudsolution.net
+vdterms.samsungcloudsolution.com
+vdterms.samsungcloudsolution.net
+
 # Amazon:
-- ad.amazonaws.com
-- ad.amazonaws.to
-- ad.amazontrust.com
-- ad.amazontrust.to
-- ad.amzn.com
-- ad.amzn.to
-- ad.apzones.com
-- ad.apzones.to
-- ad.digicert.com
-- ad.digicert.to
-- ad.marketwatch.com
-- ad.marketwatch.to
-- ad.mzstatic.com
-- ad.mzstatic.to
-- ad.symb.com
-- ad.symc.com
-- ad.symd.com
-- ad.wp.com
-- ad.wp.to
-- ad.yimg.com
-- ad.yimg.to
-- ad.youtube.com
-- ad.youtube.to
-- ad.ytimg.com
-- ad.ytimg.to
-- ads.amzn.com
-- ads.amzn.to
-- adserver-use1-ext-prod-a-563525597.us-east-1.elb.amazonaws.com
+ad.amazonaws.com
+ad.amazonaws.to
+ad.amazontrust.com
+ad.amazontrust.to
+ad.amzn.com
+ad.amzn.to
+ad.apzones.com
+ad.apzones.to
+ad.digicert.com
+ad.digicert.to
+ad.marketwatch.com
+ad.marketwatch.to
+ad.mzstatic.com
+ad.mzstatic.to
+ad.symb.com
+ad.symc.com
+ad.symd.com
+ad.wp.com
+ad.wp.to
+ad.yimg.com
+ad.yimg.to
+ad.youtube.com
+ad.youtube.to
+ad.ytimg.com
+ad.ytimg.to
+ads.amzn.com
+ads.amzn.to
+adserver-use1-ext-prod-a-563525597.us-east-1.elb.amazonaws.com
+
 # Reddit.com:
-- advertising.reddithelp.com
-- gsn-rinkeby.redditspace.com
-- ssl.redditgifts.com
-- static.redditgifts.com
-- www.reddithelp.com
+advertising.reddithelp.com
+gsn-rinkeby.redditspace.com
+ssl.redditgifts.com
+static.redditgifts.com
+www.reddithelp.com
+
 # LG Smart TV:
-- aic-gfts.lge.com
-- aic-ngfts.lge.com
-- aic-op-lss.lgthinq.com
-- aic.api.lgtviot.com
-- aic.homeprv.lgtvcommon.com
-- aic.lgtviot.com
-- aic.nudge.lgtvcommon.com
-- aic.rdl.lgtvcommon.com
-- aic.recommend.lgtvcommon.com
-- aic.sports.lgtviot.com
-- aic.wiseconfig.lgtvcommon.com
-- d186rixkn5ruba.cloudfront.net
-- d1ut6fykkt3imt.cloudfront.net
-- d306ecvlh9tfv.cloudfront.net
-- d3aqoihi2n8ty8.cloudfront.net
-- d3cqix4sol87xw.cloudfront.net
-- i.ibb.co
-- images.pluto.tv
-- images.redbox.com
-- img.nvidiagrid.net
-- ipv6.unagi-na.amazon.com
-- jc9wr0p5rbtco3ibchj4vnjz7z1lr1.ext-twitch.tv
-- lynx-prod-beacon-alb-498367235.us-east-1.elb.amazonaws.com
-- mediaservices.cdn-apple.com
-- mindfieldonline.com
-- nevoai-iothub-54-prod.azure-devices.net
-- ngfts.lge.com
-- service.idsync.analytics.yahoo.com
-- snu.lge.com
-- unagi-na.amazon.com
-- us.emp.lgsmartplatform.com
-- us.ibs.lgappstv.com
-- us.lgeapi.com
-- us.rdx2.lgtvsdp.com
-- log-0.samsungacr.com
-- log-1.samsungacr.com
-- log-2.samsungacr.com
-- log-3.samsungacr.com
+aic-gfts.lge.com
+aic-ngfts.lge.com
+aic-op-lss.lgthinq.com
+aic.api.lgtviot.com
+aic.homeprv.lgtvcommon.com
+aic.lgtviot.com
+aic.nudge.lgtvcommon.com
+aic.rdl.lgtvcommon.com
+aic.recommend.lgtvcommon.com
+aic.sports.lgtviot.com
+aic.wiseconfig.lgtvcommon.com
+d186rixkn5ruba.cloudfront.net
+d1ut6fykkt3imt.cloudfront.net
+d306ecvlh9tfv.cloudfront.net
+d3aqoihi2n8ty8.cloudfront.net
+d3cqix4sol87xw.cloudfront.net
+i.ibb.co
+images.pluto.tv
+images.redbox.com
+img.nvidiagrid.net
+ipv6.unagi-na.amazon.com
+jc9wr0p5rbtco3ibchj4vnjz7z1lr1.ext-twitch.tv
+lynx-prod-beacon-alb-498367235.us-east-1.elb.amazonaws.com
+mediaservices.cdn-apple.com
+mindfieldonline.com
+nevoai-iothub-54-prod.azure-devices.net
+ngfts.lge.com
+service.idsync.analytics.yahoo.com
+snu.lge.com
+unagi-na.amazon.com
+us.emp.lgsmartplatform.com
+us.ibs.lgappstv.com
+us.lgeapi.com
+us.rdx2.lgtvsdp.com
+log-0.samsungacr.com
+log-1.samsungacr.com
+log-2.samsungacr.com
+log-3.samsungacr.com
+
 # Other Smart TVs:
-- api.hismarttv.com
-- auth.hismarttv.com
-- unified.hismarttv.com
-- msg.hismarttv.com
+api.hismarttv.com
+auth.hismarttv.com
+unified.hismarttv.com
+msg.hismarttv.com
+
 # QNAP Computers:
-- api-software.qnap.com
-- d-20p1an5qn.qnap.com
-- marketing-materials.qnap.com
-- marketing.qnap.com
-- origin-marketing.qnap.com
-- origin-www.qnap.com
-- partner.qnap.com
-- iot.qnap.com
-- ixq.qnap.com
+api-software.qnap.com
+d-20p1an5qn.qnap.com
+marketing-materials.qnap.com
+marketing.qnap.com
+origin-marketing.qnap.com
+origin-www.qnap.com
+partner.qnap.com
+iot.qnap.com
+ixq.qnap.com
+
 # iTop VPN Telemetry:
-- api.itopvpn.com
-- applytics.nordvpn.com
-- stats.itopvpn.com
-- update.filesupdating.com
-# NFL - American Football Telemetry:
-- api.nfl.com
+api.itopvpn.com
+applytics.nordvpn.com
+stats.itopvpn.com
+update.filesupdating.com
+
+# NFL American Football Telemetry:
+api.nfl.com
+
 # Topaz Labs Telemetry:
-- api.topaz-labs.net
+api.topaz-labs.net
+
 # TrueNAS Software Telemetry:
-- usage.truenas.com
+usage.truenas.com
+
 # 3D Mark Software tracking cookies:
-- cdn.cookielaw.org
-- snap.licdn.com
+cdn.cookielaw.org
+snap.licdn.com
+
 # Adobe Systems Telemetry:
-- cc-api-data.adobe.io
+cc-api-data.adobe.io
+
 # CCleaner Telemetry Desktop App Spyware:
-- ccleaner.tools.avcdn.net
+ccleaner.tools.avcdn.net
+
 # IObit Desktop Popup Advertisements:
-- cdn.inspectlet.com
-- codes.iobit.com
-- cs833182181.wpc.etacdn.net
-- doug1izaerwt3.cloudfront.net
-# uTorrent Desktop App Ads- SlyEBL:
-- bench.utorrent.com
+cdn.inspectlet.com
+codes.iobit.com
+cs833182181.wpc.etacdn.net
+doug1izaerwt3.cloudfront.net
+
+# uTorrent Desktop App AdsSlyEBL:
+bench.utorrent.com
+
 # Cloudfront CDN Ad Server:
-- d20qwf0wrdtevy.cloudfront.net
-- d2u28gf9o4d1cv.cloudfront.net
-- prod-elb-ats-620772989.us-east-1.elb.amazonaws.com
-- prod.ups-ats.us-east-1.aolp-ds-prd.aws.oath.cloud
+d20qwf0wrdtevy.cloudfront.net
+d2u28gf9o4d1cv.cloudfront.net
+prod-elb-ats-620772989.us-east-1.elb.amazonaws.com
+prod.ups-ats.us-east-1.aolp-ds-prd.aws.oath.cloud
+
 # AdultTime Ads:
-- kinesis.us-east-1.amazonaws.com
-- d186rixkn5ruba.cloudfront.net
-- d1ut6fykkt3imt.cloudfront.net
-- d306ecvlh9tfv.cloudfront.net
-- d3aqoihi2n8ty8.cloudfront.net
+kinesis.us-east-1.amazonaws.com
+d186rixkn5ruba.cloudfront.net
+d1ut6fykkt3imt.cloudfront.net
+d306ecvlh9tfv.cloudfront.net
+d3aqoihi2n8ty8.cloudfront.net
+
 # Roblox:
-- roblox.com
-- rbxcdn.com
-- rbxtrk.com
-- roblox.plus
-- rbxcdn.plus
-- rbxtrk.plus
+roblox.com
+rbxcdn.com
+rbxtrk.com
+roblox.plus
+rbxcdn.plus
+rbxtrk.plus
+
 # Google Ads:
-- redirector.googlevideo.com
-- ads.google.com
-- captive.adservice.google.com
-- adwords.google.com
-- adwords.l.google.com
+redirector.googlevideo.com
+ads.google.com
+captive.adservice.google.com
+adwords.google.com
+adwords.l.google.com
+
 # HBBTV:
-- hbbtv.3sat.de
-- hbbtv.kabeleins.de
-- hbbtv.qvc.de
-- hbbtv.rtl2.de
-- hbbtv.sat1.de
-- hbbtv.sixx.de
-- hbbtv.superrtl.de
+hbbtv.3sat.de
+hbbtv.kabeleins.de
+hbbtv.qvc.de
+hbbtv.rtl2.de
+hbbtv.sat1.de
+hbbtv.sixx.de
+hbbtv.superrtl.de
+
 # Snapchat:
-- snap-ads.c
-- snap-ads.com
-- snap-ads.net
-- snap-chat.c
-- snap-chat.com
-- snap-chat.net
-- snap-dev.c
-- snap-dev.com
-- snap-dev.net
-- snap-kit.c
-- snap-kit.com
-- snap-kit.net
-- snap.c
-- snap.com
+snap-ads.c
+snap-ads.com
+snap-ads.net
+snap-chat.c
+snap-chat.com
+snap-chat.net
+snap-dev.c
+snap-dev.com
+snap-dev.net
+snap-kit.c
+snap-kit.com
+snap-kit.net
+snap.c
+snap.com
+
 # Spotify:
-- pixel.spotify.com
-- pixel-static.spotify.com
+pixel.spotify.com
+pixel-static.spotify.com
+
 # Tinder App:
-- tinder.com
-- gotinder.com
-- gtinder.com
-- gstinder.com
-- gstinders.com
-- sparkstinder.com
-- sparkstinders.com
-- gsparkstinder.com
-- gsparkstinders.com
+tinder.com
+gotinder.com
+gtinder.com
+gstinder.com
+gstinders.com
+sparkstinder.com
+sparkstinders.com
+gsparkstinder.com
+gsparkstinders.com
+
 # Possibly the number one domain that serves YouTube Ads. We must block it:
-- manifest.googlevideo.com
+manifest.googlevideo.com
