538c4b2ad4fa49ca3177892f97dd0e1df8a212d3
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-15 14:28:22 -0800

    Update exact-blacklist.sql

--- domains/exact-blacklist.sql
+++ domains/exact-blacklist.sql
@@ -1,5 +1,9 @@
 a1241.dsct.akamai.net -- Akamai CDN Ad Server
 a21ed24aedde648804e7-228765c84088fef4ff5e70f2710398e9.r17.cf1.rackcdn.com -- Twitch Video Ads
+abtauthprd.samsungcloudsolution.com -- Samsung Smart TV
+abtauthprd.samsungcloudsolution.net -- Samsung Smart TV
+acr0.samsungcloudsolution.com -- Samsung Smart TV
+acr0.samsungcloudsolution.net -- Samsung Smart TV
 adserver-use1-ext-prod-a-563525597.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
 aic-gfts.lge.com -- LG Smart TV
 aic-ngfts.lge.com -- LG Smart TV
@@ -12,6 +16,8 @@ aic.rdl.lgtvcommon.com -- LG Smart TV
 aic.recommend.lgtvcommon.com -- LG Smart TV
 aic.sports.lgtviot.com -- LG Smart TV
 aic.wiseconfig.lgtvcommon.com -- LG Smart TV
+amauthprd.samsungcloudsolution.com -- Samsung Smart TV
+amauthprd.samsungcloudsolution.net -- Samsung Smart TV
 api.itopvpn.com -- iTop VPN Telemetry
 api.nfl.com -- NFL - American Football Telemetry
 api.topaz-labs.net -- Topaz Labs Telemetry
@@ -23,12 +29,16 @@ cc-api-data.adobe.io -- Adobe Systems Telemetry
 ccleaner.tools.avcdn.net -- CCleaner Telemetry Desktop App Spyware
 cdn.cookielaw.org -- 3D Mark Software tracking cookies
 cdn.inspectlet.com -- IObit Desktop Popup Advertisements
+cdn.samsungcloudsolution.com -- Samsung Smart TV
+cdn.samsungcloudsolution.net -- Samsung Smart TV
 cdn77.utomik.com -- LG Smart TV
 cf-trickplay.aux.pv-cdn.net -- LG Smart TV
 client-api.nordvpn.com -- NordVPN Telemetry
 cloudfront.xp-assets.aiv-cdn.net -- LG Smart TV
 codes.iobit.com -- IObit Desktop Pop up Ads
 common.lgthinq.com -- LG Smart TV
+configprd.samsungcloudsolution.com -- Samsung Smart TV
+configprd.samsungcloudsolution.net -- Samsung Smart TV
 cs833182181.wpc.etacdn.net -- IObit Desktop Pop-up Ads
 d186rixkn5ruba.cloudfront.net -- AdultTime Ads
 d1ut6fykkt3imt.cloudfront.net -- AdultTime Ads
@@ -45,6 +55,10 @@ doh.dns.apple.com -- Apple DoH Blocks Pi-Hole Queries Entirely
 doug1izaerwt3.cloudfront.net -- IObit Desktop Pop-up Ads
 dxtkri2c61io4.cloudfront.net -- Twitch Video Ads
 estore.iobit -- IObit Ads
+fkp.samsungcloudsolution.com -- Samsung Smart TV
+fkp.samsungcloudsolution.net -- Samsung Smart TV
+gamespromotion.samsungcloudsolution.com -- Samsung Smart TV
+gamespromotion.samsungcloudsolution.net -- Samsung Smart TV
 giveaway.iobit -- IObit Ads
 global.poe.live-video.net -- Twitch Video Ads
 gsn-rinkeby.redditspace.com -- Reddit Suspected Ads
@@ -62,17 +76,35 @@ manifest.googlevideo.com -- Possibly the number one domain that servers YouTube 
 mediaservices.cdn-apple.com -- LG Smart TV
 mindfieldonline.com -- LG Smart TV
 munchkin.marketo.net -- 3D Mark Software Tracking Cookies
+musicid.samsungcloudsolution.com -- Samsung Smart TV
+musicid.samsungcloudsolution.net -- Samsung Smart TV
 ncp-gw-sports.media.yahoo.com -- Yahoo - Sports Video Ads
 nevoai-iothub-54-prod.azure-devices.net -- LG Smart TV
 ngfts.lge.com -- LG Smart TV
+notice.samsungcloudsolution.com -- Samsung Smart TV
+notice.samsungcloudsolution.net -- Samsung Smart TV
+noticecdn.samsungcloudsolution.com -- Samsung Smart TV
+noticecdn.samsungcloudsolution.net -- Samsung Smart TV
+noticefile.samsungcloudsolution.com -- Samsung Smart TV
+noticefile.samsungcloudsolution.net -- Samsung Smart TV
 ocsp.sca1b.amazontrust.com -- amazon.com Ads
+oempprd.samsungcloudsolution.com -- Samsung Smart TV
+oempprd.samsungcloudsolution.net -- Samsung Smart TV
+prderrordumphsm.samsungcloudsolution.com -- Samsung Smart TV
+prderrordumphsm.samsungcloudsolution.net -- Samsung Smart TV
 prod-elb-ats-620772989.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
 prod.ups-ats.us-east-1.aolp-ds-prd.aws.oath.cloud -- Amazon CDN Ad Server
+prov.samsungcloudsolution.com -- Samsung Smart TV
+prov.samsungcloudsolution.net -- Samsung Smart TV
 purchase-iobit-com.us-east-1.elasticbeanstalk.com -- IObit Desktop Pop-up Ads
 purchase.iobit.com -- IObit Desktop Pop up Ads
 qt2-ngfts.lge.com -- LG Smart TV
 redirector.googlevideo.com -- Google Ads
 s3-iad-2.cf.dash.row.aiv-cdn.net -- LG Smart TV
+samsungcloudsolution.com -- Samsung Smart TV
+samsungcloudsolution.net -- Samsung Smart TV
+sas.samsungcloudsolution.com -- Samsung Smart TV
+sas.samsungcloudsolution.net -- Samsung Smart TV
 service.idsync.analytics.yahoo.com -- LG Smart TV
 snap.licdn.com -- 3D Mark Software tracking cookies
 snu.lge.com -- LG Smart TV
@@ -88,6 +120,8 @@ us.ibs.lgappstv.com -- LG Smart TV
 us.lgeapi.com -- LG Smart TV
 us.rdx2.lgtvsdp.com -- LG Smart TV
 usage.truenas.com -- TrueNAS Software Telemetry
+vdterms.samsungcloudsolution.com -- Samsung Smart TV
+vdterms.samsungcloudsolution.net -- Samsung Smart TV
 video-edge-55b156.pdx01.abs.hls.ttvnw.net -- Twitch Video Ads
 video-edge-6dca22.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
 video-edge-833564.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
