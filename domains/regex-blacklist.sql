d81e4876f43c6e3a66d172893feb6597a28f83f7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-02-22 12:28:48 -0500

    Update regex-blacklist.sql

--- domains/regex-blacklist.sql
+++ domains/regex-blacklist.sql
@@ -1,84 +1,85 @@
-^[a-z]+ad\.[a-z]+\.com\.edgesuite\.net$ -- Take out all ad servers in one go for master domain com.edgesuite.net
+(\.|^)((www|(w[0-9]\.)?web|media((-[a-z]{3}|\.[a-z]{4})[0-9]{1,2}-[0-9](\.|-)(cdn|fna))?)\.)?whatsapp\.(com|net)$ -- WhatsApp
+(\.|^)ad-score\.com$ -- Overwolf Desktop App (PC Games)
+(\.|^)ahoravideo.*$ -- Suspected Virus Harboring Websites
 (\.|^)antivirushub\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
-(\.|^)shadowadcity\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
-(\.|^)trafyield\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
 (\.|^)appsync-api\.us-[a-z]+-[0-9]?\.amazonaws\.com$ -- AdultTime
+(\.|^)bideo.*$ -- Suspected Virus Harboring Websites
+(\.|^)deimos3.apple\.com$ -- Apple
+(\.|^)fairu.*$ -- Suspected Virus Harboring Websites
+(\.|^)fake\.video\.url\.webm$ -- Overwolf Desktop App (PC Games)
+(\.|^)filesupdating\.com$ -- iTop VPN
 (\.|^)gammaentertainment\.com$ -- AdultTime
 (\.|^)getbeamer\.com$ -- AdultTime
-(\.|^)lambda-url\.us-east-1\.on\.aws$ -- AdultTime
-(\.|^)usage\.trackjs\.com$ -- AdultTime
-(\.|^)[a-z0-9]+\.execute-api\.us-[a-z]+-[0-9]?\.amazonaws\.com$ -- AdultTime
-(\.|^)deimos3.apple\.com$ -- Apple
-^.*iad.*\.amazon\.com$ -- Amazon
-(\.|^)xyz$ -- Bittorrent
-(\.|^)filesupdating\.com$ -- iTop VPN
-(\.|^)ibs\.lgappstv\.com$ -- LG Smart TVs
-(\.|^)lgsmartad\.com$ -- LG Smart TVs
-(\.|^)rdx2\.lgtvsdp\.com$ -- LG Smart TVs
-(\.|^)smartshare\.lgtvsdp\.com$ -- LG Smart TVs
-(\.|^)ok\.ru$ -- OK.ru
-^(collect|usage)-(eu|us)-fy[0-9]+\.myhomescreen\.(tv|tvcollect)$ -- Other Smart TVs
-(\.|^)ad-score\.com$ -- Overwolf Desktop App (PC Games)
-(\.|^)fake\.video\.url\.webm$ -- Overwolf Desktop App (PC Games)
 (\.|^)giraffic\.com$ -- Samsung Smart TV
+(\.|^)ibs\.lgappstv\.com$ -- LG Smart TVs
+(\.|^)impala-media-production\.s3\.amazonaws\.com$ -- Snapchat
 (\.|^)internetat\.tv$ -- Samsung Smart TV
+(\.|^)lambda-url\.us-east-1\.on\.aws$ -- AdultTime
+(\.|^)lgsmartad\.com$ -- LG Smart TVs
+(\.|^)log[0-9]*\.spotify\.com$ -- Spotify
+(\.|^)ok\.ru$ -- OK.ru
 (\.|^)pavv\.co\.kr$ -- Samsung Smart TV
+(\.|^)privatproxy.*$ -- Suspected Virus Harboring Websites
+(\.|^)rdx2\.lgtvsdp\.com$ -- LG Smart TVs
 (\.|^)samsungelectronics\.com$ -- Samsung Smart TV
 (\.|^)samsungqbe\.com$ -- Samsung Smart TV
 (\.|^)samsungrm\.net$ -- Samsung Smart TV
-^ext[0-9]?-iad[0-9]?\.steamserver\.net$ -- Steam Desktop App (Gaming)
-(\.|^)ahoravideo.*$ -- Suspected Virus Harboring Websites
-(\.|^)bideo.*$ -- Suspected Virus Harboring Websites
-(\.|^)fairu.*$ -- Suspected Virus Harboring Websites
-(\.|^)privatproxy.*$ -- Suspected Virus Harboring Websites
-(\.|^)wmail.*$ -- Suspected Virus Harboring Websites
-(\.|^)impala-media-production\.s3\.amazonaws\.com$ -- Snapchat
 (\.|^)sc-cdn\.net$ -- Snapchat
-^[a-z0-9]+\.ssm[1-2]?\.internet\.sony\.tv$ -- Sony Smart TV
-(\.|^)log[0-9]*\.spotify\.com$ -- Spotify
-(\.|^)((www|(w[0-9]\.)?web|media((-[a-z]{3}|\.[a-z]{4})[0-9]{1,2}-[0-9](\.|-)(cdn|fna))?)\.)?whatsapp\.(com|net)$ -- WhatsApp
+(\.|^)shadowadcity\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
+(\.|^)smartshare\.lgtvsdp\.com$ -- LG Smart TVs
+(\.|^)stickyadstv\.com\.akadns\.net$ -- Curseforge App Ads
+(\.|^)trafyield\.com$ -- 1337x.to - Sneaky redirect domains that probably log your IP and any other information it gets from your browser sending a GET or POST request.
+(\.|^)tvinteractive\.tv$ -- Vizio Smart TV
+(\.|^)usage\.trackjs\.com$ -- AdultTime
 (\.|^)whatsapp-cdn-shv-[0-9]{2}-[a-z]{3}[0-9]\.fbcdn\.net$ -- WhatsApp
-^a-content-[a-z0-9]+-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
-^adserver-.*\.amazonaws\.com$ -- Yahoo
+(\.|^)wmail.*$ -- Suspected Virus Harboring Websites
+(\.|^)xyz$ -- Bittorrent
+(\.|^)youtube-nocookie\.com$ -- YouTube
+(\.|^)[a-z0-9]+\.execute-api\.us-[a-z]+-[0-9]?\.amazonaws\.com$ -- AdultTime
+(^|\.)giraffic\.com$ -- Samsung TV
+(^|\.)ibs\.lgappstv\.com$ -- LG TV
+(^|\.)internetat\.tv$ -- Samsung TV
+(^|\.)lgsmartad\.com$ -- LG TV
+(^|\.)lgtvcommon\.com$ -- LG TV
+(^|\.)pavv\.co\.kr$ -- Samsung TV
+(^|\.)rdx2\.lgtvsdp\.com$ -- LG TV
+(^|\.)samsungcloudsolution\.net$ -- Samsung TV
+(^|\.)samsungelectronics\.com$ -- Samsung TV
+(^|\.)samsungrm\.net$ -- Samsung TV
+(^|\.)smartshare\.lgtvsdp\.com$ -- LG TV
 [a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
-^[a-z]{2}-[a-z]{2}-[a-z]{2}\.ybp\.gysm\.yahoodns\.net$ -- Yahoo
+^(.+[_.-])?adse?rv(er?|ice)?s?[0-9]*[_.-] -- mmotti RegEx
+^(.+[_.-])?telemetry[_.-]] -- mmotti RegEx
+^(collect|usage)-(eu|us)-fy[0-9]+\.myhomescreen\.(tv|tvcollect)$ -- Other Smart TVs
+^.*iad.*\.amazon\.com$ -- Amazon
+^a-content-[a-z0-9]+-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^ad([sxv]?[0-9]*|system)[_.-]([^.[:space:]]+\.){1,}|[_.-]ad([sxv]?[0-9]*|system)[_.-]] -- mmotti RegEx
+^adim(age|g)s?[0-9]*[_.-]] -- mmotti RegEx
+^adserver-.*\.amazonaws\.com$ -- Yahoo
+^adtrack(er|ing)?[0-9]*[_.-]] -- mmotti RegEx
+^advert(s|is(ing|ements?))?[0-9]*[_.-]] -- mmotti RegEx
+^aff(iliat(es?|ion))?[_.-]] -- mmotti RegEx
+^analytics?[_.-]] -- mmotti RegEx
+^api\..*\.hismarttv\.com$ -- Other TV
+^banners?[_.-]] -- mmotti RegEx
+^beacons?[0-9]*[_.-]] -- mmotti RegEx
 ^content(|-geo)\.uplynk\.com$ -- Yahoo
+^count(ers?)?[0-9]*[_.-]] -- mmotti RegEx
 ^cs[0-9]+\.wpc\.thetacdn\.net$ -- Yahoo
 ^dms-[a-z0-9]+report\.wc\.yahoodns\.net$ -- Yahoo
 ^dns-[a-z0-9]+\.sombrero\.yahoo\.net$ -- Yahoo
 ^ds-beap[0-9]\.[a-z]{2}\.[a-z0-9]+\.yahoodns\.net$ -- Yahoo
 ^ds-global[0-9]\.l[0-9].search\.[a-z0-9]+\.yahoo\.com$ -- Yahoo
+^ext[0-9]?-iad[0-9]?\.steamserver\.net$ -- Steam Desktop App (Gaming)
+^mads\.] -- mmotti RegEx
 ^nado-ecs-lb-[a-z]+-[a-z]+-[0-9]-[0-9]+\.[a-z]+-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
+^pixels?[-.]] -- mmotti RegEx
 ^prod-elb-ats-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
 ^ssp-ats-prod-[a-z\-]+-[0-9]\.one-mobile-prod\.aws\.oath\.cloud$ -- Yahoo
+^stat(s|istics)?[0-9]*[_.-]] -- mmotti RegEx
 ^uplynk-beacon-newvpc-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
 ^v-[a-z0-9]+\.wc\.yahoodns\.net$ -- Yahoo
-(\.|^)youtube-nocookie\.com$ -- YouTube
-^(.+[_.-])?adse?rv(er?|ice)?s?[0-9]*[_.-] -- mmotti RegEx
-^(.+[_.-])?telemetry[_.-]] -- mmotti RegEx
-^ad([sxv]?[0-9]*|system)[_.-]([^.[:space:]]+\.){1,}|[_.-]ad([sxv]?[0-9]*|system)[_.-]] -- mmotti RegEx
-^adim(age|g)s?[0-9]*[_.-]] -- mmotti RegEx
-^adtrack(er|ing)?[0-9]*[_.-]] -- mmotti RegEx
-^advert(s|is(ing|ements?))?[0-9]*[_.-]] -- mmotti RegEx
-^aff(iliat(es?|ion))?[_.-]] -- mmotti RegEx
-^analytics?[_.-]] -- mmotti RegEx
-^banners?[_.-]] -- mmotti RegEx
-^beacons?[0-9]*[_.-]] -- mmotti RegEx
-^count(ers?)?[0-9]*[_.-]] -- mmotti RegEx
-^mads\.] -- mmotti RegEx
-^pixels?[-.]] -- mmotti RegEx
-^stat(s|istics)?[0-9]*[_.-]] -- mmotti RegEx
-(^|\.)ibs\.lgappstv\.com$ -- LG TV
-(^|\.)lgsmartad\.com$ -- LG TV
-(^|\.)lgtvcommon\.com$ -- LG TV
-(^|\.)rdx2\.lgtvsdp\.com$ -- LG TV
-(^|\.)smartshare\.lgtvsdp\.com$ -- LG TV
 ^[a-rt-z][a-z]{10,14}$ -- LG TV
-(^|\.)giraffic\.com$ -- Samsung TV
-(^|\.)internetat\.tv$ -- Samsung TV
-(^|\.)pavv\.co\.kr$ -- Samsung TV
-(^|\.)samsungcloudsolution\.net$ -- Samsung TV
-(^|\.)samsungelectronics\.com$ -- Samsung TV
-(^|\.)samsungrm\.net$ -- Samsung TV
-(\.|^)tvinteractive\.tv$ -- Vizio Smart TV
-^api\..*\.hismarttv\.com$ -- Other TV
+^[a-z0-9]+\.ssm[1-2]?\.internet\.sony\.tv$ -- Sony Smart TV
+^[a-z]+ad\.[a-z]+\.com\.edgesuite\.net$ -- Take out all ad servers in one go for master domain com.edgesuite.net
+^[a-z]{2}-[a-z]{2}-[a-z]{2}\.ybp\.gysm\.yahoodns\.net$ -- Yahoo
