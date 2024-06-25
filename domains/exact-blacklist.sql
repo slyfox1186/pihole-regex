1c5b3691f4db2f6a2db0c919fbbf5296c9eed49f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-25 13:38:52 -0400

    Update exact-blacklist.sql

--- domains/exact-blacklist.sql
+++ domains/exact-blacklist.sql
@@ -10,213 +10,59 @@ d3m1uefep57n8q.cloudfront.net -- Twitch Video Ads
 dxtkri2c61io4.cloudfront.net -- Twitch Video Ads
 global.poe.live-video.net -- Twitch Video Ads
 jc9wr0p5rbtco3ibchj4vnjz7z1lr1.ext-twitch.tv -- Twitch Video Ads
-video-edge-55b156.pdx01.abs.hls.ttvnw.net -- Twitch Video Ads
-video-edge-6dca22.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
-video-edge-833564.atl01.abs.hls.ttvnw.net -- Twitch Video Ads
-video-edge-8491b8.pdx01.abs.hls.ttvnw.net -- Twitch Video Ads
-video-weaver.atl01.hls.ttvnw.net -- Twitch Video Ads
-video-weaver.jfk04.hls.ttvnw.net -- Twitch Video Ads
-
-# Samsung Smart TV
-abtauthprd.samsungcloudsolution.com -- Samsung Smart TV
-abtauthprd.samsungcloudsolution.net -- Samsung Smart TV
-acr0.samsungcloudsolution.com -- Samsung Smart TV
-acr0.samsungcloudsolution.net -- Samsung Smart TV
-amauthprd.samsungcloudsolution.com -- Samsung Smart TV
-amauthprd.samsungcloudsolution.net -- Samsung Smart TV
-configprd.samsungcloudsolution.com -- Samsung Smart TV
-configprd.samsungcloudsolution.net -- Samsung Smart TV
-fkp.samsungcloudsolution.com -- Samsung Smart TV
-fkp.samsungcloudsolution.net -- Samsung Smart TV
-gamespromotion.samsungcloudsolution.com -- Samsung Smart TV
-gamespromotion.samsungcloudsolution.net -- Samsung Smart TV
-musicid.samsungcloudsolution.com -- Samsung Smart TV
-musicid.samsungcloudsolution.net -- Samsung Smart TV
-notice.samsungcloudsolution.com -- Samsung Smart TV
-notice.samsungcloudsolution.net -- Samsung Smart TV
-noticecdn.samsungcloudsolution.com -- Samsung Smart TV
-noticecdn.samsungcloudsolution.net -- Samsung Smart TV
-noticefile.samsungcloudsolution.com -- Samsung Smart TV
-noticefile.samsungcloudsolution.net -- Samsung Smart TV
-oempprd.samsungcloudsolution.com -- Samsung Smart TV
-oempprd.samsungcloudsolution.net -- Samsung Smart TV
-prderrordumphsm.samsungcloudsolution.com -- Samsung Smart TV
-prderrordumphsm.samsungcloudsolution.net -- Samsung Smart TV
-samsungcloudsolution.com -- Samsung Smart TV
-samsungcloudsolution.net -- Samsung Smart TV
-sas.samsungcloudsolution.com -- Samsung Smart TV
-sas.samsungcloudsolution.net -- Samsung Smart TV
-vdterms.samsungcloudsolution.com -- Samsung Smart TV
-vdterms.samsungcloudsolution.net -- Samsung Smart TV
 
 # Amazon
-ad.amazonaws.com -- Amazon
-ad.amazonaws.to -- Amazon
-ad.amazontrust.com -- Amazon
-ad.amazontrust.to -- Amazon
-ad.amzn.com -- Amazon
-ad.amzn.to -- Amazon
-ad.apzones.com -- Amazon
-ad.apzones.to -- Amazon
-ad.digicert.com -- Amazon
-ad.digicert.to -- Amazon
-ad.marketwatch.com -- Amazon
-ad.marketwatch.to -- Amazon
-ad.mzstatic.com -- Amazon
-ad.mzstatic.to -- Amazon
-ad.symb.com -- Amazon
-ad.symc.com -- Amazon
-ad.symd.com -- Amazon
-ad.wp.com -- Amazon
-ad.wp.to -- Amazon
-ad.yimg.com -- Amazon
-ad.yimg.to -- Amazon
-ad.youtube.com -- Amazon
-ad.youtube.to -- Amazon
-ad.ytimg.com -- Amazon
-ad.ytimg.to -- Amazon
-ads.amzn.com -- Amazon
-ads.amzn.to -- Amazon
 adserver-use1-ext-prod-a-563525597.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
 lynx-prod-beacon-alb-498367235.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
 prod-elb-ats-620772989.us-east-1.elb.amazonaws.com -- Amazon CDN Ad Server
 prod.ups-ats.us-east-1.aolp-ds-prd.aws.oath.cloud -- Amazon CDN Ad Server
+
+# Reddit Suspected Ads
+gsn-rinkeby.redditspace.com -- Reddit Suspected Ads
 
 # Reddit.com
 advertising.reddithelp.com -- Reddit.com
 www.reddithelp.com -- Reddit.com
 
 # LG Smart TV
-aic-gfts.lge.com -- LG Smart TV
-aic-ngfts.lge.com -- LG Smart TV
-aic-op-lss.lgthinq.com -- LG Smart TV
-aic.api.lgtviot.com -- LG Smart TV
-aic.homeprv.lgtvcommon.com -- LG Smart TV
-aic.lgtviot.com -- LG Smart TV
-aic.nudge.lgtvcommon.com -- LG Smart TV
-aic.rdl.lgtvcommon.com -- LG Smart TV
-aic.recommend.lgtvcommon.com -- LG Smart TV
-aic.sports.lgtviot.com -- LG Smart TV
-aic.wiseconfig.lgtvcommon.com -- LG Smart TV
 cdn77.utomik.com -- LG Smart TV
 cf-trickplay.aux.pv-cdn.net -- LG Smart TV
 common.lgthinq.com -- LG Smart TV
-d-20p1an5qn.qnap.com -- LG Smart TV
+i.ibb.co -- LG Smart TV
+img.nvidiagrid.net -- LG Smart TV
 ipv6.unagi-na.amazon.com -- LG Smart TV
-ixq.qnap.com -- LG Smart TV
 lgtvonline.lge.com -- LG Smart TV
-lss.lgthinq.com -- LG Smart TV
-nevoai-iothub-54-prod.azure-devices.net -- LG Smart TV
+mediaservices.cdn-apple.com -- LG Smart TV
+mindfieldonline.com -- LG Smart TV
 ngfts.lge.com -- LG Smart TV
 s3-iad-2.cf.dash.row.aiv-cdn.net -- LG Smart TV
 snu.lge.com -- LG Smart TV
 us.emp.lgsmartplatform.com -- LG Smart TV
 us.ibs.lgappstv.com -- LG Smart TV
-us.lgeapi.com -- LG Smart TV
-mediaservices.cdn-apple.com -- LG Smart TV
-i.ibb.co -- LG Smart TV
-images.pluto.tv -- LG Smart TV
-images.redbox.com -- LG Smart TV
-img.nvidiagrid.net -- LG Smart TV
+
+# Yahoo
+dns-fxp05etdx.sombrero.yahoo.net -- Yahoo - Video Ads
+ncp-gw-sports.media.yahoo.com -- Yahoo - Sports Video Ads
 service.idsync.analytics.yahoo.com -- LG Smart TV
+
+# IObit
+cdn.inspectlet.com -- IObit - Desktop Popup Advertisements
+codes.iobit.com -- IObit - Desktop Pop up Ads
+cs833182181.wpc.etacdn.net -- IObit - Desktop Pop-up Ads
+doug1izaerwt3.cloudfront.net -- IObit - Desktop Pop-up Ads
+purchase.iobit.com -- IObit - Desktop Pop up Ads
+
+# 3D Mark Software Tracking Cookies
+cdn.cookielaw.org -- 3D Mark Software Tracking Cookies
+snap.licdn.com -- 3D Mark Software Tracking Cookies
 
 # QNAP Computers
 api-software.qnap.com -- QNAP Computers
-d-20p1an5qn.qnap.com -- QNAP Computers
 marketing-materials.qnap.com -- QNAP Computers
 marketing.qnap.com -- QNAP Computers
 origin-marketing.qnap.com -- QNAP Computers
 origin-www.qnap.com -- QNAP Computers
 partner.qnap.com -- QNAP Computers
-ipv6.unagi-na.amazon.com -- LG Smart TV
-ixq.qnap.com -- QNAP Computers
-
-# Other Smart TVs
-api.hismarttv.com -- Other Smart TVs
-auth.hismarttv.com -- Other Smart TVs
-msg.hismarttv.com -- Other Smart TVs
-unified.hismarttv.com -- Other Smart TVs
-
-# iTop VPN Telemetry
-api.itopvpn.com -- iTop VPN Telemetry
-stats.itopvpn.com -- iTop VPN Telemetry
-update.filesupdating.com -- iTop VPN Telemetry
-
-# Adobe Systems Telemetry
-cc-api-data.adobe.io -- Adobe Systems Telemetry
-
-# NordVPN Telemetry
-applytics.nordvpn.com -- NordVPN Telemetry
-client-api.nordvpn.com -- NordVPN Telemetry
-
-# TrueNAS Software Telemetry
-usage.truenas.com -- TrueNAS Software Telemetry
-
-# Roblox
-roblox.com -- Roblox
-rbxcdn.com -- Roblox
-rbxtrk.com -- Roblox
-roblox.plus -- Roblox
-rbxcdn.plus -- Roblox
-rbxtrk.plus -- Roblox
-
-# Spotify
-pixel.spotify.com -- Spotify
-pixel-static.spotify.com -- Spotify
-
-# Tinder App
-tinder.com -- Tinder App
-gotinder.com -- Tinder App
-gtinder.com -- Tinder App
-gstinder.com -- Tinder App
-gstinders.com -- Tinder App
-sparkstinder.com -- Tinder App
-sparkstinders.com -- Tinder App
-gsparkstinder.com -- Tinder App
-gsparkstinders.com -- Tinder App
-
-# HBBTV
-hbbtv.3sat.de -- HBBTV
-hbbtv.kabeleins.de -- HBBTV
-hbbtv.qvc.de -- HBBTV
-hbbtv.rtl2.de -- HBBTV
-hbbtv.sat1.de -- HBBTV
-hbbtv.sixx.de -- HBBTV
-hbbtv.superrtl.de -- HBBTV
-
-# Reddit Suspected Ads
-gsn-rinkeby.redditspace.com -- Reddit Suspected Ads
-
-# Google Ads
-redirector.googlevideo.com -- Google Ads
-ads.google.com -- Google
-captive.adservice.google.com -- Google
-adwords.google.com -- Google
-adwords.l.google.com -- Google
-
-# Snapchat
-snap-ads.c -- Snapchat
-snap-ads.com -- Snapchat
-snap-ads.net -- Snapchat
-snap-chat.c -- Snapchat
-snap-chat.com -- Snapchat
-snap-chat.net -- Snapchat
-snap-dev.c -- Snapchat
-snap-dev.com -- Snapchat
-snap-dev.net -- Snapchat
-snap-kit.c -- Snapchat
-snap-kit.com -- Snapchat
-snap-kit.net -- Snapchat
-snap.c -- Snapchat
-snap.com -- Snapchat
-snap.net -- Snapchat
-
-# Microsoft Azure
-nevoai-iothub-54-prod.azure-devices.net -- LG Smart TV
-
-# Yahoo
-dns-fxp05etdx.sombrero.yahoo.net -- Yahoo - Video Ads
-ncp-gw-sports.media.yahoo.com -- Yahoo - Sports Video Ads
 
 # Pi-Hole Queries
 doh.dns.apple.com -- Apple DoH Blocks Pi-Hole Queries Entirely
@@ -225,32 +71,22 @@ doh.dns.apple.com -- Apple DoH Blocks Pi-Hole Queries Entirely
 cdn.cookielaw.org -- 3D Mark Software tracking cookies
 snap.licdn.com -- 3D Mark Software tracking cookies
 
-# IObit - Desktop Popup Advertisements
-cdn.inspectlet.com -- IObit - Desktop Popup Advertisements
-codes.iobit.com -- IObit - Desktop Pop up Ads
-doug1izaerwt3.cloudfront.net -- IObit - Desktop Pop-up Ads
-cs833182181.wpc.etacdn.net -- IObit - Desktop Pop-up Ads
-stats.iobit.com -- IObit - Desktop Pop up Ads
-store.iobit.com -- IObit - Desktop Pop up Ads
-purchase-iobit-com.us-east-1.elasticbeanstalk.com -- IObit - Desktop Pop-up Ads
-purchase.iobit.com -- IObit - Desktop Pop up Ads
-
 # CCleaner Telemetry Desktop App Spyware
 ccleaner.tools.avcdn.net -- CCleaner Telemetry Desktop App Spyware
 
-# AdultTime - Ads
-d186rixkn5ruba.cloudfront.net -- AdultTime - Ads
-d1ut6fykkt3imt.cloudfront.net -- AdultTime - Ads
-d306ecvlh9tfv.cloudfront.net -- AdultTime - Ads
-kinesis.us-east-1.amazonaws.com -- AdultTime - Ads
-
-# Topaz Labs Telemetry
-api.topazlabs.com -- Topaz Labs Telemetry
-api.topaz-labs.net -- Topaz Labs Telemetry
-
-# Miscellaneous
+# Google / YouTube
+captive.adservice.google.com -- Google Ads
 manifest.googlevideo.com -- Possibly the number one domain that serves YouTube Ads. We must block it.
-mindfieldonline.com -- LG Smart TV
+
+# Adobe Systems Telemetry
+cc-api-data.adobe.io -- Adobe Systems Telemetry
+
+# TrueNAS Software Telemetry
+usage.truenas.com -- TrueNAS Software Telemetry
 
 # Bittorrent
 tagoe.lstartanalystconcepts.org.uk -- BitTorrent Client - Background tracking
+
+# AdultTime Ads
+d306ecvlh9tfv.cloudfront.net
+kinesis.us-east-1.amazonaws.com -- AdultTime - Ads
