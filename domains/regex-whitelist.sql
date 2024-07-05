83f5bb0632964448efc2c515175d2f6261de18a7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-07-05 15:30:18 -0400

    Update regex-whitelist.sql

--- domains/regex-whitelist.sql
+++ domains/regex-whitelist.sql
@@ -1,3 +1,4 @@
+(\.|^)dataplicity\.com$ -- Raspberry-Pi - Used to remotely control your Raspberry-Pi
 (\.|^)([iv]?\.|)redd\.it$ -- Reddit
 (\.|^)(accountkit|messenger)\.com$ -- Facebook - Essential CDN servers
 (\.|^)(connect\.|)facebook\.(com|net)$ -- Facebook - Essential CDN servers
@@ -7,16 +8,22 @@
 (\.|^)amd\.com$ -- AMD Microchip Maker
 (\.|^)apple-(cloudkit|livephotoskit)\.com$ -- Apple - Main domains for a good chunk of legitimate iOS services
 (\.|^)gammacdn\.com$ -- Load media content
+(\.|^)icloud\.com$ -- Apple iCloud cloud servers
+(\.|^)iij\.ad\.jp$ -- github.com/mmotti/pihole-regex
 (\.|^)lencr\.org$ -- Lets Encrypt - One of the main SSL certificate encryption websites
+(\.|^)linuxtracker\.org$ -- Linux
 (\.|^)mee6\.xyz$ -- Chrome Browser Discord App MEE6 Support Website
 (\.|^)minecraft\.net$ -- Mincraft Video Game
+(\.|^)no-ip\.com$ -- No-IP offers DNS services, email, network monitoring and SSL certificates
 (\.|^)reddit\.com$ -- Reddit
 (\.|^)robvanderwoude\.com$ -- Rob VanderWoude Learn how to code multiple languages
 (\.|^)samsungcloudcdn\.com$ -- Smart TV
 (\.|^)samsungotn\.net$ -- Smart TV
 (\.|^)twimg\.com$ -- Twitter
 (\.|^)unraid\.net$ -- Unraid - Required for the operating system to update
+(\.|^)wa\.me$ -- Misc
 (\.|^)windowsupdate\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
+(\.|^)yahoo\.com$ -- Yahoo had to be whitelisted due to some unknown conflict with the blacklist domains take domain over the whitelist
 (^|\.)itunes(-apple)?\.com(\.akadns\.net)?$ -- Apple iTunes
 (www\.|^)((go\.|)microsoft|windows)\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
 (www\.|^)yahoo\.com$ -- Yahoo
@@ -43,6 +50,7 @@
 ^(blog|business|(www\.|)careers|email|(dev(|\.careers)|channels|contests|insights|meta|talent))\.stackoverflow\.com$ -- Stackoverflow Developer\Coding help site
 ^(blog|community(-cdn|-staging)?|d-cloudflare|docs|downloads(-staging)?|et|guide|help|learn|models(-r2)?)\.(topazlabs\.com)$ -- TopazLabs - Legit purposes
 ^(calendar|developer(|s)|dns|docs|home|inbox|clients[1-6]*|mail|meet|pay(|ments)|photos|sites|time|wallet|workspace|www)\.google\.com$ -- Google - Allow known essential widget service
+^(cdn-mobile\.api|images|players|silo|siloh|stitcher)\.pluto\.tv$ -- pluto.tv streaming
 ^(cdn\.embed\.ly|cdn\.jsdelivr\.net|cdnjs\.loli\.net)\.cdn\.cloudflare\.net$ -- Crucial CDN server
 ^(client\.wns|edge\.activity)\.windows\.com$ -- Microsoft legit services
 ^(clientconfig\.passport|dns\.msftncsi|ipv6\.msftncsi|logincdn\.msauth|msedge\.api\.cdp|winatp-gw-neu|windows)\.microsoft\.com$ -- Microsoft legit services
@@ -90,4 +98,3 @@
 ^v[0-2]+?\.events\.data\.microsoft\.com$ -- Microsoft OneDrive, Azure, Windows Essentials updates, other, xboxlive required for saves to the cloud
 ^video-edge-[0-9]+?\.[a-z]+?[0-9]+?\.abs\.hls\.ttvnw\.net$ -- Twitch.tv
 ^whatsapp-cdn-shv-[0-9]{2}-[a-z]{3}[0-9]\.fbcdn\.net$ -- WhatsApp
-^(cdn-mobile\.api|images|players|silo|siloh|stitcher)\.pluto\.tv$ -- pluto.tv streaming
