604b686c8a23aed332285df1ba8fbcbe10be0f30
Jeff <37704019+slyfox1186@users.noreply.github.com>
2025-12-10 21:23:01 -0500

    Update regex-blacklist.sql

--- domains/regex-blacklist.sql
+++ domains/regex-blacklist.sql
@@ -56,26 +56,26 @@
 (^|\.)smartshare\.lgtvsdp\.com -- LG Smart TV
 [a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
 ^(.+[_.-])?adse?rv(er?|ice)?s?[0-9]*[_.-] -- mmotti RegEx
-^(.+[_.-])?telemetry[_.-]] -- mmotti RegEx
+^(.+[_.-])?telemetry[_.-] -- mmotti RegEx
 ^(collect|usage)-(eu|us)-fy[0-9]+\.myhomescreen\.(tv|tvcollect)$ -- Other Smart TVs
 ^.*iad.*\.amazon\.com$ -- Amazon
 ^[a-z0-9]+\.ssm[1-2]?\.internet\.sony\.tv$ -- Sony Smart TV
 ^[a-z]+ad\.[a-z]+\.com\.edgesuite\.net$ -- Take out all ad servers in one go for master domain com.edgesuite.net
 ^[a-z]{2}-[a-z]{2}-[a-z]{2}\.ybp\.gysm\.yahoodns\.net$ -- Yahoo
 ^a-content-[a-z0-9]+-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
-^ad([sxv]?[0-9]*|system)[_.-]([^.[:space:]]+\.){1,}|[_.-]ad([sxv]?[0-9]*|system)[_.-]] -- mmotti RegEx
-^adim(age|g)s?[0-9]*[_.-]] -- mmotti RegEx
+^ad([sxv]?[0-9]*|system)[_.-]([^.[:space:]]+\.){1,}|[_.-]ad([sxv]?[0-9]*|system)[_.-] -- mmotti RegEx
+^adim(age|g)s?[0-9]*[_.-] -- mmotti RegEx
 ^adserver-.*\.amazonaws\.com$ -- Yahoo
-^adtrack(er|ing)?[0-9]*[_.-]] -- mmotti RegEx
-^advert(s|is(ing|ements?))?[0-9]*[_.-]] -- mmotti RegEx
-^aff(iliat(es?|ion))?[_.-]] -- mmotti RegEx
-^analytics?[_.-]] -- mmotti RegEx
+^adtrack(er|ing)?[0-9]*[_.-] -- mmotti RegEx
+^advert(s|is(ing|ements?))?[0-9]*[_.-] -- mmotti RegEx
+^aff(iliat(es?|ion))?[_.-] -- mmotti RegEx
+^analytics?[_.-] -- mmotti RegEx
 ^api\..*\.hismarttv\.com$ -- Other TV
 ^api\..*\.hismarttv\.com$ -- Vizio Smart TV
-^banners?[_.-]] -- mmotti RegEx
-^beacons?[0-9]*[_.-]] -- mmotti RegEx
+^banners?[_.-] -- mmotti RegEx
+^beacons?[0-9]*[_.-] -- mmotti RegEx
 ^content(|-geo)\.uplynk\.com$ -- Yahoo
-^count(ers?)?[0-9]*[_.-]] -- mmotti RegEx
+^count(ers?)?[0-9]*[_.-] -- mmotti RegEx
 ^cs[0-9]+\.wpc\.thetacdn\.net$ -- Yahoo
 ^dms-[a-z0-9]+report\.wc\.yahoodns\.net$ -- Yahoo
 ^dns-[a-z0-9]+\.sombrero\.yahoo\.net$ -- Yahoo
@@ -83,11 +83,11 @@
 ^ds-global[0-9]\.l[0-9].search\.[a-z0-9]+\.yahoo\.com$ -- Yahoo
 ^ext[0-9]?-iad[0-9]?\.steamserver\.net$ -- Steam Desktop App (Gaming)
 ^iad[0-9]*-[a-z]+[0-9]*\.netflix\.com$ -- Netflix Ads
-^mads\.] -- mmotti RegEx
+^mads\. -- mmotti RegEx
 ^nado-ecs-lb-[a-z]+-[a-z]+-[0-9]-[0-9]+\.[a-z]+-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
-^pixels?[-.]] -- mmotti RegEx
+^pixels?[-.] -- mmotti RegEx
 ^prod-elb-ats-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
 ^ssp-ats-prod-[a-z\-]+-[0-9]\.one-mobile-prod\.aws\.oath\.cloud$ -- Yahoo
-^stat(s|istics)?[0-9]*[_.-]] -- mmotti RegEx
+^stat(s|istics)?[0-9]*[_.-] -- mmotti RegEx
 ^uplynk-beacon-newvpc-[0-9]+\.[a-z]{2}-[a-z]+-[0-9]\.elb\.amazonaws\.com$ -- Yahoo
 ^v-[a-z0-9]+\.wc\.yahoodns\.net$ -- Yahoo
